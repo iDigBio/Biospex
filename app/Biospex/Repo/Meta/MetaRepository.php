b1bc3c7c102c9caa34f510ac0246d88a03d56b9b
Robert Bruhn <79e6ef82@opayq.com>
2014-06-04 08:26:07 -0400

    Added Meta repo

---
+++ app/Biospex/Repo/Meta/MetaRepository.php
@@ -0,0 +1,39 @@
+<?php namespace Biospex\Repo\Meta;
+/**
+ * MetaRepository.php
+ *
+ * @package    Biospex Package
+ * @version    1.0
+ * @author     Robert Bruhn <bruhnrp@gmail.com>
+ * @license    GNU General Public License, version 3
+ * @copyright  (c) 2014, Biospex
+ * @link       http://biospex.org
+ *
+ * This file is part of Biospex.
+ * Biospex is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Biospex is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Biospex.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+use Biospex\Repo\Repository;
+use Meta;
+
+class MetaRepository extends Repository implements MetaInterface {
+
+    /**
+     * @param Meta $meta
+     */
+    public function __construct(Meta $meta)
+    {
+        $this->model = $meta;
+    }
+}
