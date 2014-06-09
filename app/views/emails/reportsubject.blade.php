7b566f7f703d4b5f921e14042c2992c9fe21eb7d
Robert Bruhn <79e6ef82@opayq.com>
2014-06-09 17:33:10 -0400

    Created subject import email

---
+++ app/views/emails/reportsubject.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+<h2>{{ trans('emails.import_complete') }}</h2>
+
+<p>{{ trans('emails.import_message') }}:</p>
+<p>
+    <blockquote>
+    {{{ trans('projects.project') }}}: {{{ $projectTitle }}}<br />
+    {{{ trans('emails.duplicates') }}}: {{{ $duplicateCount }}}
+    </blockquote>
+</p>
+<p>{{ trans('emails.explain_duplicates') }}</p>
+<p>{{ trans('emails.thank_you') }}, <br />
+    ~{{ trans('emails.signature') }}</p>
+</body>
+</html>
