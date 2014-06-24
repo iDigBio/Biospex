3501566ea9d62ee73b1824b0a652c14edea45e40
Robert Bruhn <79e6ef82@opayq.com>
2014-06-24 09:41:41 -0400

    Adding migrations for project_workflow and manager

---
+++ app/database/migrations/2014_06_18_160548_workflow_manager.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class WorkflowManager extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create('workflow_manager', function(Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('expedition_id');
+            $table->foreign('expedition_id')->references('id')->on('expeditions')->onDelete('cascade');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+
+            $table->engine = 'InnoDB';
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::drop('workflow_manager');
+	}
+
+}
