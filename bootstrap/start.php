5b3f1fdabc5ab96821c24c5084225fcd70af5221
Robert Bruhn <79e6ef82@opayq.com>
2014-06-25 11:01:08 -0400

    Renamed staging to develop

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 	'local' => array('vbdev'),
-    'staging' => array('idiginfo-test'),
+    'develop' => array('idiginfo-test'),
 ));
 
 /*
