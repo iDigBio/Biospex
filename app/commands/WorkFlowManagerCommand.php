5ba5d8b8ee8d2f01f4fb2cda8b677e5b2eada451
Robert Bruhn <79e6ef82@opayq.com>
2014-07-10 10:57:28 -0400

    Run testing

--- app/commands/WorkFlowManagerCommand.php
+++ app/commands/WorkFlowManagerCommand.php
@@ -93,7 +93,7 @@ class WorkFlowManagerCommand extends Command {
                     array(
                         'class' => $workflow->class,
                         'id' => $manager->workflow_id,
-                        'error' => $e->getTrace()
+                        'error' => var_export($e->getTrace(), true)
                     )));
                 $this->report->reportSimpleError();
                 continue;
