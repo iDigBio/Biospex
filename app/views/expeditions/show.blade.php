6b4d714def0118f1be26e6f9fc72da7f62c9a233
Robert Bruhn <79e6ef82@opayq.com>
2014-07-14 15:26:24 -0400

    Display changes

--- app/views/expeditions/show.blade.php
+++ app/views/expeditions/show.blade.php
@@ -10,7 +10,9 @@
 @section('content')
 <h3>{{ trans('projects.project') }}: <a href="{{ URL::route('groups.projects.show', [$groupId, $project->id]) }}">{{ $project->title }}</a></h3>
 <h4>{{ $expedition->title }}</h4>
-
+<div class="clearfix">
+<button class="btn btn-success pull-right" {{ isset($workflowManager->id) ? 'disabled="disabled"' : '' }} onClick="location.href='{{ action('process', [$groupId, $project->id, $expedition->id]) }}'">@lang('buttons.process')</button>
+</div>
 <div class="well clearfix">
     <div class="col-md-8">
         <p><strong>@lang('pages.description'):</strong> {{ $expedition->description }} </p>
@@ -19,10 +21,12 @@
         <p><strong>@lang('pages.expedition_download'):</strong> <a href="{{ URL::route('expeditionDownload', [$groupId, $project->id, $expedition->id, $expedition->download->id]) }}">{{ $expedition->download->file }}</a> </p>
         @endif
     </div>
-    <div class="col-md-4">
+    <div class="col-md-2">
         <p><em>@lang('pages.created'): {{ $expedition->created_at }}</em></p>
         <p><em>@lang('pages.updated'): {{ $expedition->updated_at }}</em></p>
-        <button class="btn btn-success" {{ isset($workflowManager->id) ? 'disabled="disabled"' : '' }} onClick="location.href='{{ action('process', [$groupId, $project->id, $expedition->id]) }}'">@lang('buttons.process')</button>
+    </div>
+    <div class="col-md-2">
+        <button class="btn btn-default" type="button" onClick="location.href='{{ URL::route('addData', [$project->group_id, $project->id]) }}'">@lang('buttons.data')</button>
         <button class="btn btn-primary" type="button" onClick="location.href='{{ URL::route('expedition-dup', [$groupId, $project->id, $expedition->id]) }}'">@lang('buttons.duplicate')</button>
         <button class="btn btn-warning" onClick="location.href='{{ action('ExpeditionsController@edit', [$groupId, $project->id, $expedition->id]) }}'">@lang('buttons.edit')</button>
         <button class="btn btn-default btn-danger action_confirm" href="{{ URL::route('groups.projects.expeditions.destroy', [$groupId, $project->id, $expedition->id]) }}" data-token="{{ Session::getToken() }}" data-method="delete">@lang('buttons.delete')</button></td>
