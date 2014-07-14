0e14263fd9b6dff3b18c63f19ee11b65052b931f
Robert Bruhn <79e6ef82@opayq.com>
2014-07-14 08:27:55 -0400

    Added soft delete

--- app/models/WorkflowManager.php
+++ app/models/WorkflowManager.php
@@ -23,6 +23,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Biospex.  If not, see <http://www.gnu.org/licenses/>.
  */
+use Illuminate\Database\Eloquent\SoftDeletingTrait;
 
 class WorkflowManager extends Eloquent {
 
