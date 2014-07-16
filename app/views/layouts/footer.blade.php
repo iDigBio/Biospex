9c156e9437f6703eac8ace9e344e47b66292d643
Jeremy Spinks <jspinks@fus.edu>
2014-07-16 13:50:27 -0400

    Adding footer

---
+++ app/views/layouts/footer.blade.php
@@ -0,0 +1,25 @@
+<div class="container" id="footer-home">
+    <!-- Notifications -->
+        <!-- ./ notifications -->
+
+    <!-- Content -->
+        <ul id="social-list">
+	        <li><a href="http://www.facebook.com/biospex" target="_blank"><img alt="Like iDigBio on Facebook" src="/assets/facebook.png" /></a></li>
+	        <li><a href="https://twitter.com/biospex" target="_blank"><img alt="Follow iDigbio on Twitter" src="/assets/twitter.png" /></a></li>
+        </ul>
+        	
+				
+		<ul id="logo-list">
+	        <li><a href="http://idigbio.org"><img alt="iDigBio logo" class="logo-center" src="/assets/idigbio.png" style="height: 60px; " /></a></li>
+	        <li><a href="http://ufl.edu"><img alt="University of Florida logo" class="logo-center" src="/assets/uf.png" style="width: 60px; height: 60px; " /></a></li>
+	        <li><a href="http://fsu.edu"><img alt="Florida State University logo" class="logo-center" src="/assets/fsu.png" style="width: 60px; height: 60px; " /></a></li>
+	        <li><a href="http://flmnh.ufl.edu"><img alt="Florida Museum logo" class="logo-center" src="/assets/flmnh.png" style="width: 60px; height: 60px; " /></a></li>
+	        <li><a href="http://nsf.gov"><img alt="National Science Foundation logo" class="logo-center" src="/assets/nsf.png" style="width: 60px; height: 60px; " /></a></li>
+        </ul>
+        
+        
+        <p class="small">iDigBio is funded by a grant from the National Science Foundation's Advancing Digitization of Biodiversity Collections Program (Cooperative Agreement EF-1115210). Any opinions, findings, and conclusions or recommendations expressed in this material are those of the author(s) and do not necessarily reflect the views of the National Science Foundation.
+</p>
+    <!-- ./ content -->
+</div>
+
