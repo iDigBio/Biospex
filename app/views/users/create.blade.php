10c2079593c001f752d7dca1ce11ea214767bd7b
Robert Bruhn <79e6ef82@opayq.com>
2014-07-17 10:43:57 -0400

    Added invite text field

--- app/views/users/create.blade.php
+++ app/views/users/create.blade.php
@@ -12,26 +12,27 @@
     <div class="col-md-4 col-md-offset-4">
         {{ Form::open(array('action' => 'UsersController@store')) }}
 
-            <h2>@lang('pages.register_account')</h2>
+        <h2>@lang('pages.register_account')</h2>
 
-            <div class="form-group {{ ($errors->has('email')) ? 'has-error' : '' }}">
-                {{ Form::text('email', null, array('class' => 'form-control', 'placeholder' => trans('pages.email'))) }}
-                {{ ($errors->has('email') ? $errors->first('email') : '') }}
-            </div>
+        <div class="form-group {{ ($errors->has('email')) ? 'has-error' : '' }}">
+            {{ Form::text('email', null, array('class' => 'form-control', 'placeholder' => trans('pages.email'))) }}
+            {{ ($errors->has('email') ? $errors->first('email') : '') }}
+        </div>
 
-            <div class="form-group {{ ($errors->has('password')) ? 'has-error' : '' }}">
-                {{ Form::password('password', array('class' => 'form-control', 'placeholder' => trans('pages.password'))) }}
-                {{ ($errors->has('password') ?  $errors->first('password') : '') }}
-            </div>
+        <div class="form-group {{ ($errors->has('password')) ? 'has-error' : '' }}">
+            {{ Form::password('password', array('class' => 'form-control', 'placeholder' => trans('pages.password'))) }}
+            {{ ($errors->has('password') ?  $errors->first('password') : '') }}
+        </div>
 
-            <div class="form-group {{ ($errors->has('password_confirmation')) ? 'has-error' : '' }}">
-                {{ Form::password('password_confirmation', array('class' => 'form-control', 'placeholder' => trans('pages.password_confirm'))) }}
-                {{ ($errors->has('password_confirmation') ?  $errors->first('password_confirmation') : '') }}
-            </div>
+        <div class="form-group {{ ($errors->has('password_confirmation')) ? 'has-error' : '' }}">
+            {{ Form::password('password_confirmation', array('class' => 'form-control', 'placeholder' => trans('pages.password_confirm'))) }}
+            {{ ($errors->has('password_confirmation') ?  $errors->first('password_confirmation') : '') }}
+        </div>
 
-            <div class="form-group {{ ($errors->has('group')) ? 'has-error' : '' }}">
-                {{ Form::text('group', null, array('id' => 'group', 'class' => 'typeahead form-control', 'placeholder' => 'Group')) }}
-            </div>
+        <div class="form-group {{ ($errors->has('invite')) ? 'has-error' : '' }}">
+            {{ Form::text('invite', array('class' => 'form-control', 'placeholder' => trans('pages.invite'))) }}
+            {{ ($errors->has('invite') ?  $errors->first('invite') : '') }}
+        </div>
 
         {{ Form::submit(trans('buttons.register'), array('class' => 'btn btn-primary')) }}
             
