65566037b0eb1f2087dd800e631f1ebb6133516f
Robert Bruhn <79e6ef82@opayq.com>
2014-07-18 10:43:05 -0400

    Fixed html error with invite field

--- app/views/users/create.blade.php
+++ app/views/users/create.blade.php
@@ -30,7 +30,7 @@
         </div>
 
         <div class="form-group {{ ($errors->has('invite')) ? 'has-error' : '' }}">
-            {{ Form::text('invite', array('class' => 'form-control', 'placeholder' => trans('pages.invite'))) }}
+            {{ Form::text('invite', null, array('class' => 'form-control', 'placeholder' => trans('pages.invite_code'))) }}
             {{ ($errors->has('invite') ?  $errors->first('invite') : '') }}
         </div>
 
