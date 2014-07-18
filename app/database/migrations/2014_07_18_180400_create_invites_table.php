149c3be2e75b2974d2e8c138129158cee45f2131
Robert Bruhn <79e6ef82@opayq.com>
2014-07-18 14:07:17 -0400

    Add invite migration

---
+++ app/database/migrations/2014_07_18_180400_create_invites_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInvitesTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invites', function(Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('group_id');
+            $table->foreign('group_id')->references('id')->on('groups')->onDelete('cascade');
+            $table->string('email');
+            $table->string('code')->index();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->engine = 'InnoDB';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('imports');
+    }
+
+}
