dd14a87dec6b48ed22f135bfb6ec4c5ea5e5b93f
Robert Bruhn <79e6ef82@opayq.com>
2014-08-04 09:58:50 -0400

    Updated notifications to use array

--- app/views/layouts/notifications.blade.php
+++ app/views/layouts/notifications.blade.php
@@ -1,31 +1,39 @@
-@if ($message = Session::get('success'))
+@if (Session::has('success'))
 <div class="alert alert-success alert-dismissable">
-  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-  <strong>@lang('pages.success'):</strong> {{ $message }}
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+    @foreach (Session::get('success') as $message)
+    <p><strong>@lang('pages.success'):</strong> {{ $message }}</p>
+    @endforeach
 </div>
 {{ Session::forget('success') }}
 @endif
 
-@if ($message = Session::get('error'))
+@if (Session::has('error'))
 <div class="alert alert-danger alert-dismissable">
-  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-  <strong>@lang('pages.error'):</strong> {{ $message }}
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+    @foreach (Session::get('error') as $message)
+    <p><strong>@lang('pages.error'):</strong> {{ $message }}</p>
+    @endforeach
 </div>
 {{ Session::forget('error') }}
 @endif
 
-@if ($message = Session::get('warning'))
+@if (Session::has('warning'))
 <div class="alert alert-warning alert-dismissable">
-  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-  <strong>@lang('pages.warning'):</strong> {{ $message }}
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+    @foreach (Session::get('warning') as $message)
+    <p><strong>@lang('pages.warning'):</strong> {{ $message }}</p>
+    @endforeach
 </div>
 {{ Session::forget('warning') }}
 @endif
 
-@if ($message = Session::get('info'))
+@if (Session::has('info'))
 <div class="alert alert-info alert-dismissable">
-  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-  <strong>@lang('pages.fyi'):</strong> {{ $message }}
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+    @foreach (Session::get('info') as $message)
+    <p><strong>@lang('pages.fyi'):</strong> {{ $message }}</p>
+    @endforeach
 </div>
 {{ Session::forget('info') }}
 @endif
