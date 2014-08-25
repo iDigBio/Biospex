dad7e59fe359249bf5c9b229ca3fbe06e5411b64
Robert Bruhn <79e6ef82@opayq.com>
2014-08-25 13:59:53 -0400

    Removed unnecessary input values and added cancel button

--- app/views/projects/edit.blade.php
+++ app/views/projects/edit.blade.php
@@ -27,11 +27,10 @@
 
     <div class="form-group">
         <div class="col-sm-offset-2 col-sm-10">
-            {{ Form::hidden('targetCount', $count, array('id' => 'targetCount')) }}
-            {{ Form::hidden('user_id', $project->user_id) }}
-            {{ Form::hidden('group_id', $project->group_id) }}
             {{ Form::hidden('id', $project->id) }}
+            {{ Form::hidden('targetCount', $count, array('id' => 'targetCount')) }}
             {{ Form::submit(trans('buttons.update'), array('class' => 'btn btn-primary'))}}
+            {{ Form::button(trans('buttons.cancel'), ['class' => 'btn btn-large btn-primary btn-danger', 'onClick' => "location.href='$cancel'"]) }}
         </div>
     </div>
     {{ Form::close()}}
