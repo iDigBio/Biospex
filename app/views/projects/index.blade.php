d75f5bfd0678ad90aa37027b2c0f8430e347e98f
Robert Bruhn <79e6ef82@opayq.com>
2014-08-25 14:29:31 -0400

    Added logic to not show Delete button unless group owner

--- app/views/projects/index.blade.php
+++ app/views/projects/index.blade.php
@@ -42,7 +42,9 @@
                             <button class="btn btn-default" type="button" onClick="location.href='{{ URL::route('addData', [$project->group_id, $project->id]) }}'">@lang('buttons.data')</button>
                             <button class="btn btn-primary" type="button" onClick="location.href='{{ URL::route('project-dup', [$project->group_id, $project->id]) }}'">@lang('buttons.duplicate')</button>
                             <button class="btn btn-warning" type="button" onClick="location.href='{{ URL::route('groups.projects.edit', [$project->group_id, $project->id]) }}'">@lang('buttons.edit')</button>
+                            @if ($user->id == $group->user_id || $isSuperUser)
                             <button class="btn btn-default btn-danger action_confirm" href="{{ URL::route('groups.projects.destroy', [$project->group_id, $project->id]) }}" data-token="{{ Session::getToken() }}" data-method="delete">@lang('buttons.delete')</button></td>
+                            @endif
                         <td></td>
                     </tr>
                     <tr>
