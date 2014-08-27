0e2a8f01889daae7da78d93a6ffb1bfbe9b84bc2
Robert Bruhn <79e6ef82@opayq.com>
2014-08-27 11:03:23 -0400

    Removed cancel variable and placed in template

--- app/controllers/ProjectsController.php
+++ app/controllers/ProjectsController.php
@@ -103,12 +103,11 @@ class ProjectsController extends BaseController {
      */
     public function create()
 	{
-		$cancel = URL::previous();
         $groups = ['' => '--Select--'] + $this->group->selectOptions(false);
         $count = is_null(Input::old('targetCount')) ? 0 : Input::old('targetCount');
         $create =  Route::currentRouteName() == 'projects.create' ? true : false;
 
-        return View::make('projects.create', compact('cancel', 'groups', 'count', 'create'));
+        return View::make('projects.create', compact('groups', 'count', 'create'));
 	}
 
     /**
@@ -162,9 +161,8 @@ class ProjectsController extends BaseController {
         $groups = ['' => '--Select--'] + $this->group->selectOptions(false);
         $count = is_null($project->target_fields) ? 0 : count($project->target_fields);
         $create =  Route::currentRouteName() == 'projects.create' ? true : false;
-		$cancel = URL::previous();
 
-        return View::make('projects.clone', compact('groups', 'project', 'count', 'create', 'cancel'));
+        return View::make('projects.clone', compact('groups', 'project', 'count', 'create'));
     }
 
     /**
@@ -179,9 +177,8 @@ class ProjectsController extends BaseController {
         $groups = ['' => '--Select--'] + $this->group->selectOptions(false);
         $count = is_null($project->target_fields) ? 0 : count($project->target_fields);
         $create =  Route::currentRouteName() == 'projects.create' ? true : false;
-		$cancel = URL::previous();
 
-        return View::make('projects.edit', compact('project', 'groups', 'count', 'create', 'cancel'));
+        return View::make('projects.edit', compact('project', 'groups', 'count', 'create'));
 	}
 
     /**
