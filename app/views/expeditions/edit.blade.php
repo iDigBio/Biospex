3ef53a88198f83f860e63ca86bb25d9d511cc6a8
Robert Bruhn <79e6ef82@opayq.com>
2014-08-27 11:26:02 -0400

    Fixed Expedition views and removed reference to groups. Added cancel buttons.

--- app/views/expeditions/edit.blade.php
+++ app/views/expeditions/edit.blade.php
@@ -9,11 +9,11 @@
 {{-- Content --}}
 @section('content')
 
-<h2>{{ $group->name }} : {{ $project->title }}</h2>
+<h2>{{ $project->title }}</h2>
 <h4>{{ trans('pages.edit') }} {{ trans('expeditions.expedition') }}</h4>
 <div class="well">
     {{ Form::open(array(
-    'action' => array('ExpeditionsController@update', $group->id, $project->id, $expedition->id),
+    'action' => array('ExpeditionsController@update', $project->id, $expedition->id),
     'method' => 'put',
     'class' => 'form-horizontal',
     'role' => 'form'
@@ -23,7 +23,6 @@
 
     <div class="form-group">
         <div class="col-sm-offset-2 col-sm-10">
-            {{ Form::hidden('group_id', $group->id) }}
             {{ Form::hidden('project_id', $project->id) }}
             {{ Form::hidden('id', $expedition->id) }}
             {{ Form::submit(trans('buttons.update'), array('class' => 'btn btn-primary'))}}
