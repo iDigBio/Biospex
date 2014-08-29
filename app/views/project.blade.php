be2d49b7cb8d936774077784b9b5ad150a985b26
Jeremy Spinks <jspinks@fus.edu>
2014-08-29 15:31:13 -0400

    Added project fields to layout for project page

--- app/views/project.blade.php
+++ app/views/project.blade.php
@@ -9,10 +9,10 @@
 {{-- Content --}}
 @section('content')
 
-<div id="banner" style="background: url(http://clientwebstage.com/idigbio/biospex/dev/images/herb-banner.jpg) top left no-repeat; height: 250px;">
+<div id="banner" style="background: url({{ $project->banner_file_name }}) top left no-repeat; height: 250px;">
     <div class="container">
         <div class="col-md-12">
-        <img src="http://clientwebstage.com/idigbio/biospex/dev/images/herb.jpg" alt="biospex" style="border: 5px solid #fff; margin-top: 100px; margin-left: -20px;"/>
+        <img src="{{ $project->logo_file_name }}" alt="{{ $project->title }}" style="border: 5px solid #fff; margin-top: 100px; margin-left: -20px;"/>
         </div>
         
         
@@ -27,15 +27,16 @@
 
         <!-- Content -->
         <div class="row">
-            <h1 class="banner">Florida Plant Hotspot Digitization Blitz</h1>
+            <h1 class="banner">{{ $project->title }}</h1>
                 
                 
             <div class="col-md-7">
                 
                  
-                <p class="description">Build a dataset for a biodiversity hotspot—help FSU’s Godfrey Herbarium digitize its local plant specimens.</p>
+                <p class="description">{{ $project->description_short }}</p>
                 
-                <p>The Florida State University’s Robert K. Godfrey Herbarium seeks to digitally image and database 100% of its current Florida specimens in a 24-month period starting May 1, 2014 using a combination of staff and volunteers.  The approach is purposefully groundbreaking, involving volunteers in all steps of the digitization process on- and off-site using new tools (e.g., Notes from Nature and BIOSPEX). The herbarium is the most extensive plant collection documenting plant diversity in the Florida panhandle—a national biodiversity hotspot with many very narrowly distributed plant species and subspecies.  As such, it represents an irreplaceable resource to researchers, natural resource managers, policy makers, and nature enthusiasts.  The data will be made available online through the Godfrey Herbarium website, iDigBio, and the Global Biodiversity Information Facility.</p>
+                {{ $project->description_long }}
+                
                 
                 <h2 style="color: #8dc63f; font-size: 18px; font-weight: bold; margin: 45px 0 10px 0;">How to Participate</h2>
                 
@@ -82,35 +83,35 @@
                 
                 <dl>
                 <dt class="firstdl">Managed by</dt>
-                <dd class="firstdl">The Florida State University’s Robert K. Godfrey Herbarium</dd>
+                <dd class="firstdl">{{ $project->managed }}</dd>
                 
                 <dt>Contact</dt>
-                <dd><a href="">Austin Mast</a></dd>
+                <dd><a href="mailto:{{ $project->contact_email }}">{{ $project->contact }}</a></dd>
                 
                 <dt>Website</dt>
-                <dd><a href="">http://herbarium.bio.fsu.edu</a></dd>
+                <dd><a href="{{ $project-> }}">{{ $project-> }}</a></dd>
                 
                 
                 <dt>Circumscription</dt>
-                <dd>The project targets approximately # specimens collected in Florida that have not yet been digitally imaged or databased to date.</dd>
+                <dd>{{ $project->circumscription }}</dd>
                 
                 <dt>Strategy</dt>
-                <dd>The Godfrey Herbarium will host a series of digital imaging blitzes onsite with a goal of producing 3000 images at each blitz.  These will be wrapped into 500-specimen “expeditions” with themes that make them interesting (e.g., all from swamp habitat) or lead to greater efficiencies (e.g., all from same collector) for online transcription via Notes from Nature and/or similar tools.</dd>
+                <dd>{{ $project->strategy }}</dd>
                 
                 <dt>Incentives</dt>
-                <dd>Volunteers who contribute 3 days onsite during the imaging blitzes or >500 online transcriptions will be sent a coffee mug or water bottle with the project logo on it.</dd>
+                <dd>{{ $project->incentives }}</dd>
                 
                 <dt>Geographic Scope</dt>
-                <dd>Florida, U.S.A.</dd>
+                <dd>{{ $project->geographic_scope }}</dd>
                 
                 <dt>Taxonomic Scope</dt>
-                <dd>Seed Plants</dd>
+                <dd>{{ $project->taxonomic_scope }}</dd>
                 
                 <dt>Temporal Scope</dt>
-                <dd>1860–present</dd>
+                <dd>{{ $project->temporal_scope }}</dd>
                 
                 <dt>Language Skills Required</dt>
-                <dd>English and perhaps occasionally Spanish</dd>
+                <dd>{{ $project->language_skills }}</dd>
                 
                 </dl>
                
