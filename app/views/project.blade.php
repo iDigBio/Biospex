8f097b4e879615231972d0fc71e645c7fa7cf154
Robert Bruhn <79e6ef82@opayq.com>
2014-09-05 11:32:26 -0400

    Remove image url test

--- app/views/project.blade.php
+++ app/views/project.blade.php
@@ -9,11 +9,10 @@
 {{-- Content --}}
 @section('content')
 
-<div id="banner" style="background: url( {{ $project->banner_file_name }} ) top left no-repeat; height: 250px;">
+<div id="banner" style="background: url( {{ $project->banner->url()) }} ) top left no-repeat; heig">
 	<div class="container">
 		<div class="col-md-12">
-			<img src="{{ $project->logo->url() }}" alt="{{ $project->title }}"
-				 style="border: 5px solid #fff; margin-top: 100px; margin-left: -20px;"/>
+			<img src="{{ $project->logo->url() }}" alt="{{ $project->title }}" style="border: 5px solid #fff; margin-top: 100px; margin-left: -20px;"/>
 		</div>
 	</div>
 </div>
