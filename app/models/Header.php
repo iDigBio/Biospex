87f4a44799b6af607b78134f8ee658d78cbf54cb
Robert Bruhn <79e6ef82@opayq.com>
2014-09-14 20:12:22 -0400

    Adde header model

---
+++ app/models/Header.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Header.php
+ *
+ * @package    Biospex Package
+ * @version    1.0
+ * @author     Robert Bruhn <bruhnrp@gmail.com>
+ * @license    GNU General Public License, version 3
+ * @copyright  (c) 2014, Biospex
+ * @link       http://biospex.org
+ *
+ * This file is part of Biospex.
+ * Biospex is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Biospex is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Biospex.  If not, see <http://www.gnu.org/licenses/>.
+ */
+use Illuminate\Database\Eloquent\SoftDeletingTrait;
+
+class Header extends Eloquent{
+
+	use SoftDeletingTrait;
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'headers';
+
+	/**
+	 * @var array
+	 */
+	protected $dates = ['deleted_at'];
+
+	/**
+	 * @var array
+	 */
+	protected $fillable = array(
+		'project_id',
+		'header'
+	);
+
+	/**
+	 * Array used by FactoryMuff to create Test objects
+	 */
+	public static $factory = array(
+		'project_id' => 'factory|Project',
+		'header' => 'text'
+	);
+
+	/**
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+	 */
+	public function project()
+	{
+		return $this->belongsTo('Project');
+	}
+
+	/**
+	 * Find header by project Id
+	 *
+	 * @param $id
+	 * @return \Illuminate\Database\Eloquent\Model|null|static
+	 */
+	public function getByProjectId($id)
+	{
+		return $this->where('project_id', '=', $id)->first();
+	}
+}
