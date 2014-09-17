c623771c98d4adadb4919b4478a3e9bd626bc34f
Robert Bruhn <79e6ef82@opayq.com>
2014-09-17 09:29:13 -0400

    Add text for welcome when no projects exist

---
+++ app/lang/en/welcome.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * welcome.php
+ *
+ * @package    Biospex Package
+ * @version    1.0
+ * @author     Robert Bruhn <bruhnrp@gmail.com>
+ * @license    GNU General Public License, version 3
+ * @copyright  (c) 2014, Biospex
+ * @link       http://biospex.org
+ *
+ * This file is part of Biospex.
+ * Biospex is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Biospex is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Biospex.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+return array(
+	'no_projects' => '<p>Welcome to Biospex!</p>
+					<p>Follow these instructions to create your first project.</p>
+					<p>A <b>project</b> contains the most complete set of data that will be sent out for transcription
+					and/or georeferencing as smaller <b>expeditions</b>. A project may contain tens of thousands of
+					records. We recommend dividing it into several smaller expeditions of hundreds or just a few
+					thousand that are more manageable for the digitization tools and more exciting for citizen
+					scientists. You may choose to separate the expeditions based on year, taxa, location or another
+					variable. You may also separate at random.</p>
+					<p>The first step in creating a project is to click <b>Create</b>, and provide the descriptive
+					information that is requested on the page that follows. The material you provide on this page will
+					be used when your data are sent out for transcription (Notes from Nature) and/or georeferencing
+					(GEOLocate) and will be seen by citizen scientists who will use this information to decide if
+					they are interested in and qualified to participate.</p>
+					<p>You are now ready to upload data to BIOSPEX. Click <b>Data</b> and follow the instructions to begin this
+					process. After you upload your first expedition, you can use the
+					<b>Clone</b> feature to upload additional expeditions with the same
+					descriptive information. The <b>Edit </b>and <b>Delete</b> features
+					are also available once you upload an expedition.</p>
+					<p>When you have uploaded your dataset,
+					you can then <b>Process</b> your project to send it off for
+					transcription or georeferencing. Follow the progress made by citizen
+					scientists from your Project page.
+					<p>Still have questions? Feel free to
+					contact us with any questions or concerns.</p>',
+);
