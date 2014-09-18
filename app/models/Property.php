d711c2b8ab6f747d909d32ecc6084d359e06dbf8
Robert Bruhn <79e6ef82@opayq.com>
2014-09-18 14:59:32 -0400

    Add Property model

---
+++ app/models/Property.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Properties.php
+ *
+ * @package    Biospex Package
+ * @version    1.0
+ * @author     Robert Bruhn <bruhnrp@gmail.com>
+ * @license    GNU General Public License, version 3
+ * @copyright  (c) 2014, Biospex
+ * @link       http://biospex.org
+ *
+ * This file is part of Biospex.
+ * Biospex is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Biospex is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Biospex.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+use Illuminate\Database\Eloquent\SoftDeletingTrait;
+
+class Property extends Eloquent{
+
+	use SoftDeletingTrait;
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'properties';
+
+	/**
+	 * @var array
+	 */
+	protected $dates = ['deleted_at'];
+
+	/**
+	 * @var array
+	 */
+	protected $fillable = array(
+		'qualified',
+		'short',
+		'ns'
+	);
+
+	/**
+	 * Array used by FactoryMuff to create Test objects
+	 */
+	public static $factory = array(
+		'qualified' => 'string',
+		'short' => 'string',
+		'ns' => 'string'
+	);
+}
