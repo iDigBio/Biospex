de2ce1f79aa4a6db0c472147d602b26851257677
Robert Bruhn <79e6ef82@opayq.com>
2014-09-18 14:46:00 -0400

    Create migration for properties

---
+++ app/database/migrations/2014_09_18_181514_create_properties_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePropertiesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('properties', function(Blueprint $table) {
+			$table->increments('id');
+			$table->string('qualified');
+			$table->string('short');
+			$table->string('ns');
+			$table->unique('qualified');
+			$table->unique('short');
+			$table->timestamps();
+
+			$table->engine = 'InnoDB';
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('properties');
+	}
+
+}
