04c84d1a91d0ad3c386bb710832aed341cd813d4
Robert Bruhn <79e6ef82@opayq.com>
2014-09-29 10:21:16 -0400

    Refactored code to work with new SubjectProcess

--- app/database/seeds/SubjectsDocsTableSeeder.php
+++ app/database/seeds/SubjectsDocsTableSeeder.php
@@ -26,17 +26,8 @@
 
 use Illuminate\Database\Seeder;
 use Biospex\Services\Subject\SubjectProcess;
-use Biospex\Services\Xml\XmlProcess;
-use Biospex\Repo\Property\PropertyInterface;
 
 class SubjectsDocsTableSeeder extends Seeder {
-
-    /**
-     * Default project Id
-     *
-     * @var int
-     */
-    protected $projectId = 1;
 
 	/**
 	 * Constructor
@@ -45,15 +36,10 @@ class SubjectsDocsTableSeeder extends Seeder {
 	 * @param XmlProcess $xmlProcess
 	 */
     public function __construct (
-		SubjectProcess $subjectProcess,
-		XmlProcess $xmlProcess,
-		PropertyInterface $property
+		SubjectProcess $subjectProcess
 	)
     {
         $this->subjectProcess = $subjectProcess;
-		$this->xmlProcess = $xmlProcess;
-		$this->property = $property;
-		$this->metaFile = Config::get('config.metaFile');
     }
 
     /**
@@ -68,82 +54,23 @@ class SubjectsDocsTableSeeder extends Seeder {
 		DB::table('properties')->truncate();
 		DB::table('headers')->truncate();
 		DB::table('subjects')->truncate();
-        DB::connection('mongodb')->collection('subjectsdocs')->delete();
+		DB::connection('mongodb')->collection('subjectsdocs')->truncate();
 
-		// Load xml
-		$xml = $this->xmlProcess->load('app/database/seeds/data/meta.xml');
+		$this->subjectProcess->processMetaFile('app/database/seeds/data/meta.xml');
 
-		// Set core type and file
-		$coreType = $this->xmlProcess->getDomTagAttribute('core', 'rowType');
-		$coreFile = $this->xmlProcess->getElementByTag('core');
+		$this->subjectProcess->setProjectId(1);
 
-		// Set delimited and project id
-		$this->subjectProcess->setDelimiter($this->xmlProcess->getDomTagAttribute('core', 'fieldsTerminatedBy'));
-		$this->subjectProcess->setProjectId($this->projectId);
+		$multiMediaFile = $this->subjectProcess->getMultiMediaFile();
+		$occurrenceFile = $this->subjectProcess->getOccurrenceFile();
 
-		if (preg_match('/occurrence/i', $coreType))
-		{
-			$this->subjectProcess->setCore(false);
-			$this->subjectProcess->setOccurrenceFile($coreFile);
+		$multimedia = $this->subjectProcess->loadCsv("app/database/seeds/data/$multiMediaFile", "multimedia");
+		$occurrence = $this->subjectProcess->loadCsv("app/database/seeds/data/$occurrenceFile", "occurrence");
 
-			$multiMediaQuery = $this->xmlProcess->queryOne("//ns:extension[contains(@rowType, '{$this->metaFile['multimediaFile']}')]");
-			$this->subjectProcess->setMultiMediaFile($multiMediaQuery->nodeValue);
-
-			$multiMediaIndexQuery = $this->xmlProcess->queryOne("//ns:field[contains(@term, '{$this->metaFile['identifier']}')]");
-			$identifier = $multiMediaIndexQuery->attributes->getNamedItem("index")->nodeValue;
-
-			$xpath_query = "/ns:archive/ns:extension[contains(@rowType, '{$this->metaFile['multimediaFile']}')]/ns:field";
-		}
-		elseif (preg_match('/multimedia/', $coreType))
-		{
-			$this->subjectProcess->setCore(true);
-			$this->subjectProcess->setMultiMediaFile($coreFile);
-
-			$occurrenceQuery = $this->xmlProcess->queryOne("//ns:extension[contains(@rowType, '{$this->metaFile['occurrenceFile']}')]");
-			$this->subjectProcess->setOccurrenceFile($occurrenceQuery->nodeValue);
-			$identifier = null;
-
-			$xpath_query = "/ns:archive/ns:extension[contains(@rowType, '{$this->metaFile['occurrenceFile']}')]/ns:field";
-		}
-
-        $multiMediaFile = $this->subjectProcess->getMultiMediaFile();
-        $occurrenceFile = $this->subjectProcess->getOccurrenceFile();
-
-        $multimedia = $this->subjectProcess->loadCsv("app/database/seeds/data/$multiMediaFile", 'multimedia');
-        $occurrence = $this->subjectProcess->loadCsv("app/database/seeds/data/$occurrenceFile", 'occurrence');
-
-		// Build fields array using index and qualified name
-		$multimediaHeader = $this->subjectProcess->getMultiMediaHeader();
-		$fields = array();
-		$fields[0] = $multimediaHeader[0];
-		foreach($this->xmlProcess->xpathQuery($xpath_query) as $child)
-		{
-			$index = $child->attributes->getNamedItem("index")->nodeValue;
-			$url = $child->attributes->getNamedItem("term")->nodeValue;
-			list($namespace, $short) = preg_match('/:/', $multimediaHeader[$index]) ?
-				preg_split('/:/', $multimediaHeader[$index]) : array('', $multimediaHeader[$index]);
-			$array = array(
-				'qualified' => $url,
-				'short' => $short,
-				'namespace' => $namespace,
-			);
-			try{
-				$this->property->create($array);
-			}
-			catch(Exception $e)
-			{
-				die($e->getMessage());
-			}
-
-			$fields[$index] = $short;
-			$dbArray = null;
-		}
-
-		$this->subjectProcess->setMultiMediaIdentifier($identifier);
-		$this->subjectProcess->setHeaderArray($fields);
+		$this->subjectProcess->setHeaderArray();
 
 		$subjects = $this->subjectProcess->buildSubjectsArray($multimedia, $occurrence);
 
-        $this->subjectProcess->insertDocs($subjects);
+		$this->subjectProcess->insertDocs($subjects);
+
     }
 }
