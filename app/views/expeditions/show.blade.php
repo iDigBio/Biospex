eb1013468f84aa22e1a9a06a9a3f71737b20dbbe
Robert Bruhn <79e6ef82@opayq.com>
2014-10-17 14:04:37 -0400

    Added download button if a download exists. gh-16

--- app/views/expeditions/show.blade.php
+++ app/views/expeditions/show.blade.php
@@ -10,7 +10,7 @@
 @section('content')
 
     <ul class="breadcrumb">
-    <li><a href="{{ URL::route('projects.show', [$project->id]) }}">{{ $project->title }}</a></li>
+    <li><a href="{{ URL::route('projects.show', [$expedition->project->id]) }}">{{ $expedition->project->title }}</a></li>
     <li>@lang('pages.created'): {{ $expedition->created_at }}</li>
     <li>@lang('pages.updated'): {{ $expedition->updated_at }}</li>
     </ul>
@@ -24,24 +24,22 @@
 
 <div class="panel panel-default">
     <div style="padding: 10px;">
-    @if ( ! empty($expedition->download) && file_exists($filepath))
-        <p class="eyesright"><strong>@lang('pages.expedition_download'):</strong> <a href="{{ URL::route('projects.expeditions.download', [$project->id, $expedition->id, $expedition->download->id]) }}">{{ $expedition->download->file }}</a> </p>
+    @if ( ! $expedition->download->isEmpty())
+        <button title="@lang('buttons.downloadTitle')" class="btn btn-success btn-xs" type="button" onClick="location.href='{{ action('ExpeditionsController@download', [$expedition->project->id, $expedition->id]) }}'"><span class="glyphicon glyphicon-file"></span> @lang('buttons.download') </button>
     @endif
-    
-    <button title="@lang('buttons.dataTitle')" class="btn btn-default btn-xs" type="button" onClick="location.href='{{ URL::route('projects.data', [$project->id]) }}'"><span class="glyphicon glyphicon-plus-sign"></span> @lang('buttons.data')</button>
-    <button title="@lang('buttons.duplicateTitle')" class="btn btn-primary btn-xs" type="button" onClick="location.href='{{ URL::route('projects.expeditions.duplicate', [$project->id, $expedition->id]) }}'"><span class="glyphicon glyphicon-share-alt"></span> @lang('buttons.duplicate')</button>
-    <button title="@lang('buttons.editTitle')" class="btn btn-warning btn-xs" onClick="location.href='{{ URL::route('projects.expeditions.edit', [$project->id, $expedition->id]) }}'"><span class="glyphicon glyphicon-cog"></span> @lang('buttons.edit')</button>
-    <button title="@lang('buttons.deleteTitle')" class="btn btn-default btn-danger action_confirm btn-xs" href="{{ URL::route('projects.expeditions.destroy', [$project->id, $expedition->id]) }}" data-token="{{ Session::getToken() }}" data-method="delete"><span class="glyphicon glyphicon-remove-circle"></span> @lang('buttons.delete')</button></td>
-    
+    <button title="@lang('buttons.dataTitle')" class="btn btn-default btn-xs" type="button" onClick="location.href='{{ action('ProjectsController@data', [$expedition->project->id]) }}'"><span class="glyphicon glyphicon-plus-sign"></span> @lang('buttons.data')</button>
+    <button title="@lang('buttons.duplicateTitle')" class="btn btn-primary btn-xs" type="button" onClick="location.href='{{ action('ExpeditionsController@duplicate', [$expedition->project->id, $expedition->id]) }}'"><span class="glyphicon glyphicon-share-alt"></span> @lang('buttons.duplicate')</button>
+    <button title="@lang('buttons.editTitle')" class="btn btn-warning btn-xs" onClick="location.href='{{ action('ExpeditionsController@edit', [$expedition->project->id, $expedition->id]) }}'"><span class="glyphicon glyphicon-cog"></span> @lang('buttons.edit')</button>
+    <button title="@lang('buttons.deleteTitle')" class="btn btn-default btn-danger action_confirm btn-xs" href="{{ action('ExpeditionsController@destroy', [$expedition->project->id, $expedition->id]) }}" data-token="{{ Session::getToken() }}" data-method="delete"><span class="glyphicon glyphicon-remove-circle"></span> @lang('buttons.delete')</button>
     </div>
 </div>
 
 
 <div class="clearfix">
-@if (is_null($workflowManager))
-	<button title="@lang('buttons.processTitle')" class="btn btn-success pull-right" onClick="location.href='{{ action('projects.expeditions.process', [$project->id, $expedition->id]) }}'"><span class="glyphicon glyphicon-play"></span> @lang('buttons.process')</button>
+@if ($expedition->workflowManager->isEmpty())
+	<button title="@lang('buttons.processTitle')" class="btn btn-success pull-right" onClick="location.href='{{ action('ExpeditionsController@process', [$expedition->project->id, $expedition->id]) }}'"><span class="glyphicon glyphicon-play"></span> @lang('buttons.process')</button>
 @else
-	<button title="@lang('buttons.stopTitle')" class="btn btn-default btn-danger pull-right action_confirm" href="{{ URL::route('projects.expeditions.stop', [$project->id, $expedition->id]) }}" data-token="{{ Session::getToken() }}" data-method="delete"><span class="glyphicon glyphicon-stop"></span> @lang('buttons.stop')</button></td>
+	<button title="@lang('buttons.stopTitle')" class="btn btn-default btn-danger pull-right action_confirm" href="{{ action('ExpeditionsController@stop', [$expedition->project->id, $expedition->id]) }}" data-token="{{ Session::getToken() }}" data-method="delete"><span class="glyphicon glyphicon-stop"></span> @lang('buttons.stop')</button></td>
 @endif
 </div>
 
