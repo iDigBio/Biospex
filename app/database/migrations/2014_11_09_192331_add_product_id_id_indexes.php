4249af8c0f663c69d51476166b1559a60bd0b348
Robert Bruhn <79e6ef82@opayq.com>
2014-11-09 21:37:21 -0500

    Add new indexes for MongoDb

---
+++ app/database/migrations/2014_11_09_192331_add_product_id_id_indexes.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddProductIdIdIndexes extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::connection('mongodb')->collection('subjectdocs', function(Blueprint $collection)
+		{
+			$collection->index('id');
+			$collection->unique(['project_id', 'id']);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::connection('mongodb')->collection('subjectdocs', function(Blueprint $collection)
+		{
+			$collection->dropIndex('id');
+			$collection->dropIndex(['project_id', 'id']);
+		});
+	}
+
+}
