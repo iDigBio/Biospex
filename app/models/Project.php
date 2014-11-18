8f05309f6b3c81fb37df2deb2d57908dac607334
Robert Bruhn <79e6ef82@opayq.com>
2014-11-18 13:15:53 -0500

    Update to dev version

--- app/models/Project.php
+++ app/models/Project.php
@@ -23,7 +23,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Biospex.  If not, see <http://www.gnu.org/licenses/>.
  */
-use Jenssegers\Mongodb\Model as Eloquent;
+use Jenssegers\Eloquent\Model as Eloquent;
 use Codesleeve\Stapler\ORM\EloquentTrait;
 use Codesleeve\Stapler\ORM\StaplerableInterface;
 use Illuminate\Database\Eloquent\SoftDeletingTrait;
