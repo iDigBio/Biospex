0db8558d94ea0d6f99d9f047459d79bec9a044bc
Robert Bruhn <79e6ef82@opayq.com>
2014-12-02 09:22:50 -0500

    Replaces spaces in url when using curl

--- app/Biospex/Services/Image/Image.php
+++ app/Biospex/Services/Image/Image.php
@@ -177,7 +177,7 @@ class Image {
 	public function getImageFromUrl ($url)
 	{
 		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_URL, str_replace(" ", "%20", $url));
 		curl_setopt($ch, CURLOPT_HEADER, 0);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_BINARYTRANSFER, 1);
