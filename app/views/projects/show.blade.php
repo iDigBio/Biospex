a47f64850842bb4418fa1ca2b87bd615a81eedba
Robert Bruhn <79e6ef82@opayq.com>
2014-12-05 08:27:03 -0500

    Test memory limit

--- app/views/projects/show.blade.php
+++ app/views/projects/show.blade.php
@@ -52,7 +52,7 @@
             <td>{{ $expedition->title }}</td>
             <td>{{ $expedition->description }}</td>
             <td>{{ $expedition->created_at }}</td>
-            <td>{{  }}</td>
+            <td></td>
             @if( ! $expedition->actors->isEmpty())
             <td>0</td>
             <td>0</td>
