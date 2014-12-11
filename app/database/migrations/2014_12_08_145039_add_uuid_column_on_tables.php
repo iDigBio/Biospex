f57139166f91f7751a3f54889bb0689c64882ce0
Robert Bruhn <79e6ef82@opayq.com>
2014-12-11 13:47:30 -0500

    REdo of the uuids. Only makng columns on projects and expeditions.

--- app/database/migrations/2014_12_08_145039_add_uuid_column_on_tables.php
+++ app/database/migrations/2014_12_08_145039_add_uuid_column_on_tables.php
@@ -12,51 +12,8 @@ class AddUuidColumnOnTables extends Migration {
 	 */
 	public function up()
 	{
-		Schema::table('projects', function(Blueprint $table)
-		{
-			$table->char('uuid', 36)->after('id');
-		});
-
-		Schema::table('expeditions', function(Blueprint $table)
-		{
-			$table->char('uuid', 36)->after('id');
-			$table->char('project_uuid', 36)->after('uuid');
-		});
-
-		Schema::table('headers', function(Blueprint $table)
-		{
-			$table->char('project_uuid', 36)->after('id');
-		});
-
-		Schema::table('imports', function(Blueprint $table)
-		{
-			$table->char('project_uuid', 36)->after('id');
-		});
-
-		Schema::table('metas', function(Blueprint $table)
-		{
-			$table->char('project_uuid', 36)->after('id');
-		});
-
-		Schema::table('project_actor', function(Blueprint $table)
-		{
-			$table->char('project_uuid', 36)->after('id');
-		});
-
-		Schema::table('downloads', function(Blueprint $table)
-		{
-			$table->char('expedition_uuid', 36)->after('id');
-		});
-
-		Schema::table('expedition_actor', function(Blueprint $table)
-		{
-			$table->char('expedition_uuid', 36)->after('id');
-		});
-
-		Schema::table('workflow_manager', function(Blueprint $table)
-		{
-			$table->char('expedition_uuid', 36)->after('id');
-		});
+		DB::statement("ALTER TABLE projects ADD uuid BINARY(16) NULL AFTER id");
+		DB::statement("ALTER TABLE expeditions ADD uuid BINARY(16) NULL AFTER id");
 	}
 
 	/**
@@ -74,42 +31,6 @@ class AddUuidColumnOnTables extends Migration {
 		Schema::table('expeditions', function(Blueprint $table)
 		{
 			$table->dropColumn('uuid');
-			$table->dropColumn('project_uuid');
-		});
-
-		Schema::table('headers', function(Blueprint $table)
-		{
-			$table->dropColumn('project_uuid');
-		});
-
-		Schema::table('imports', function(Blueprint $table)
-		{
-			$table->dropColumn('project_uuid');
-		});
-
-		Schema::table('metas', function(Blueprint $table)
-		{
-			$table->dropColumn('project_uuid');
-		});
-
-		Schema::table('project_actor', function(Blueprint $table)
-		{
-			$table->dropColumn('project_uuid');
-		});
-
-		Schema::table('downloads', function(Blueprint $table)
-		{
-			$table->dropColumn('expedition_uuid');
-		});
-
-		Schema::table('expedition_actor', function(Blueprint $table)
-		{
-			$table->dropColumn('expedition_uuid');
-		});
-
-		Schema::table('workflow_manager', function(Blueprint $table)
-		{
-			$table->dropColumn('expedition_uuid');
 		});
 	}
 
