99a923b55ab62b5820c6a29adfae3da36d07140c
Robert Bruhn <79e6ef82@opayq.com>
2014-12-21 18:38:15 -0500

    Fixed issues with grid and altered breadcrumbs

--- app/views/subjects/show.blade.php
+++ app/views/subjects/show.blade.php
@@ -7,7 +7,7 @@
 
 {{-- Content --}}
 @section('content')
-    {{ Breadcrumbs::render('projects.show', $project) }}
+    {{ Breadcrumbs::render('projects.subjects', $project) }}
     <div class="jumbotron">
         <h4>Project:</h4>
         <h2>{{ $project->title }}</h2>
@@ -17,11 +17,11 @@
         <div style="padding: 10px;">
             <p class="eyesright"><strong>@lang('pages.project_url'):</strong> {{ HTML::linkAction('HomeController@project', $project->title, [$project->slug]) }} </p>
             <button title="@lang('buttons.dataTitle')" class="btn btn-default btn-xs" type="button" onClick="location.href='{{ URL::route('projects.data', [$project->id]) }}'"><span class="glyphicon glyphicon-plus-sign"></span> @lang('buttons.data')</button>
-            <button title="@lang('buttons.editTitle')" class="btn btn-warning btn-xs" type="button" onClick="location.href='{{ URL::route('projects.edit', [$project->id]) }}'"><span class="glyphicon glyphicon-cog"></span> @lang('buttons.edit')</button>
         </div>
     </div>
     <div class="table-responsive" id="jqtable">
         <input type="hidden" id="projectId" value="{{ $project->id }}">
+        <input type="hidden" id="expeditionId" value="0">
         <table id="list"></table>
         <div id="pager"></div>
     </div>
