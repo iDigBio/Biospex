42a18224aa2e2d1c69523a5fc731a715790740d3
Robert Bruhn <79e6ef82@opayq.com>
2015-01-07 10:08:35 -0500

    Add queue column to workflow manager

---
+++ app/database/migrations/2015_01_07_100410_add_queue_to_workflow_manager.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddQueueToWorkflowManager extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('workflow_manager', function (Blueprint $table)
+		{
+			$table->tinyInteger('queue')->index()->default(0);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('workflow_manager', function (Blueprint $table)
+		{
+			$table->dropColumn('queue');
+		});
+	}
+
+}
