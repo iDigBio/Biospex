82c3501c9701b0a88ede59cbf42fa8070e489006
Robert Bruhn <79e6ef82@opayq.com>
2015-01-14 10:18:03 -0500

    Fixe error in namespace

--- app/Biospex/Services/Queue/OcrService.php
+++ app/Biospex/Services/Queue/OcrService.php
@@ -23,6 +23,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Biospex.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 use Biospex\Services\Report\Report;
 
 class OcrService {
