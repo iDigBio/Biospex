01efb8a3a4ac1e20d3850d6a66f5e7d8df170eeb
Robert Bruhn <79e6ef82@opayq.com>
2015-01-21 10:03:17 -0500

    Add uuid trait

--- app/models/OcrQueue.php
+++ app/models/OcrQueue.php
@@ -23,7 +23,11 @@
  * You should have received a copy of the GNU General Public License
  * along with Biospex.  If not, see <http://www.gnu.org/licenses/>.
  */
+use Biospex\Traits\UuidTrait;
+
 class OcrQueue extends Eloquent {
+
+	use UuidTrait;
 
 	/**
 	 * The database table used by the model.
