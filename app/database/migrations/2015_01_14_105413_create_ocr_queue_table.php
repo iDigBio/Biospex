e5dd98be0fd784963aae4874bf9e46c008d0108a
Robert Bruhn <79e6ef82@opayq.com>
2015-01-21 12:20:13 -0500

    Add migration for ocr queue table

---
+++ app/database/migrations/2015_01_14_105413_create_ocr_queue_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOcrQueueTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('ocr_queue', function(Blueprint $table) {
+			$table->increments('id');
+			$table->string('status', 30)->nullable();
+			$table->text('message')->nullable();
+			$table->timestamps();
+
+			$table->engine = 'InnoDB';
+		});
+
+		DB::statement("ALTER TABLE ocr_queue ADD data LONGBLOB AFTER id");
+		DB::statement("ALTER TABLE ocr_queue ADD uuid BINARY(16) NULL AFTER id");
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('ocr_queue');
+	}
+
+}
