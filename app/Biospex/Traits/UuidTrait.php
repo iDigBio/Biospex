fc8076144a49193af7661ccb63f8db88b9616cd3
Robert Bruhn <79e6ef82@opayq.com>
2015-01-22 10:36:29 -0500

    Added attributes to uuid trait

--- app/Biospex/Traits/UuidTrait.php
+++ app/Biospex/Traits/UuidTrait.php
@@ -38,4 +38,29 @@ trait UuidTrait
 			$model->uuid = Uuid::uuid4()->__toString();
 		});
 	}
+
+	/**
+	 * Set uuid for binary storage.
+	 *
+	 * @param $value
+	 */
+	public function setUuidAttribute($value)
+	{
+		$this->attributes['uuid'] = pack('H*', str_replace('-', '', $value));
+	}
+
+	/**
+	 * Return uuid in normal format.
+	 *
+	 * @param $value
+	 * @return string
+	 */
+	public function getUuidAttribute($value)
+	{
+		if (is_null($value))
+			return;
+
+		$uuid = bin2hex($value);
+		return substr($uuid, 0, 8) . '-' . substr($uuid, 8, 4) . '-' . substr($uuid, 12, 4) . '-' . substr($uuid, 16, 4) . '-' . substr($uuid, 20);
+	}
 }
