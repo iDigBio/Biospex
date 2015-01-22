d09d0d788a779b39c0ab2e7b00341db5fea6409d
Robert Bruhn <79e6ef82@opayq.com>
2015-01-22 10:35:23 -0500

    Moved uuid attributes to the uuid trait

--- app/models/Project.php
+++ app/models/Project.php
@@ -191,31 +191,6 @@ class Project extends Eloquent implements StaplerableInterface, SluggableInterfa
 		return $this->where('uuid', pack('H*', str_replace('-', '', $uuid)))->get();
 	}
 
-	/**
-	 * Set uuid for binary storage.
-	 *
-	 * @param $value
-	 */
-	public function setUuidAttribute($value)
-	{
-		$this->attributes['uuid'] = pack('H*', str_replace('-', '', $value));
-	}
-
-	/**
-	 * Return uuid in normal format.
-	 *
-	 * @param $value
-	 * @return string
-	 */
-	public function getUuidAttribute($value)
-	{
-		if (is_null($value))
-			return;
-
-		$uuid = bin2hex($value);
-		return substr($uuid, 0, 8) . '-' . substr($uuid, 8, 4) . '-' . substr($uuid, 12, 4) . '-' . substr($uuid, 16, 4) . '-' . substr($uuid, 20);
-	}
-
     /**
      * Mutator for target_fields
      *
