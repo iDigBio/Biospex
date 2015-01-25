41faf7c3a0c3fff971498875a3e2295eeec20c6a
Robert Bruhn <79e6ef82@opayq.com>
2015-01-25 12:33:24 -0500

    Add project id to ocr_queue table

--- app/database/migrations/2015_01_14_105413_create_ocr_queue_table.php
+++ app/database/migrations/2015_01_14_105413_create_ocr_queue_table.php
@@ -14,18 +14,19 @@ class CreateOcrQueueTable extends Migration {
 	{
 		Schema::create('ocr_queue', function(Blueprint $table) {
 			$table->increments('id');
+			$table->unsignedInteger('project_id');
 			$table->string('status', 30)->nullable();
 			$table->integer('subject_count')->default(0);
 			$table->tinyInteger('tries')->default(0);
 			$table->tinyInteger('error')->default(0);
 			$table->text('message')->nullable();
 			$table->timestamps();
-
+			$table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
 			$table->engine = 'InnoDB';
 		});
 
-		DB::statement("ALTER TABLE ocr_queue ADD data LONGBLOB AFTER id");
-		DB::statement("ALTER TABLE ocr_queue ADD uuid BINARY(16) NULL AFTER id");
+		DB::statement("ALTER TABLE ocr_queue ADD data LONGBLOB AFTER project_id");
+		DB::statement("ALTER TABLE ocr_queue ADD uuid BINARY(16) NULL AFTER project_id");
 	}
 
 	/**
