e26121239bd4dac0bce56aede44917e2cc601416
Robert Bruhn <79e6ef82@opayq.com>
2015-01-29 12:16:27 -0500

    Update test command

--- app/commands/TestCommand.php
+++ app/commands/TestCommand.php
@@ -1,10 +1,6 @@
 <?php
 
 use Illuminate\Console\Command;
-use Biospex\Repo\OcrQueue\OcrQueueInterface;
-use Biospex\Repo\Subject\SubjectInterface;
-use Biospex\Services\Report\Report;
-use Illuminate\Events\Dispatcher;
 
 class TestCommand extends Command {
 
@@ -25,49 +21,16 @@ class TestCommand extends Command {
 	 * @param SubjectInterface $subject
 	 * @param Report $report
 	 */
-	public function __construct (
-		Report $report,
-		Dispatcher $events
-	)
+	public function __construct ()
 	{
 		parent::__construct();
-		$this->events = $events;
 	}
 
 	/**
 	 * Fire queue.
-	 *
-	 * @param $job
-	 * @param $data
 	 */
 	public function fire ()
 	{
 
-		$this->events->fire('user.newpassword', [
-			'email' => "biospex@gmail.com",
-			'newPassword' => "asfdfasasfa"
-		]);
-
-
-		/*
-		$data = array(
-			'projectTitle' => "Testing Email",
-			'mainMessage' => trans('projects.ocr_complete'),
-		);
-		$subject = trans('emails.ocr_complete');
-		$view = 'emails.reportocr';
-		*/
-		//$this->fireEvent('user.sendreport', 'biospex@gmail.com', $subject, $view, $data);
-
-		return;
-	}
-	protected function fireEvent ($event, $email, $subject, $data, $attachments = array())
-	{
-		\Event::fire($event, [
-			'email' => $email,
-			'subject' => $subject,
-			'data' => $data,
-			'attachment' => $attachments
-		]);
 	}
 }
