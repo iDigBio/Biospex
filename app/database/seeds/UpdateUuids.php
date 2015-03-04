27f13fa94b371bca9219d07237ef6a20a3a569a7
Robert <bruhnrp@gmail.com>
2015-03-04 09:34:41 -0500

    Remove database seeds to protect production.

---
+++ app/database/seeds/UpdateUuids.php
@@ -0,0 +1,45 @@
+<?php
+/*
+project_id
+	Projects
+	Expeditions
+	Headers
+	Imports
+	Metas
+	Project_Actor
+
+
+expedition_id
+	Downloads
+	Expedition_Actor
+	Workflow_Manager
+
+ */
+use Rhumsaa\Uuid\Uuid;
+
+class UpdateUuids extends Seeder {
+
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run ()
+	{
+		Eloquent::unguard();
+
+		$projects = Project::withTrashed()->get();
+		foreach ($projects as $project)
+		{
+			$project->uuid = is_null($project->uuid) ? Uuid::uuid4()->__toString() : $project->uuid;
+			$project->save();
+		}
+
+		$expeditions = Expedition::withTrashed()->get();
+		foreach($expeditions as $expedition)
+		{
+			$expedition->uuid = empty($expedition->uuid) ? Uuid::uuid4()->__toString() : $expedition->uuid;
+			$expedition->save();
+		}
+	}
+}
