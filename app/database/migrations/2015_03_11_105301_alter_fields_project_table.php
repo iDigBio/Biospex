a1ef00ad0053e424268b9fa0e1a864ae740acab1
Robert <bruhnrp@gmail.com>
2015-03-11 11:41:18 -0400

    Move add fields to new migration

--- app/database/migrations/2015_03_11_105301_alter_fields_project_table.php
+++ app/database/migrations/2015_03_11_105301_alter_fields_project_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class AddProjectFieldChanges extends Migration {
+class AlterFieldsProjectTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -17,12 +17,6 @@ class AddProjectFieldChanges extends Migration {
             $table->renameColumn('website', 'organization_website');
             $table->renameColumn('managed', 'organization');
             $table->renameColumn('hashtag', 'twitter');
-
-            $table->string('facebook')->after('keywords');
-            $table->string('project_partners')->after('organization');
-            $table->string('funding_source')->after('project_partners');
-
-
 		});
 	}
 
@@ -39,10 +33,6 @@ class AddProjectFieldChanges extends Migration {
             $table->renameColumn('organization_website', 'website');
             $table->renameColumn('organization', 'managed');
             $table->renameColumn('twitter', 'hashtag');
-
-            $table->dropColumn('facebook');
-            $table->dropColumn('project_partners');
-            $table->dropColumn('funding_source');
 		});
 	}
 
