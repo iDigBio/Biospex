0ddb186076ef6a556ef36b1266254d17075620c7
Robert <bruhnrp@gmail.com>
2015-03-16 10:23:59 -0400

    Changed errors reference to emails

--- app/views/emails/report-simple-error.blade.php
+++ app/views/emails/report-simple-error.blade.php
@@ -4,11 +4,11 @@
     <meta charset="utf-8">
 </head>
 <body>
-<h2>{{ trans('errors.error') }}</h2>
+<h2>{{ trans('emails.error') }}</h2>
 
 <p>
 <blockquote>
-{{ trans('errors.error_message') }}<br />
+{{ trans('emails.error_message') }}<br />
 {{{ $errorMessage }}}
 </blockquote>
 </p>
