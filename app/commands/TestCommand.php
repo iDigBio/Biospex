67ca0b859ed3dcada1d395b1ef9f04641d0b0f04
Robert <bruhnrp@gmail.com>
2015-03-27 16:30:03 -0400

    Run test

--- app/commands/TestCommand.php
+++ app/commands/TestCommand.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Console\Command;
+use Biospex\Services\Image\Image;
+use Illuminate\Filesystem\Filesystem;
+use Biospex\Services\Curl\Curl;
 
 class TestCommand extends Command {
 
@@ -17,9 +20,11 @@ class TestCommand extends Command {
     /**
      * Constructor
      */
-    public function __construct()
+    public function __construct(Image $image, Filesystem $filesystem)
     {
         parent::__construct();
+        $this->image = $image;
+        $this->filesystem = $filesystem;
     }
 
     /**
@@ -27,6 +32,24 @@ class TestCommand extends Command {
      */
     public function fire()
     {
+        $url = "http://herbarium.bio.fsu.edu/showimage.php?Image=images/herbarium/jpegs/000016295.jpg";
+
+        $rc = new Curl([$this, "save"]);
+        $rc->options = [CURLOPT_RETURNTRANSFER => 1, CURLOPT_FOLLOWLOCATION => 1, CURLINFO_HEADER_OUT => 1];
+        $rc->window_size = 1;
+        $rc->get($url);
+        $rc->execute();
+
+        return;
+    }
+
+    public function save($image, $info)
+    {
+        $this->filesystem->put(storage_path() . "/test.jpg", $image);
+        $this->image->imageMagick(storage_path() . "/test.jpg");
+        $this->image->resize(storage_path() . "/testsm.jpg", 50, 0);
+        $this->image->destroy();
+
         return;
     }
 }
