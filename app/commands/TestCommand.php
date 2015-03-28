7adb7cd4c73c08737b5edff169116b26ad345d28
Robert <bruhnrp@gmail.com>
2015-03-28 19:55:58 -0400

    Reverted code.

--- app/commands/TestCommand.php
+++ app/commands/TestCommand.php
@@ -1,9 +1,6 @@
 <?php
 
 use Illuminate\Console\Command;
-use Biospex\Services\Image\Image;
-use Illuminate\Filesystem\Filesystem;
-use Biospex\Services\Actor\NotesFromNature;
 
 class TestCommand extends Command {
 
@@ -20,12 +17,9 @@ class TestCommand extends Command {
     /**
      * Constructor
      */
-    public function __construct(Image $image, Filesystem $filesystem, NotesFromNature $notes)
+    public function __construct()
     {
         parent::__construct();
-        $this->image = $image;
-        $this->filesystem = $filesystem;
-        $this->notes = $notes;
     }
 
     /**
@@ -33,44 +27,6 @@ class TestCommand extends Command {
      */
     public function fire()
     {
-     //   convert /data/web/staging.biospex.org/app/storage/data/4-17f9a20a23/5512ba2500cf791f438b4ffe.jpg  -resize 64x64  /data/web/staging.biospex.org/app/storage/testsm.jpg
-        $this->notes->setPaths();
-        $this->notes->buildFiles();
-
-        return;
-
-        $files = $this->filesystem->files("/data/web/staging.biospex.org/app/storage/data/4-aa4ef932d6");
-
-        foreach ($files as $file)
-        {
-            $this->image->imageMagick($file);
-            $origWidth = $this->image->getImageWidth();
-            $origHeight = $this->image->getImageHeight();
-            $baseName = $this->image->getBaseName();
-            $fileName = $this->image->getFileName();
-            $extension = $this->image->getExtension();
-
-            $lrgTargetName = "$fileName.large.$extension";
-            $targetFilePathLg = "/data/web/staging.biospex.org/app/storage/data/4-aa4ef932d6/large/" . $lrgTargetName;
-            $this->image->resize($targetFilePathLg, 1540, 0);
-
-            echo $baseName . PHP_EOL;
-            return;
-        }
-
-        return;
-    }
-
-    public function save($image, $info)
-    {
-        $info = $this->image->getImageInfoFromString($image);
-        print_r($info);
-
-        $this->filesystem->put(storage_path() . "/test.jpg", $image);
-        $this->image->imageMagick(storage_path() . "/data/4-17f9a20a23/5512ba2500cf791f438b4ffe.jpg");
-        $this->image->resize(storage_path() . "/testsm.jpg", 50, 50);
-        $this->image->destroy();
-
         return;
     }
 }
