6b648fcda7c31a6050515621f37ee4e0fd4b7b2e
Robert <bruhnrp@gmail.com>
2015-04-01 10:49:48 -0400

    Removed log statements

--- app/Biospex/Services/Actor/NotesFromNature.php
+++ app/Biospex/Services/Actor/NotesFromNature.php
@@ -255,15 +255,11 @@ class NotesFromNature extends ActorAbstract {
         foreach ($this->imageUriArray as $key => $uri)
         {
             $result = glob("{$this->tmpFileDir}/$key.*");
-            if (empty($result))
-            {
-                $rc->get($uri, ["key: $key"]);
-                $execute = true;
-            }
-            else
-            {
-                \Log::alert("Image for uri found");
-            }
+            if ( ! empty($result))
+                continue;
+
+            $rc->get($uri, ["key: $key"]);
+            $execute = true;
         }
 
         if ($execute)
@@ -329,23 +325,10 @@ class NotesFromNature extends ActorAbstract {
             $smFilePath = "{$this->smFilePath}/$fileName.small.$extension";
 
             if( ! $this->filesystem->exists($lrgFilePath))
-            {
                 $this->image->resize($lrgFilePath, $this->largeWidth, 0);
-            }
-            else
-            {
-                \Log::alert("Image for lrg path found");
-            }
 
             if( ! $this->filesystem->exists($smFilePath))
-            {
                 $this->image->resize($smFilePath, $this->smallWidth, 0);
-            }
-            else
-            {
-                \Log::alert("Image for sm path found");
-            }
-
 
             $this->image->destroy();
             $this->imgCount++;
