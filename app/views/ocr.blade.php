c8430ccb9b029fa0cb4fd33d154f5889a4c0b6c4
Robert <bruhnrp@gmail.com>
2015-04-08 10:10:52 -0400

    Add check all for ocr page

--- app/views/ocr.blade.php
+++ app/views/ocr.blade.php
@@ -28,8 +28,8 @@
             @if (preg_match('/\.json/i', $item->nodeValue))
                 <div class="form-group col-md-offset-4">
                     <div class="col-sm-6">
-                        {{ Form::checkbox('files[]', $item->nodeValue, null, ['id'=>$item->nodeValue]) }}
-                        {{ Form::label($item->nodeValue, $item->nodeValue,array('id'=>'','class'=>'')) }}
+                        {{ Form::checkbox('files[]', $item->nodeValue, null, ['id'=>$item->nodeValue, 'class' => 'checkbox']) }}
+                        {{ Form::label($item->nodeValue, $item->nodeValue) }}
                     </div>
                 </div>
             @endif
