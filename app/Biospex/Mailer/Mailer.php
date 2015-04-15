d94ff0e53619f33bbd0a0d86165f765b0d479eb1
Robert <bruhnrp@gmail.com>
2015-04-15 09:46:52 -0400

    Readding Mailer. Somehow removed.

---
+++ app/Biospex/Mailer/Mailer.php
@@ -0,0 +1,41 @@
+<?php namespace Biospex\Mailer;
+/**
+ * Mailer.php
+ *
+ * @package    Biospex Package
+ * @version    1.0
+ * @author     Robert Bruhn <79e6ef82@opayq.com>
+ * @license    GNU General Public License, version 3
+ * @copyright  (c) 2014, Biospex
+ * @link       http://biospex.org
+ *
+ * This file is part of Biospex.
+ * Biospex is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Biospex is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Biospex.  If not, see <http://www.gnu.org/licenses/>.
+ */
+use Mail;
+
+abstract class Mailer {
+
+	public function sendTo ($from, $email, $subject, $view, $data = [], $attachments = [])
+	{
+		Mail::queue($view, $data, function ($message) use ($from, $email, $subject, $attachments)
+		{
+			$message->from($from)->subject($subject)->to($email);
+			$size = sizeof($attachments);
+			for ($i = 0; $i < $size; $i++){
+				$message->attach($attachments[$i]);
+			}
+		});
+	}
+}
