7c000213582bdc932f70e9022df9e881012f872e
Robert <bruhnrp@gmail.com>
2015-04-23 14:37:18 -0400

    Adding tmiezones to users table

--- app/database/migrations/2015_04_23_165825_add_timezone_to_users_table.php
+++ app/database/migrations/2015_04_23_165825_add_timezone_to_users_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddTimezoneToProfilesTable extends Migration {
+class AddTimezoneToUsersTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,9 +12,9 @@ class AddTimezoneToProfilesTable extends Migration {
 	 */
 	public function up()
 	{
-        Schema::table('profiles', function(Blueprint $table)
+        Schema::table('users', function(Blueprint $table)
         {
-            $table->string('timezone')->default('America/New_York')->after('last_name');
+            $table->string('timezone')->default('America/New_York')->after('reset_password_code');
         });
 	}
 
@@ -25,9 +25,9 @@ class AddTimezoneToProfilesTable extends Migration {
 	 */
 	public function down()
 	{
-        Schema::table('profiles', function(Blueprint $table)
+        Schema::table('users', function(Blueprint $table)
         {
-            $table->dropColumn('last_name');
+            $table->dropColumn('timezone');
         });
 	}
 
