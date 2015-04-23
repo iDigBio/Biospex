59b2feab9bb25b0f93eb7f1c552f9b54745475d9
Robert <bruhnrp@gmail.com>
2015-04-23 17:22:31 -0400

    Added Helper for file size

--- app/Biospex/Helpers/Helper.php
+++ app/Biospex/Helpers/Helper.php
@@ -1,4 +1,5 @@
 <?php namespace Biospex\Helpers;
+
 /**
  * Helper.php
  *
@@ -28,23 +29,25 @@ use Illuminate\Support\Facades\Session;
 
 class Helper {
 
-    public static function sessionFlashPush($key, $value) {
+    public static function sessionFlashPush($key, $value)
+    {
         $values = \Session::get($key, []);
         $values[] = $value;
         Session::flash($key, $values);
     }
 
-	/**
-	 * Round up to an integer, then to the nearest multiple of 5
-	 * Used for scaling project page percent complete
-	 *
-	 * @param $n
-	 * @param int $x
-	 * @return float
-	 */
-	public static function roundUpToAnyFive($n,$x=5) {
-		return (ceil($n)%$x === 0) ? ceil($n) : round(($n+$x/2)/$x)*$x;
-	}
+    /**
+     * Round up to an integer, then to the nearest multiple of 5
+     * Used for scaling project page percent complete
+     *
+     * @param $n
+     * @param int $x
+     * @return float
+     */
+    public static function roundUpToAnyFive($n, $x = 5)
+    {
+        return (ceil($n) % $x === 0) ? ceil($n) : round(($n + $x / 2) / $x) * $x;
+    }
 
     /**
      * Format date using timezone and format.
@@ -60,6 +63,21 @@ class Helper {
     }
 
     /**
+     * Give file size in humna readable form.
+     *
+     * @param $bytes
+     * @param int $decimals
+     * @return string
+     */
+    public static function humanFilesize($bytes, $decimals = 2)
+    {
+        $size = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+        $factor = floor((strlen($bytes) - 1) / 3);
+
+        return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$size[$factor];
+    }
+
+    /**
      * Return timezone array for select box
      *
      * @return array
@@ -69,17 +87,17 @@ class Helper {
         $timezones = \DateTimeZone::listIdentifiers(\DateTimeZone::ALL);
 
         $timezone_offsets = [];
-        foreach( $timezones as $timezone )
+        foreach ($timezones as $timezone)
         {
             $tz = new \DateTimeZone($timezone);
             $timezone_offsets[$timezone] = $tz->getOffset(new \DateTime);
         }
 
         $timezone_list = [];
-        foreach( $timezone_offsets as $timezone => $offset )
+        foreach ($timezone_offsets as $timezone => $offset)
         {
             $offset_prefix = $offset < 0 ? '-' : '+';
-            $offset_formatted = gmdate( 'H:i', abs($offset) );
+            $offset_formatted = gmdate('H:i', abs($offset));
 
             $pretty_offset = "UTC${offset_prefix}${offset_formatted}";
 
@@ -110,7 +128,8 @@ class Helper {
                 {
                     return false;
                 }
-            } else
+            }
+            else
             {
                 if (in_array($fileinfo->getFilename(), $ignore))
                 {
