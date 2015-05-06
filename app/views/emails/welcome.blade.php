1e74b544698806cd637e52ea697c139c7616380f
Robert <bruhnrp@gmail.com>
2015-05-06 11:46:37 -0400

    Fix email templating

--- app/views/emails/welcome.blade.php
+++ app/views/emails/welcome.blade.php
@@ -2,9 +2,8 @@
 
 {{-- Content --}}
 @section('content')
-		<h2><h2>{{ trans('emails.welcome') }}</h2></h2>
-
-		<p><b>{{ trans('email.account') }}:</b> {{{ $email }}}</p>
-		<p>{{ trans('email.activate_message_html') }}, {{{ $activateHtmlLink }}}</p>
-		<p>{{ trans('email.activate_message_text') }}: <br /> {{{ $activateTextLink }}}</p>
+		<h2>{{ trans('emails.welcome') }}</h2>
+		<p><b>{{ trans('emails.account') }}:</b> {{{ $email }}}</p>
+		<p>{{ trans('emails.activate_message_html') }}, {{ $activateHtmlLink }}</p>
+		<p>{{ trans('emails.activate_message_text') }}: {{ $activateTextLink }}</p>
 @stop
