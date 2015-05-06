641d7c39ad3e05ba0334d11cfa75817448232488
Robert <bruhnrp@gmail.com>
2015-05-06 10:40:11 -0400

    Add email template layout.

---
+++ app/views/layouts/email.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+<!-- Content -->
+@yield('content')
+<!-- ./ content -->
+<p>
+{{ trans('emails.thank_you') }}, <br /><br />
+{{ trans('emails.signature') }}<br />
+{{{ $adminEmail  }}}
+</p>
+</body>
+</html>
