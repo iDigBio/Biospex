2c4afcc6299527816f2b133863cadf2bc687a995
Robert <bruhnrp@gmail.com>
2015-05-28 15:57:22 -0400

    Add user log in request for validation

---
+++ app/Http/Requests/UserLoginRequest.php
@@ -0,0 +1,30 @@
+<?php namespace Biospex\Http\Requests;
+
+use Biospex\Http\Requests\Request;
+
+class UserLoginRequest extends Request {
+
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+		return true;
+	}
+
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
+		return [
+            'email' => 'required|min:4|max:32|email',
+            'password' => 'required|min:6'
+		];
+	}
+
+}
