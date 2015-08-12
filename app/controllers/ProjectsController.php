a013b82e2d1eeb903c9e37620cd6138ad0b26b4a
Robert <bruhnrp@gmail.com>
2015-08-12 11:15:41 -0400

    Bring up to date with repo

--- app/controllers/ProjectsController.php
+++ app/controllers/ProjectsController.php
@@ -174,7 +174,7 @@ class ProjectsController extends BaseController {
 		$user = $this->sentry->getUser();
 		$isSuperUser = $user->isSuperUser();
         $isOwner = ($user->id == $project->group->user_id || $isSuperUser) ? true : false;
-		
+
         return View::make('projects.show', compact('user', 'isOwner', 'project'));
 	}
 
