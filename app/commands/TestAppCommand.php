10f597312269011168ed606e59c57d4e3f152ba5
Robert <bruhnrp@gmail.com>
2015-09-29 20:15:53 -0400

    Fix

--- app/commands/TestAppCommand.php
+++ app/commands/TestAppCommand.php
@@ -3,10 +3,6 @@
 use Illuminate\Console\Command;
 use Biospex\Repo\WorkflowManager\WorkflowManagerInterface;
 use Biospex\Services\Actor\NotesFromNature;
-use Biospex\Repo\Expedition\ExpeditionInterface;
-use Biospex\Repo\Project\ProjectInterface;
-use Biospex\Repo\Subject\SubjectInterface;
-
 
 class TestAppCommand extends Command
 {
@@ -21,17 +17,11 @@ class TestAppCommand extends Command
     protected $description = 'Used to test code';
 
     public function __construct(
-        WorkflowManagerInterface $workflow,
-        ExpeditionInterface $expedition,
-        ProjectInterface $project,
-        SubjectInterface $subject
+        WorkflowManagerInterface $workflow
     )
     {
         parent::__construct();
 
-        $this->subject = $subject;
-        $this->project = $project;
-        $this->expedition = $expedition;
         $this->workflow = $workflow;
     }
 
@@ -40,21 +30,12 @@ class TestAppCommand extends Command
      */
     public function fire()
     {
-        //$this->project->setPass(true);
-
-        Project::with('subjects')->find(1);
-        //$project = $this->project->findWith(1, ['subjects']);
-        dd(\DB::connection('mongodb')->getQueryLog());
-        echo "Done" . PHP_EOL;
-
-        /*
         $manager = $this->workflow->findWith(2, ['expedition.actors']);
         foreach ($manager->expedition->actors as $actor) {
             $class = \App::make(NotesFromNature::class);
             $class->setProperties($actor);
             $class->process();
         }
-        */
 
         return;
     }
