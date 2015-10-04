7c1ee4265057d392a47c747390e07c3af57cad51
Robert <bruhnrp@gmail.com>
2015-10-04 09:46:33 -0400

    Change table name

--- database/migrations/2015_09_14_153119_create_user_grid_fields_table.php
+++ database/migrations/2015_09_14_153119_create_user_grid_fields_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUserGridFieldTable extends Migration
+class CreateUserGridFieldsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,7 +12,7 @@ class CreateUserGridFieldTable extends Migration
      */
     public function up()
     {
-        Schema::create('user_grid_field', function (Blueprint $table) {
+        Schema::create('user_grid_fields', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('user_id');
             $table->unsignedInteger('project_id');
@@ -26,7 +26,7 @@ class CreateUserGridFieldTable extends Migration
             $table->engine = 'InnoDB';
         });
 
-        DB::statement("ALTER TABLE user_grid_field ADD data LONGBLOB AFTER project_id");
+        DB::statement("ALTER TABLE user_grid_fields ADD data LONGBLOB AFTER project_id");
     }
 
     /**
@@ -36,6 +36,6 @@ class CreateUserGridFieldTable extends Migration
      */
     public function down()
     {
-        Schema::drop('user_grid_field');
+        Schema::drop('user_grid_fields');
     }
 }
