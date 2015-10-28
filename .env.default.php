e844e38bded45236626b6ea3f134662b14e00cda
Robert <bruhnrp@gmail.com>
2015-10-28 14:26:31 -0400

    Update environment file and composer

--- .env.default.php
+++ .env.default.php
@@ -1,30 +1,5 @@
 <?php
-/**
- * default.env.php
- *
- * @package    Biospex Package
- * @version    1.0
- * @author     Robert Bruhn <bruhnrp@gmail.com>
- * @license    GNU General Public License, version 3
- * @copyright  (c) 2014, Biospex
- * @link       http://biospex.org
- *
- * This file is part of Biospex.
- * Biospex is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Biospex is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Biospex.  If not, see <http://www.gnu.org/licenses/>.
- */
 
-// Descriptions can be found in /app/config files
 return [
     'site.ip'             => '127.0.0.1',
     'site.name'           => 'Your Site Name',
@@ -36,30 +11,26 @@ return [
     'site.locale'         => 'en',
     'site.adminEmail'     => 'youremail@provider.org',
     'site.registration'   => true,
-    'site.ocrPostUrl'     => 'http://ocr.dev.morphbank.net/batchocr',
-    'site.ocrGetUrl'      => 'http://ocr.dev.morphbank.net/status',
-    'site.ocrDeleteUrl'   => 'http://ocr.dev.morphbank.net/delete',
+    'site.ocrPostUrl'     => 'http://ocr.idiginfo.org/batchocr',
+    'site.ocrGetUrl'      => 'http://ocr.idiginfo.org/status',
+    'site.ocrDeleteUrl'   => 'http://ocr.idiginfo.org/delete',
     'site.ocrCrop'        => 'no',
     'site.disableOcr'     => true,
-
+    'api.domain'          => 'api.yoursite.loc',
     'nfn.collection'      => 'fsu',
-
     'beanstalkd.default'  => 'default',
     'beanstalkd.import'   => 'import',
     'beanstalkd.workflow' => 'workflow',
     'beanstalkd.ocr'      => 'ocr',
-
     'mysql.host'          => 'localhost',
     'mysql.database'      => 'db',
     'mysql.username'      => 'user',
     'mysql.password'      => 'pass',
-
     'mongodb.host'        => 'localhost',
     'mongodb.port'        => 27017,
     'mongodb.username'    => 'user',
     'mongodb.password'    => 'pass',
     'mongodb.database'    => 'db',
-
     'mail.domain'         => 'mailgun domain',
     'mail.secret'         => 'mailgun secret key',
     'mail.driver'         => 'mailgun',
