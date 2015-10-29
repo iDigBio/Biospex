4809df9c441eedc1cad936ea2c6608bdc8244025
Robert <bruhnrp@gmail.com>
2015-10-29 18:20:37 -0400

    Reset TestCommand

--- app/commands/TestAppCommand.php
+++ app/commands/TestAppCommand.php
@@ -1,9 +1,6 @@
 <?php
 
-use Biospex\Repo\Header\HeaderInterface;
 use Illuminate\Console\Command;
-
-
 
 class TestAppCommand extends Command
 {
@@ -17,16 +14,10 @@ class TestAppCommand extends Command
      */
     protected $description = 'Used to test code';
 
-    /**
-     * @var HeaderInterface
-     */
-    private $headerInterface;
-
-    public function __construct(HeaderInterface $headerInterface)
+    public function __construct()
     {
         parent::__construct();
 
-        $this->headerInterface = $headerInterface;
     }
 
     /**
@@ -34,42 +25,7 @@ class TestAppCommand extends Command
      */
     public function fire()
     {
-        $projects = Project::all();
 
-        foreach($projects as $project) {
-            $advertise = json_decode($project->advertise);
-            $project->advertise = serialize($advertise);
-            $project->save();
-
-            /*
-            $header = $this->headerInterface->getByProjectId($project->id);
-
-            if (empty($header)) {
-                continue;
-            }
-
-            foreach (json_decode($header->header) as $key => $value)
-            {
-                $newHeader['image'][] = $key;
-            }
-
-            $occurrenceHeader = [];
-            foreach($project->expeditions as $expedition) {
-                foreach ($expedition->subjects as $subject) {
-                    $subject = $subject->toArray();
-                    $occurrence = $subject['occurrence'];
-                    unset($occurrence['_id']);
-                    $occurrenceHeader = array_merge(array_diff(array_keys($occurrence), $occurrenceHeader), $occurrenceHeader);
-                }
-            }
-            $newHeader['occurrence'] = $occurrenceHeader;
-
-            $header->header = serialize($newHeader);
-            $header->save();
-            */
-        }
-
-        echo  "Done" . PHP_EOL;
         return;
     }
 }
