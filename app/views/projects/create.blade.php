ed1b7adbb5b580e17f67c1f0e50286711b710ea1
Robert <bruhnrp@gmail.com>
2015-11-16 11:22:27 -0500

    Added functionality for workflows and jqGrid

--- app/views/projects/create.blade.php
+++ app/views/projects/create.blade.php
@@ -19,7 +19,205 @@
     'role' => 'form'
     )) }}
 
-    @include('partials.project-fields')
+    <div class="form-group required {{ ($errors->has('group')) ? 'has-error' : '' }}" for="group">
+        {{ Form::label('group', trans('forms.group'), array('class' => 'col-sm-2 control-label')) }}
+        <div class="col-sm-10">
+            {{ Form::select('group_id', $selectGroups, array('class' => 'form-control', 'placeholder' => trans('forms.group'))) }}
+        </div>
+        {{ ($errors->has('group_id') ? $errors->first('group_id') : '') }}
+    </div>
+
+    <div class="form-group required {{ ($errors->has('status')) ? 'has-error' : '' }}" for="group">
+        {{ Form::label('status', trans('forms.status'), array('class' => 'col-sm-2 control-label')) }}
+        <div class="col-sm-10">
+            {{ Form::select('status', $statusSelect, array('class' => 'form-control', 'placeholder' => trans('forms.status'))) }}
+        </div>
+        {{ ($errors->has('status') ? $errors->first('status') : '') }}
+    </div>
+
+    <div class="form-group required {{ ($errors->has('title')) ? 'has-error' : '' }}" for="title">
+        {{ Form::label('title', trans('forms.title'), array('class' => 'col-sm-2 control-label')) }}
+        <div class="col-sm-10">
+            {{ Form::text('title', null, array('class' => 'form-control', 'placeholder' => trans('forms.title'))) }}
+        </div>
+        {{ ($errors->has('title') ? $errors->first('title') : '') }}
+    </div>
+
+    <div class="form-group required {{ ($errors->has('contact')) ? 'has-error' : '' }}">
+        {{ Form::label('contact', trans('forms.contact'), array('class' => 'col-sm-2 control-label')) }}
+        <div class="col-sm-10">
+            {{ Form::text('contact', null, array('class' => 'form-control', 'placeholder' => trans('forms.contact'))) }}
+        </div>
+        {{ ($errors->has('contact') ? $errors->first('contact') : '') }}
+    </div>
+
+    <div class="form-group required {{ ($errors->has('contact_email')) ? 'has-error' : '' }}">
+        {{ Form::label('contact_email', trans('forms.contact_email'), array('class' => 'col-sm-2 control-label')) }}
+        <div class="col-sm-10">
+            {{ Form::text('contact_email', null, array('class' => 'form-control', 'placeholder' => trans('forms.contact_email'))) }}
+        </div>
+        {{ ($errors->has('contact_email') ? $errors->first('contact_email') : '') }}
+    </div>
+
+    <div class="form-group required {{ ($errors->has('contact_title')) ? 'has-error' : '' }}">
+        {{ Form::label('contact_title', trans('forms.contact_title'), array('class' => 'col-sm-2 control-label')) }}
+        <div class="col-sm-10">
+            {{ Form::text('contact_title', null, array('class' => 'form-control', 'placeholder' => trans('forms.contact_title'))) }}
+        </div>
+        {{ ($errors->has('contact_title') ? $errors->first('contact_title') : '') }}
+    </div>
+
+    <div class="form-group {{ ($errors->has('organization')) ? 'has-error' : '' }}">
+        {{ Form::label('organization', trans('forms.organization'), array('class' => 'col-sm-2 control-label')) }}
+        <div class="col-sm-10">
+            {{ Form::text('organization', null, array('class' => 'form-control', 'placeholder' => trans('forms.organization_format'))) }}
+        </div>
+        {{ ($errors->has('organization') ? $errors->first('organization') : '') }}
+    </div>
+
+    <div class="form-group {{ ($errors->has('organization_website')) ? 'has-error' : '' }}">
+        {{ Form::label('organization_website', trans('forms.organization_website'), array('class' => 'col-sm-2 control-label')) }}
+        <div class="col-sm-10">
+            {{ Form::text('organization_website', null, array('class' => 'form-control', 'placeholder' => trans('forms.organization_website_format'))) }}
+        </div>
+        {{ ($errors->has('organization_website') ? $errors->first('organization_website') : '') }}
+    </div>
+
+    <div class="form-group {{ ($errors->has('project_partners')) ? 'has-error' : '' }}">
+        {{ Form::label('project_partners', trans('forms.project_partners'), array('class' => 'col-sm-2 control-label')) }}
+        <div class="col-sm-10">
+            {{ Form::text('project_partners', null, array('class' => 'form-control', 'placeholder' => trans('forms.project_partners'))) }}
+        </div>
+        {{ ($errors->has('project_partners') ? $errors->first('project_partners') : '') }}
+    </div>
+
+    <div class="form-group {{ ($errors->has('funding_source')) ? 'has-error' : '' }}">
+        {{ Form::label('funding_source', trans('forms.funding_source'), array('class' => 'col-sm-2 control-label')) }}
+        <div class="col-sm-10">
+            {{ Form::text('funding_source', null, array('class' => 'form-control', 'placeholder' => trans('forms.funding_source'))) }}
+        </div>
+        {{ ($errors->has('funding_source') ? $errors->first('funding_source') : '') }}
+    </div>
+
+    <div class="form-group required {{ ($errors->has('description_short')) ? 'has-error' : '' }}">
+        {{ Form::label('description_short', trans('forms.description_short'), array('class' => 'col-sm-2 control-label')) }}
+        <div class="col-sm-10">
+            {{ Form::text('description_short', null, array('class' => 'form-control', 'placeholder' => trans('forms.description_short_max'))) }}
+        </div>
+        {{ ($errors->has('description_short') ? $errors->first('description_short') : '') }}
+    </div>
+
+    <div class="form-group required {{ ($errors->has('description_long')) ? 'has-error' : '' }}">
+        {{ Form::label('description_long', trans('forms.description_long'), array('class' => 'col-sm-2 control-label')) }}
+        <div class="col-sm-10">
+            {{ Form::textarea('description_long', null, array('class' => 'form-control', 'placeholder' => trans('forms.description_long'))) }}
+        </div>
+        {{ ($errors->has('description_long') ? $errors->first('description_long') : '') }}
+    </div>
+
+    <div class="form-group {{ ($errors->has('incentives')) ? 'has-error' : '' }}">
+        {{ Form::label('incentives', trans('forms.incentives'), array('size' => '30x3', 'class' => 'col-sm-2 control-label')) }}
+        <div class="col-sm-10">
+            {{ Form::textarea('incentives', null, array('size' => '30x3', 'class' => 'form-control', 'placeholder' => trans('forms.incentives'))) }}
+        </div>
+        {{ ($errors->has('incentives') ? $errors->first('incentives') : '') }}
+    </div>
+
+    <div class="form-group {{ ($errors->has('geographic_scope')) ? 'has-error' : '' }}">
+        {{ Form::label('geographic_scope', trans('forms.geographic_scope'), array('class' => 'col-sm-2 control-label')) }}
+        <div class="col-sm-10">
+            {{ Form::text('geographic_scope', null, array('class' => 'form-control', 'placeholder' => trans('forms.geographic_scope'))) }}
+        </div>
+        {{ ($errors->has('geographic_scope') ? $errors->first('geographic_scope') : '') }}
+    </div>
+
+    <div class="form-group {{ ($errors->has('taxonomic_scope')) ? 'has-error' : '' }}">
+        {{ Form::label('taxonomic_scope', trans('forms.taxonomic_scope'), array('class' => 'col-sm-2 control-label')) }}
+        <div class="col-sm-10">
+            {{ Form::text('taxonomic_scope', null, array('class' => 'form-control', 'placeholder' => trans('forms.taxonomic_scope'))) }}
+        </div>
+        {{ ($errors->has('taxonomic_scope') ? $errors->first('taxonomic_scope') : '') }}
+    </div>
+
+    <div class="form-group {{ ($errors->has('temporal_scope')) ? 'has-error' : '' }}">
+        {{ Form::label('temporal_scope', trans('forms.temporal_scope'), array('class' => 'col-sm-2 control-label')) }}
+        <div class="col-sm-10">
+            {{ Form::text('temporal_scope', null, array('class' => 'form-control', 'placeholder' => trans('forms.temporal_scope'))) }}
+        </div>
+        {{ ($errors->has('temporal_scope') ? $errors->first('temporal_scope') : '') }}
+    </div>
+
+    <div class="form-group required {{ ($errors->has('keywords')) ? 'has-error' : '' }}">
+        {{ Form::label('keywords', trans('forms.keywords'), array('class' => 'col-sm-2 control-label')) }}
+        <div class="col-sm-10">
+            {{ Form::text('keywords', null, array('class' => 'form-control', 'placeholder' => trans('forms.keywords'))) }}
+        </div>
+        {{ ($errors->has('keywords') ? $errors->first('keywords') : '') }}
+    </div>
+
+    <div class="form-group {{ ($errors->has('blog_url')) ? 'has-error' : '' }}">
+        {{ Form::label('blog_url', trans('forms.blog_url'), array('class' => 'col-sm-2 control-label')) }}
+        <div class="col-sm-10">
+            {{ Form::text('blog_url', null, array('class' => 'form-control', 'placeholder' => trans('forms.blog_url_format'))) }}
+        </div>
+        {{ ($errors->has('blog_url') ? $errors->first('blog_url') : '') }}
+    </div>
+
+    <div class="form-group {{ ($errors->has('facebook')) ? 'has-error' : '' }}">
+        {{ Form::label('facebook', trans('forms.facebook'), array('class' => 'col-sm-2 control-label')) }}
+        <div class="col-sm-10">
+            {{ Form::text('facebook', null, array('class' => 'form-control', 'placeholder' => trans('forms.facebook_format'))) }}
+        </div>
+        {{ ($errors->has('facebook') ? $errors->first('facebook') : '') }}
+    </div>
+
+    <div class="form-group {{ ($errors->has('twitter')) ? 'has-error' : '' }}">
+        {{ Form::label('twitter', trans('forms.twitter'), array('class' => 'col-sm-2 control-label')) }}
+        <div class="col-sm-10">
+            {{ Form::text('twitter', null, array('class' => 'form-control', 'placeholder' => trans('forms.twitter_format'))) }}
+        </div>
+        {{ ($errors->has('twitter') ? $errors->first('twitter') : '') }}
+    </div>
+
+    <div class="form-group {{ ($errors->has('activities')) ? 'has-error' : '' }}">
+        {{ Form::label('activities', trans('forms.activities'), array('class' => 'col-sm-2 control-label')) }}
+        <div class="col-sm-10">
+            {{ Form::text('activities', null, array('class' => 'form-control', 'placeholder' => trans('forms.activities'))) }}
+        </div>
+        {{ ($errors->has('activities') ? $errors->first('activities') : '') }}
+    </div>
+
+    <div class="form-group {{ ($errors->has('language_skills')) ? 'has-error' : '' }}">
+        {{ Form::label('language_skills', trans('forms.language_skills'), array('class' => 'col-sm-2 control-label')) }}
+        <div class="col-sm-10">
+            {{ Form::text('language_skills', null, array('class' => 'form-control', 'placeholder' => trans('forms.language_skills'))) }}
+        </div>
+        {{ ($errors->has('language_skills') ? $errors->first('language_skills') : '') }}
+    </div>
+
+    <div class="form-group required {{ ($errors->has('workflow')) ? 'has-error' : '' }}">
+        {{ Form::label('workflow', trans('forms.workflows'), array('class' => 'col-sm-2 control-label')) }}
+        <div class="col-sm-4">
+            {{ Form::select('workflow', $workflows, array('class' => 'form-control', 'placeholder' => trans('forms.workflows'))) }}
+        </div>
+        {{ ($errors->has('workflow') ? $errors->first('workflow') : '') }}
+    </div>
+
+    <div class="form-group {{ ($errors->has('logo')) ? 'has-error' : '' }}">
+        {{ Form::label('logo', trans('forms.logo'), array('class' => 'col-sm-2 control-label')) }}
+        <div class="col-sm-10">
+            {{ Form::file('logo') }} {{ trans('forms.logo_max') }}
+        </div>
+        {{ ($errors->has('logo') ? $errors->first('logo') : '') }}
+    </div>
+
+    <div class="form-group {{ ($errors->has('banner')) ? 'has-error' : '' }}">
+        {{ Form::label('banner', trans('forms.banner'), array('class' => 'col-sm-2 control-label')) }}
+        <div class="col-sm-10">
+            {{ Form::file('banner') }} {{ trans('forms.banner_min') }}
+        </div>
+        {{ ($errors->has('banner') ? $errors->first('banner') : '') }}
+    </div>
 
     <div class="form-group">
         <div class="col-sm-offset-2 col-sm-10">
--- app/views/projects/explore.blade.php
+++ app/views/projects/explore.blade.php
@@ -19,7 +19,7 @@
         <div style="padding: 10px;">
             <p class="eyesright"><strong>@lang('pages.project_url'):</strong> {{ HTML::linkAction('HomeController@project', $project->title, [$project->slug]) }} </p>
             <button title="@lang('buttons.dataTitle')" class="btn btn-success btn-xs" type="button" onClick="location.href='{{ URL::route('projects.import', [$project->id]) }}'"><span class="glyphicon glyphicon-plus-sign"></span> @lang('buttons.data')</button>
-            <button title="@lang('buttons.dataViewTitle')" class="btn btn-info btn-xs" type="button" onClick="location.href='{{ URL::route('projects.subjects.show', [$project->id]) }}'"><span class="glyphicon glyphicon-search"></span> @lang('buttons.dataView')</button>
+            <button title="@lang('buttons.dataViewTitle')" class="btn btn-info btn-xs" type="button" onClick="location.href='{{ URL::route('projects.explore', [$project->id]) }}'"><span class="glyphicon glyphicon-search"></span> @lang('buttons.dataView')</button>
             <button title="@lang('buttons.duplicateTitle')" class="btn btn-success btn-xs" type="button" onClick="location.href='{{ URL::route('projects.duplicate', [$project->id]) }}'"><span class="glyphicon glyphicon-share-alt"></span> @lang('buttons.duplicate')</button>
             <button title="@lang('buttons.editTitle')" class="btn btn-warning btn-xs" type="button" onClick="location.href='{{ URL::route('projects.edit', [$project->id]) }}'"><span class="glyphicon glyphicon-cog"></span> @lang('buttons.edit')</button>
             @if ($isOwner)
@@ -31,11 +31,21 @@
 
     <hr />
 
-    <h3>{{ trans('pages.subjects') }}:</h3>
+    <h4>{{ trans_choice('pages.subjects_assigned', $subjectAssignedCount) }}: <span id="subjectCount">{{ $subjectAssignedCount }}</span> </h4>
     <div class="table-responsive" id="jqtable">
+        <input type="hidden" id="url" value="{{ URL::route('projects.grids.explore', [$project->id]) }}">
+        <input type="hidden" id="showCb" value="0">
         <input type="hidden" id="projectId" value="{{ $project->id }}">
         <input type="hidden" id="expeditionId" value="0">
-        <table id="list"></table>
+        <table class="table table-bordered jgrid" id="jqGridExplore"></table>
         <div id="pager"></div>
+        <br />
+        <button id="savestate" class="btn btn-default">Save Grid State</button>
+        <button id="loadstate" class="btn btn-default">Load Grid State</button>
     </div>
+    @include('layouts/jqgrid-modal')
+@stop
+@section('javascript')
+    @parent
+    @javascripts('grid/application')
 @stop
