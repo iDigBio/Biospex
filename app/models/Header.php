8756f99cf70878eb45158ad25c56948d4f186c8e
Robert <bruhnrp@gmail.com>
2015-11-18 18:48:16 -0500

    Add mutator and accessor for header column

--- app/models/Header.php
+++ app/models/Header.php
@@ -57,8 +57,32 @@ class Header extends Eloquent
         return $this->belongsTo('Project');
     }
 
+    /**
+     * Get header by project id
+     * @param $id
+     * @return \Illuminate\Database\Eloquent\Model|null|static
+     */
     public function getByProjectId($id)
     {
         return $this->where('project_id', '=', $id)->first();
+    }
+
+    /**
+     * Accessor for header column
+     * @param $value
+     * @return mixed
+     */
+    public function getHeaderAttribute($value)
+    {
+        return unserialize($value);
+    }
+
+    /**
+     * Mutator for header column
+     * @param $value
+     */
+    public function setHeaderAttribute($value)
+    {
+        $this->attributes['header'] = serialize($value);
     }
 }
