45ecff2edb22392dc36f4a6c290c6275020139ef
Robert <bruhnrp@gmail.com>
2015-11-19 12:01:50 -0500

    Testing for subject counts

--- app/commands/TestAppCommand.php
+++ app/commands/TestAppCommand.php
@@ -4,8 +4,8 @@ use Biospex\Repo\Subject\SubjectInterface;
 use Illuminate\Console\Command;
 use League\Csv\Reader;
 
-class TestAppCommand extends Command
-{
+class TestAppCommand extends Command {
+
     /**
      * The console command name.
      */
@@ -33,8 +33,10 @@ class TestAppCommand extends Command
         $this->reader = Reader::createFromPath($file);
         $this->reader->setDelimiter(",");
 
-        foreach ($this->reader->setOffset(1)->fetch() as $row) {
-            if (empty($row[0])) {
+        foreach ($this->reader->setOffset(1)->fetch() as $row)
+        {
+            if (empty($row[0]))
+            {
                 continue;
             }
             $this->processRow($row);
@@ -45,12 +47,10 @@ class TestAppCommand extends Command
 
     public function processRow($row)
     {
-        $count = Subject::where('occurrence.id', $row[0])->count();
-        if ($count > 1) {
-            $subjects = Subject::where('occurrence.id', $row[0])->get();
-            foreach($subjects as $subject) {
-                echo $subject->_id . " : " . $subject->occurrence->id . " : " . $subject->project_id . PHP_EOL;
-            }
+        $subjects = Subject::where('occurrence.id', $row[0])->get();
+        foreach ($subjects as $subject)
+        {
+            echo $subject->_id . " : " . $subject->occurrence->id . " : " . $subject->project_id . PHP_EOL;
         }
     }
 }
