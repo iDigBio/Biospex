e2a9bb19ba9df74837d5d9486769088127bc7927
Robert <bruhnrp@gmail.com>
2015-12-05 14:59:34 -0500

    Testing

--- app/commands/TestAppCommand.php
+++ app/commands/TestAppCommand.php
@@ -14,22 +14,26 @@ class TestAppCommand extends Command {
      * The console command description.
      */
     protected $description = 'Used to test code';
-    /**
-     * @var \Biospex\Repo\Subject\SubjectInterface
-     */
-    private $subjectInterface;
 
+    public $reader;
 
-    public function __construct(\Biospex\Repo\Subject\SubjectInterface $subjectInterface)
+    public function __construct()
     {
         parent::__construct();
-        $this->subjectInterface = $subjectInterface;
     }
 
     /**
      * Fire queue.
      */
     public function fire()
+    {
+        //$this->processImages();
+        $this->processOccurrences();
+
+        return;
+    }
+
+    public function processImages()
     {
         $file = storage_path('images.csv');
 
@@ -58,9 +62,45 @@ class TestAppCommand extends Command {
             echo "Updating " . $subject['_id'] . PHP_EOL;
             $i++;
         }
-
         echo "Finished updating $i records" . PHP_EOL;
-        return;
+    }
+
+    public function processOccurrences()
+    {
+        $file = storage_path('occurrences.csv');
+
+        $this->createReader($file);
+
+        $header = $this->reader->fetchOne();
+
+        $i = 0;
+        foreach ($this->reader->setOffset(1)->fetch() as $row)
+        {
+            $combined = array_combine($header, $row);
+
+            $subjects = Subject::where("project_id", 2)->where('occurrence.id', $combined['id'])->get();
+            if (empty($subjects))
+                continue;
+
+            foreach ($subjects as $subject) {
+                foreach ($combined as $key => $value)
+                {
+                    $subject->occurrence->{$key} = $value;
+                }
+
+                $subject->save();
+
+                echo "Updating " . $subject['_id'] . PHP_EOL;
+                $i++;
+            }
+        }
+        echo "Finished updating $i records" . PHP_EOL;
+    }
+
+    public function createReader($file)
+    {
+        $this->reader = Reader::createFromPath($file);
+        $this->reader->setDelimiter(",");
     }
 
 }
