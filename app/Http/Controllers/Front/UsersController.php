7bffc63f7f45f8422b07099bf38ca1cbe4139a7a
Robert <bruhnrp@gmail.com>
2016-01-16 18:56:43 -0500

    Update user controller

--- app/Http/Controllers/Front/UsersController.php
+++ app/Http/Controllers/Front/UsersController.php
@@ -1,61 +1,21 @@
 <?php namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
+use App\Repositories\Contracts\User;
 use App\Services\Common\UserService;
 use App\Http\Requests\EditUserFormRequest;
-
-use App\Repositories\Contracts\User;
-use App\Repositories\Contracts\Group;
-use App\Repositories\Contracts\Permission;
-use App\Repositories\Contracts\Invite;
-use App\Http\Requests\RegisterFormRequest;
-use Illuminate\Contracts\Events\Dispatcher;
 
 class UsersController extends Controller
 {
     /**
      * @var User
      */
-    protected $user;
+    public $user;
 
-    /**
-     * @var Group
-     */
-    protected $group;
-
-    /**
-     * @var UserForm
-     */
-    protected $userForm;
-
-    /**
-     * @var ResendActivationForm
-     */
-    protected $resendActivationForm;
-
-    /**
-     * @var ForgotPasswordForm
-     */
-    protected $forgotPasswordForm;
-
-    /**
-     * @var ChangePasswordForm
-     */
-    protected $changePasswordForm;
-
-    /**
-     * @var SuspendUserForm
-     */
-    protected $suspendUserForm;
-
-    /**
-     * @var Permission
-     */
-    protected $permission;
     /**
      * @var UserService
      */
-    private $service;
+    public $service;
 
     /**
      * Instantiate a new UsersController
@@ -66,20 +26,13 @@ class UsersController extends Controller
      * @param Group $group
      * @param Permission $permission
      * @param Invite $invite
+     * @param Router $router
      */
     public function __construct(
         UserService $service,
-        Dispatcher $events,
-        User $user,
-        Group $group,
-        Permission $permission,
-        Invite $invite
+        User $user
     ) {
-        $this->events = $events;
         $this->user = $user;
-        $this->group = $group;
-        $this->permission = $permission;
-        $this->invite = $invite;
         $this->service = $service;
     }
 
@@ -89,22 +42,29 @@ class UsersController extends Controller
      * @param  int $id
      * @return Response
      */
-    public function show($id)
+    public function read($id)
     {
-        return redirect()->route('users.edit', [$id]);
+        return redirect()->route('users.get.edit', [$id]);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
+     * @param $id
      * @return Response
      */
-    public function edit()
+    public function edit($id)
     {
-        $vars = $this->service->edit();
+        $user = $this->user->find($id);
 
-        if ( ! $vars)
-            return redirect()->route('home');
+        if ( ! policy($user)->edit($user))
+        {
+            session_flash_push('warning', trans('pages.insufficient_permissions'));
+
+            return redirect()->route('projects.get.index');
+        }
+
+        $vars = $this->service->edit($user);
 
         return view('front.users.edit', $vars);
     }
@@ -117,66 +77,17 @@ class UsersController extends Controller
      */
     public function update(EditUserFormRequest $request)
     {
+        $user = $this->user->find($request->input('users'));
+
+        if ( ! policy($user)->update($user))
+        {
+            session_flash_push('warning', trans('pages.insufficient_permissions'));
+
+            return redirect()->route('projects.get.index');
+        }
+
         $this->service->update($request);
 
-        return redirect()->route('users.edit', $request->get('id'));
-    }
-
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int $id
-     * @return Response
-     */
-    public function destroy($id)
-    {
-        if (! is_numeric($id)) {
-            session_flash_push('error', trans('pages.error_delete_user'));
-
-            return redirect()->route('users.index');
-        }
-
-        if ($this->user->destroy($id)) {
-            $this->events->fire('user.destroyed', ['userId' => $id]);
-
-            session_flash_push('success', trans('users.deleted'));
-
-            return redirect()->route('users.index');
-        } else {
-            session_flash_push('error', trans('pages.error_delete_user'));
-
-            return redirect()->route('users.index');
-        }
-    }
-
-    /**
-     * Process a password change request
-     *
-     * @param  int $id
-     * @return redirect
-     */
-    public function change($id)
-    {
-        $data = \Input::all();
-        $data['id'] = $id;
-
-        // Form Processing
-        $result = $this->changePasswordForm->change($data);
-
-        if ($result['success']) {
-            $this->events->fire('user.passwordchange', ['userId' => $id]);
-
-            // Success!
-            session_flash_push('success', $result['message']);
-
-            return redirect()->route('users.show', [$id]);
-        } else {
-            session_flash_push('error', $result['message']);
-
-            return redirect()->route('users.edit', [$id])
-                ->withInput()
-                ->withErrors($this->changePasswordForm->errors());
-        }
+        return redirect()->route('users.get.edit', [$user->id]);
     }
 }
