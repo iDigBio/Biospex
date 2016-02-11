4bdb0a5bf3b7f24303530655191169e9470bdfc3
Robert <bruhnrp@gmail.com>
2016-02-11 12:05:36 -0500

    Update language and app css

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -3,11 +3,37 @@ $(document).ready(function() {
     $('.collapse').on('shown.bs.collapse', function () {
         $("#collapse"+this.id).removeClass("fa-folder").addClass("fa-folder-open");
         $("#"+this.id).load("/projects/"+this.id+"/expeditions");
+        $(this).closest('tr').show();
     });
 
     $('.collapse').on('hidden.bs.collapse', function () {
         $("#collapse"+this.id).removeClass("fa-folder-open").addClass("fa-folder");
         $( "#expeditions"+this.id).html('');
+        $(this).closest('tr').hide();
+    });
+
+    $(".table-sort").tablesorter({
+        // this will apply the bootstrap theme if "uitheme" widget is included
+        // the widgetOptions.uitheme is no longer required to be set
+        theme : "bootstrap",
+
+        widthFixed: false,
+
+        headerTemplate : '{content} {icon}', // new in v2.7. Needed to add the bootstrap icon!
+
+        // widget code contained in the jquery.tablesorter.widgets.js file
+        // use the zebra stripe widget if you plan on hiding any rows (filter widget)
+        widgets : [ "uitheme", "zebra" ],
+
+        widgetOptions : {
+            // using the default zebra striping class name, so it actually isn't included in the theme variable above
+            // this is ONLY needed for bootstrap theming if you are using the filter widget, because rows are hidden
+            zebra : ["even", "odd"],
+            // reset filters button
+            filter_reset : ".reset",
+            // extra css class name (string or array) added to the filter element (input or select)
+            filter_cssFilter: "form-control",
+        }
     });
 
     $('#add_target').on('click', function() {
