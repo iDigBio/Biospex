bf4d9cc9bdbb0684579ed7e3e33efd6300f4236a
Robert <bruhnrp@gmail.com>
2016-02-11 12:12:07 -0500

    update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,7 @@ elixir(function(mix) {
             'vendor/jqgrid/css/ui.jqgrid-bootstrap.css',
             'vendor/jqgrid/css/ui.jqgrid-bootstrap-ui.css',
             'vendor/jqgrid/plugins/ui.multiselect.css',
+            'vendor/tablesorter/dist/css/theme.bootstrap.min.css',
         ], 'public/css/app.css', assetsDir)
         .scripts([
             'vendor/jquery/dist/jquery.min.js',
@@ -29,60 +30,13 @@ elixir(function(mix) {
             'vendor/jqgrid/plugins/ui.multiselect.js',
             'vendor/bootstrap/dist/js/bootstrap.min.js',
             'vendor/bootstrap-select/dist/js/bootstrap-select.min.js',
+            'vendor/tablesorter/dist/js/jquery.tablesorter.min.js',
+            'vendor/tablesorter/dist/js/jquery.tablesorter.widgets.js',
             'js/app.js',
             'js/grid.js'
         ], 'public/js/app.js', assetsDir)
         .copy(assetsDir + 'vendor/font-awesome/fonts', 'public/fonts')
         .copy(assetsDir + 'vendor/jquery-ui/themes/smoothness/images', 'public/css/images')
-        //.version(['css/app.css', 'js/app.js']);
-
-    /*
-     mix.less('app.less', assetsDir + '/css', { paths: lessPaths })
-     .styles([
-     'css/app.css',
-     'vendor/jquery.qtip.custom/jquery.qtip.min.css',
-     'vendor/jqgrid/css/ui.jqgrid.css',
-     'vendor/jqgrid/css/addons/ui.multiselect.css',
-     'vendor/admin-lte/dist/css/AdminLTE.min.css',
-     'vendor/admin-lte/dist/css/skins/skin-blue.min.css',
-
-     ], 'public/css/app.css', assetsDir)
-     .scripts([
-     'vendor/jquery/dist/jquery.min.js',
-     'vendor/jquery-ui/jquery-ui.min.js',
-     'vendor/jquery.validation/dist/jquery.validate.min.js',
-     'vendor/jquery.validation/dist/additional-methods.min.js',
-     'vendor/jquery.qtip.custom/jquery.qtip.min.js',
-     'vendor/jqgrid/js/minified/i18n/grid.locale-en.js',
-     'vendor/jqgrid/js/minified/jquery.jqGrid.min.js',
-     'vendor/bootstrap/dist/js/bootstrap.min.js',
-     'vendor/bootstrap-select/dist/js/bootstrap-select.min.js',
-     'vendor/admin-lte/dist/js/app.min.js',
-     'vendor/admin-lte/dist/js/pages/dashboard.js',
-     'vendor/admin-lte/dist/js/pages/dashboard2.js',
-     'js/app.js',
-     'js/grid.js'
-     ], 'public/js/app.js', assetsDir)
-     .copy(assetsDir + 'vendor/font-awesome/fonts', 'public/fonts')
-     .copy(assetsDir + 'vendor/admin-lte/plugins', 'public/plugins')
-     .copy(assetsDir + 'vendor/admin-lte/dist/img', 'public/img')
-     .version(['css/app.css', 'js/app.js']);
-    */
-
      });
 
-/*
-elixir(function(mix) {
-    mix.less('app.less', 'public/css', { paths: lessPaths })
-        .scripts([
-            'vendor/jquery/dist/jquery.min.js',
-            'vendor/bootstrap/dist/js/bootstrap.min.js',
-            'vendor/bootstrap-select/dist/js/bootstrap-select.min.js',
-            'js/app.js',
-            'js/grid.js'
-        ], 'public/js/app.js', assetsDir)
-        .copy(assetsDir + 'vendor/font-awesome/fonts', 'public/fonts')
-        .version(['css/app.css', 'js/app.js']);
-});
-*/
 
