1a4703e0b7d1f59b7200ae8f6e5df900ddad17b0
Robert <bruhnrp@gmail.com>
2016-02-17 12:43:47 -0500

    Fixing isSuperUser reference

--- resources/views/front/projects/index.blade.php
+++ resources/views/front/projects/index.blade.php
@@ -70,7 +70,7 @@
                                             type="button"
                                             onClick="location.href='{{ route('projects.get.edit', [$project->id]) }}'"><span
                                                 class="fa fa-cog fa-lg"></span> @lang('buttons.edit')</button>
-                                    @if ($user->id == $group->user_id)
+                                    @if ($user->id == $group->user_id || $isSuperUser)
                                         <button title="@lang('buttons.deleteTitle')"
                                                 class="btn btn-default btn-danger action_confirm btn-xs"
                                                 href="{{ route('projects.delete.delete', [$project->id]) }}"
