0aa6808128873aaf96c07ee3f210a11a88c2013a
Robert <bruhnrp@gmail.com>
2016-04-08 13:57:53 -0400

    Added temp code for import processing. Updated to use language files.

--- resources/views/frontend/partials/process-modal.blade.php
+++ resources/views/frontend/partials/process-modal.blade.php
@@ -1,38 +1,67 @@
 <div class="modal fade" id="processModal">
     <div class="modal-dialog">
         <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h2 class="modal-title">@lang('pages.processes')</h2>
+            </div>
             <div class="modal-body">
                 @if (Auth::check())
-                    <div id="noProcess">No processes running at this time.</div>
-                    @foreach(Auth::getUser()->groups()->get() as $group)
-                        <p id="channel-{{ $group->id }}"></p>
-                    @endforeach
+                    <div><h4>@lang('pages.process_title', ['type' => 'OCR'])</h4></div>
+                    <div id="processHtml">@lang('pages.retrieve_process', ['type' => 'OCR'])</div>
+                @endif
+            </div>
+            <div class="modal-body">
+                @if (Auth::check())
+                    <div><h4>@lang('pages.process_title', ['type' => 'Import'])</h4></div>
+                    <div id="importHtml">@lang('pages.no_processes')</div>
                 @endif
             </div>
             <div class="modal-footer">
+                <span class="text-danger pull-left">@lang('pages.process_warning')</span>
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
             </div>
         </div><!-- /.modal-content -->
     </div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
-<a class='btn btn-primary btn' id="processes" data-direction='left'>Processes</a>
 <script src="http://192.168.56.20:3000/socket.io/socket.io.js"></script>
 @if (Auth::check())
     <script>
         var socket = io('http://192.168.56.20:3000');
-        @foreach(Auth::getUser()->groups()->get() as $group)
-            socket.on("channel-{{ $group->id }}:app.polling", function (message) {
+        <?php
+        $groups = Auth::getUser()->groups()->get();
+        $uuids = $groups->map(function ($item, $key) {
+            return $item['uuid'];
+        });
+        ?>
+        socket.on("ocr:app.polling", function (message) {
+            var html = '{!! trans('pages.no_processes') !!}';
+            var uuids = {!! json_encode($uuids) !!};
+
             if (jQuery.isEmptyObject(message)) {
+                $("#processHtml").text(html);
+
                 return;
             }
-            $("#noProcess").text('');
-            $.each(data, function (index) {
-                var html = data[index].projectTitle + "<br />" +
-                        "There are " + data[index].totalSubjectsAhead + " records in the queue before yours.<br />" +
-                        data[index].groupSubjectRemaining + " out of " + data[index].groupSubjectCount + "remaining to be processed";
-                $("#channel-" + data[index].groupId).text(html);
+
+            var processHtml = '';
+            var data = message.data;
+
+            $.each(data, function (index){
+                if( $.inArray(data[index].groupUuid, uuids) == -1 ) {
+                    return true;
+                }
+                processHtml += '<div class="processes"><span class="title">' + data[index].projectTitle + '</span><br />' +
+                        'Ocr Batch #' + data[index].batchId + ' - ';
+                processHtml += data[index].groupSubjectRemaining + " out of " + data[index].groupSubjectCount + " remaining to be processed";
+                if (data[index].totalSubjectsAhead > 0) {
+                    processHtml += '<br />' + data[index].totalSubjectsAhead + ' subjects being processed before this batch begins';
+                }
+                processHtml += '</div>';
             });
+
+            $("#processHtml").html(processHtml.length > 0 ? processHtml : html);
         });
-        @endforeach
+
     </script>
 @endif
