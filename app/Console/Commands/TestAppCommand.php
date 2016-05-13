6e3bec6c85a1e444e90a94a8760bd93e10bb74e0
Robert <bruhnrp@gmail.com>
2016-05-13 13:02:39 -0400

    Reset test command

--- app/Console/Commands/TestAppCommand.php
+++ app/Console/Commands/TestAppCommand.php
@@ -2,9 +2,8 @@
 
 namespace App\Console\Commands;
 
-use App\Models\Expedition;
-use App\Models\Subject;
 use Illuminate\Console\Command;
+
 
 class TestAppCommand extends Command
 {
@@ -32,19 +31,6 @@ class TestAppCommand extends Command
      */
     public function handle()
     {
-        $expeditions = Expedition::all();
-        foreach ($expeditions as $expedition)
-        {
-            $subjects = Subject::where('expedition_ids', '=', (int) $expedition->id)->get();
-            foreach ($subjects as $subject)
-            {
-                if(count($subject->expedition_ids) > 1)
-                {
-                    $subject->expedition_ids = [$expedition->id];
-                    echo 'Updating subject' . PHP_EOL;
-                    $subject->save();
-                }
-            }
-        }
+
     }
 }
