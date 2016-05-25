ce9be8181edd690e77b346ae07be0e1470b73668
Robert <bruhnrp@gmail.com>
2016-05-25 13:23:24 -0400

    Add methods for dealing with AmChart and updating stats

--- app/Models/Transcription.php
+++ app/Models/Transcription.php
@@ -1,6 +1,7 @@
 <?php namespace App\Models;
 
 use Jenssegers\Mongodb\Model as Eloquent;
+use MongoDate;
 
 class Transcription extends Eloquent
 {
@@ -36,8 +37,70 @@ class Transcription extends Eloquent
         return $this->belongsTo(Project::class);
     }
 
+    /**
+     * Mutate finished_at date for MongoDb
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public function setFinishedAtAttribute($value)
+    {
+        $this->attributes['finished_at'] = new MongoDate(strtotime($value));
+    }
+
+    /**
+     * Return finished_at in usable format
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public function getFinishedAtAttribute($value)
+    {
+        return $value->toDateTime()->format('Y-m-d H:i:s');
+    }
+
+    /**
+     * Mutate started_at for MongoDb
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public function setStartedAtAttribute($value)
+    {
+        $this->attributes['started_at'] = new MongoDate(strtotime($value));
+    }
+
+    /**
+     * Return started_at date in usable format
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public function getStartedAtAttribute($value)
+    {
+        return $value->toDateTime()->format('Y-m-d H:i:s');
+    }
+
+    /**
+     * Get count using expedition id.
+     * 
+     * @param $expeditionId
+     * @return mixed
+     */
     public function getCountByExpeditionId($expeditionId)
     {
-        return $this->whereIn('expedition_ids', [$expeditionId])->count();
+        return $this->where('expedition_id', $expeditionId)->count();
+    }
+
+    /**
+     * Get earliest finished date of transcriptions.
+     * 
+     * @param $project_id
+     * @param $expedition_id
+     * @return mixed
+     */
+    public function getEarliestDate($project_id, $expedition_id)
+    {
+        return $this->where('project_id', $project_id)->where('expedition_id', $expedition_id)->orderBy('finished_at', 'asc')->first();
     }
 }
