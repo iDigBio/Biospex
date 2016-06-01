eb26a9b2cf5387a35509ebad2f83be879c4b9ff7
Robert <bruhnrp@gmail.com>
2016-06-01 13:29:35 -0400

    Biospex-108 Social Media Feeds. Added Socialite.

--- config/services.php
+++ config/services.php
@@ -34,4 +34,16 @@ return [
         'secret' => '',
     ],
 
+    'facebook' => [
+        'client_id' => 'your-github-app-id',
+        'client_secret' => 'your-github-app-secret',
+        'redirect' => 'http://your-callback-url',
+    ],
+
+    'twitter' => [
+        'client_id' => 'your-github-app-id',
+        'client_secret' => 'your-github-app-secret',
+        'redirect' => 'http://your-callback-url',
+    ],
+
 ];
