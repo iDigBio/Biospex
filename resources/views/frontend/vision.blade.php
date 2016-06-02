3cd39afb3c47c52a79307ec6c3265601305963ac
Robert <bruhnrp@gmail.com>
2016-06-02 14:36:40 -0400

    Update to give example of new menu layout and pages

--- resources/views/frontend/vision.blade.php
+++ resources/views/frontend/vision.blade.php
@@ -178,17 +178,14 @@
 <div class="slide" id="slide5" data-slide="5" data-stellar-background-ratio="0.5">
     <div style="background: url(/vision/images/biospex_red.png) 10% 0 no-repeat; padding-top: 370px; padding-bottom: 50px;">
         <div class="container clearfix">
-            <div class="row">
-                <div class="">
-                    <p>Biospex about text.... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.
-                        Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.
-                        Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum
-                        lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
-                        inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc.</p>
-                </div>
+            <div>
+                <p>Biospex about text.... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.
+                    Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.
+                    Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum
+                    lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+                    inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc.</p>
+                @include('frontend.layouts.footer')
             </div>
-            @include('frontend.layouts.footer')
-
         </div>
     </div>
 </div>
