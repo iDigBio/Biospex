db906420674b06fea9ffaaab55b1a76cd4601c08
Robert <bruhnrp@gmail.com>
2016-06-04 21:57:56 -0400

    Create Facade for Toastr

---
+++ app/Facades/Toastr.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Toastr extends Facade
+{
+
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'toastr';
+    }
+}
