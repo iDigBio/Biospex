45eb6c2b9c3da948987ae9f79aa031cef929eede
Robert <bruhnrp@gmail.com>
2016-06-12 14:44:52 -0400

    Update spelling mistake

--- app/Http/Routes/Api/v1/group.php
+++ app/Http/Routes/Api/v1/group.php
@@ -1,8 +1,8 @@
 <?php
 
 // Groups
-$api->post('groups', ['as' => 'api.groups.create', 'uses' =>  'GoupsController@create']);
-$api->get('groups/{uuid}', ['as' => 'api.groups.show', 'uses' =>  'GoupsController@show']);
-$api->put('groups/{uuid}', ['as' => 'api.groups.update', 'uses' =>  'GoupsController@update']);
-$api->delete('groups/{uuid}', ['as' => 'api.groups.delete', 'uses' =>  'GoupsController@delete']);
-$api->get('groups', ['as' => 'api.groups.index', 'uses' =>  'GoupsController@index']);
+$api->post('groups', ['as' => 'api.groups.create', 'uses' =>  'GroupsController@create']);
+$api->get('groups/{uuid}', ['as' => 'api.groups.show', 'uses' =>  'GroupsController@show']);
+$api->put('groups/{uuid}', ['as' => 'api.groups.update', 'uses' =>  'GroupsController@update']);
+$api->delete('groups/{uuid}', ['as' => 'api.groups.delete', 'uses' =>  'GroupsController@delete']);
+$api->get('groups', ['as' => 'api.groups.index', 'uses' =>  'GroupsController@index']);
