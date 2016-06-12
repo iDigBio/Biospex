6f82decf5d6a462bf97df927dd4b3fb1b7df6e0a
Robert <bruhnrp@gmail.com>
2016-06-12 14:48:56 -0400

    Remove social network config

--- config/services.php
+++ config/services.php
@@ -33,17 +33,4 @@ return [
         'model'  => 'App\Models\User',
         'secret' => '',
     ],
-
-    'facebook' => [
-        'client_id' => 'your-github-app-id',
-        'client_secret' => 'your-github-app-secret',
-        'redirect' => 'http://your-callback-url',
-    ],
-
-    'twitter' => [
-        'client_id' => 'your-github-app-id',
-        'client_secret' => 'your-github-app-secret',
-        'redirect' => 'http://your-callback-url',
-    ],
-
 ];
