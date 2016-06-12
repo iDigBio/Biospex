ae28412cfd0f19f728a150d04c3b3243759a13ec
Robert <bruhnrp@gmail.com>
2016-06-12 14:45:14 -0400

    Save updated code. Added twitter widget

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -14,14 +14,15 @@
                 Expeditions
             </h1>
         </div>
-        <div class="container" id="home-content">
+        <div class="container-fluid" id="home-content">
+            <div class="col-md-11 col-lg-offset-1">
             <!-- Notifications -->
             @include('frontend.layouts.notifications')
                     <!-- ./ notifications -->
 
             <!-- Content -->
             <div class="row">
-                <div class="col-md-6">
+                <div class="col-md-5">
                     <h2>Liberate data from the cabinets</h2>
 
                     <p>The world's 3 billion biodiversity research specimens provide the historical baseline for
@@ -40,48 +41,60 @@
                         data goes back to the museum that curates the physical specimen.</p>
 
                     <p>Get started -> <a href="/login">Lead an Expedition</a></p>
+                    <h2 class="mailchimp">Subscribe to our mailing list</h2>
+                    <!-- Begin MailChimp Signup Form -->
+                    <div id="mc_embed_signup" style="padding: 5px 5px 5px 0px; ">
+                        <p style="font-size: 11px; font-style: italic;"><span class="asterisk">*</span> indicates required</p>
+
+                        <form
+                                action="http://idigbio.us4.list-manage.com/subscribe/post?u=5c564b4cf1e8157b450723e1c&amp;id=5aa1451449"
+                                method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate"
+                                target="_blank" novalidate>
+
+                            <div class="mc-field-group">
+                                <label for="mce-EMAIL">Email Address <span class="asterisk">*</span>
+                                </label>
+                                <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                            </div>
+                            <div class="mc-field-group">
+                                <label for="mce-FNAME">First Name </label>
+                                <input type="text" value="" name="FNAME" class="" id="mce-FNAME">
+                            </div>
+                            <div class="mc-field-group">
+                                <label for="mce-LNAME">Last Name </label>
+                                <input type="text" value="" name="LNAME" class="" id="mce-LNAME">
+                            </div>
+                            <div id="mce-responses" class="clear">
+                                <div class="response" id="mce-error-response" style="display:none"></div>
+                                <div class="response" id="mce-success-response" style="display:none"></div>
+                            </div>
+                            <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                            <div style="position: absolute; left: -5000px;">
+                                <input type="text" name="b_5c564b4cf1e8157b450723e1c_5aa1451449" tabindex="-1" value="">
+                            </div>
+                            <br/><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe"
+                                        class="button btn btn-xs btn-info">
+                        </form>
+
+                        <br clear="right"/>&nbsp;
+                    </div>
+                    <!--End mc_embed_signup-->
+                </div>
+                <div class="col-md-2">
+                </div>
+                <div class="col-md-5">
+                    <div class="panel panel-primary" style="margin-top: 50px;">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">BIOXPEXing</h3>
+                        </div>
+                        <div class="panel-body">
+                            <a class="twitter-timeline"  href="https://twitter.com/hashtag/biospex" data-widget-id="742050505711595520">#biospex Tweets</a>
+                            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+                        </div>
+                    </div>
                 </div>
             </div>
-
-
-            <h2 class="mailchimp">Subscribe to our mailing list</h2>
-            <!-- Begin MailChimp Signup Form -->
-            <div id="mc_embed_signup" style="padding: 5px 5px 5px 0px; ">
-                <p style="font-size: 11px; font-style: italic;"><span class="asterisk">*</span> indicates required</p>
-
-                <form
-                        action="http://idigbio.us4.list-manage.com/subscribe/post?u=5c564b4cf1e8157b450723e1c&amp;id=5aa1451449"
-                        method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate"
-                        target="_blank" novalidate>
-
-                    <div class="mc-field-group">
-                        <label for="mce-EMAIL">Email Address <span class="asterisk">*</span>
-                        </label>
-                        <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
-                    </div>
-                    <div class="mc-field-group">
-                        <label for="mce-FNAME">First Name </label>
-                        <input type="text" value="" name="FNAME" class="" id="mce-FNAME">
-                    </div>
-                    <div class="mc-field-group">
-                        <label for="mce-LNAME">Last Name </label>
-                        <input type="text" value="" name="LNAME" class="" id="mce-LNAME">
-                    </div>
-                    <div id="mce-responses" class="clear">
-                        <div class="response" id="mce-error-response" style="display:none"></div>
-                        <div class="response" id="mce-success-response" style="display:none"></div>
-                    </div>
-                    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                    <div style="position: absolute; left: -5000px;">
-                        <input type="text" name="b_5c564b4cf1e8157b450723e1c_5aa1451449" tabindex="-1" value="">
-                    </div>
-                    <br/><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe"
-                                class="button btn btn-xs btn-info">
-                </form>
-
-                <br clear="right"/>&nbsp;
-            </div>
-
-            <!--End mc_embed_signup-->
+                </div>
         </div>
 @stop
