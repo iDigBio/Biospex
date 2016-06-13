4b6ab80ad0333b0f66312e7122478517284ea570
Robert <bruhnrp@gmail.com>
2016-06-13 10:55:53 -0400

    Change twitter feed headline

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -85,10 +85,10 @@
                 <div class="col-md-5">
                     <div class="panel panel-primary" style="margin-top: 50px;">
                         <div class="panel-heading">
-                            <h3 class="panel-title">BIOXPEXing</h3>
+                            <h3 class="panel-title">The Buzz</h3>
                         </div>
                         <div class="panel-body">
-                            <a class="twitter-timeline"  href="https://twitter.com/hashtag/biospex" data-widget-id="742050505711595520">#biospex Tweets</a>
+                            <a class="twitter-timeline"  href="https://twitter.com/search?q=%23biospex%20%23WeDigFLPlants" data-widget-id="742050505711595520">Tweets about #biospex #WeDigFLPlants</a>
                             <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 
                         </div>
