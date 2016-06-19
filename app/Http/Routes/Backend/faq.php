b7e03874275d62ff6b72a047611e2513a8e874e6
Robert <bruhnrp@gmail.com>
2016-06-19 14:25:11 -0400

    Fixed routes

--- app/Http/Routes/Backend/faq.php
+++ app/Http/Routes/Backend/faq.php
@@ -1,45 +1,53 @@
 <?php
-$router->get('faq', [
+
+// Index
+$router->get('faqs', [
     'uses' => 'FaqsController@index',
     'as'   => 'admin.faqs.index'
 ]);
 
-$router->get('faq/create/{categories?}', [
+// Begin Faqs
+$router->get('faqs/{categories}', [
     'uses' => 'FaqsController@create',
     'as'   => 'admin.faqs.create'
 ]);
 
-$router->post('faq/create', [
+$router->post('faqs/{categories}', [
     'uses' => 'FaqsController@store',
     'as'   => 'admin.faqs.store'
 ]);
 
-$router->post('faq/createCategory', [
-    'uses' => 'FaqsController@storeCategory',
-    'as'   => 'admin.faqs.category.store'
-]);
-
-$router->get('faq/{faq}', [
-    'uses' => 'FaqsController@show',
-    'as'   => 'admin.faqs.show'
-]);
-
-$router->get('faq/{categories}/{faqs?}/edit', [
+$router->get('faqs/{categories}/{faqs}', [
     'uses' => 'FaqsController@edit',
     'as'   => 'admin.faqs.edit'
 ]);
 
-$router->put('faq/{categories}/{faqs}', [
+$router->put('faqs/{categories}/{faqs}', [
     'uses' => 'FaqsController@update',
     'as'   => 'admin.faqs.update'
 ]);
 
-$router->put('faq/{categories}', [
+
+$router->delete('faqs/{categories}/{faqs}', [
+    'uses' => 'FaqsController@delete',
+    'as'   => 'admin.faqs.delete'
+]);
+// End Faqs
+
+
+// Begin Categories
+$router->get('faqs/{categories}/{faqs}/categories', [
+    'uses' => 'FaqsController@editCategory',
+    'as'   => 'admin.faqs.categories.edit'
+]);
+
+$router->put('faqs/{categories}/{faqs}/categories', [
     'uses' => 'FaqsController@updateCategory',
     'as'   => 'admin.faqs.categories.update'
 ]);
 
-$router->delete('faq/{categories}/{faqs?}/edit', [
-    'uses' => 'FaqsController@delete',
-    'as'   => 'admin.faqs.delete'
+$router->post('faqs/create/category', [
+    'uses' => 'FaqsController@storeCategory',
+    'as'   => 'admin.faqs.category.store'
 ]);
+// End Categories
