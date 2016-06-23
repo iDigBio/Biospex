53acfc5b426da9cac61fb8d263839bc76fc6720d
Robert <bruhnrp@gmail.com>
2016-06-23 12:39:15 -0400

    Update NodeJs to use SSL

--- socket.js
+++ socket.js
@@ -1,13 +1,12 @@
-//var app = require('express')();
+var https = require('https');
+var app = require('express')();
 var fs = require('fs');
-
 var options = {
-    key: fs.readFileSync('/etc/letsencrypt/live/biospex.org/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/biospex.org/cert.pem'),
-    ca: fs.readFileSync('/etc/letsencrypt/live/biospex.org/fullchain.pem')
+    key  : fs.readFileSync('/etc/letsencrypt/live/biospex.org/privkey.pem'),
+    cert : fs.readFileSync('/etc/letsencrypt/live/biospex.org/cert.pem')
 };
 
-var server = require('https').createServer(options);
+var server = https.createServer(options, app);
 var io = require('socket.io')(server);
 io.on('connection', function(){});
 server.listen(8080);
@@ -21,3 +20,22 @@ redis.on('pmessage', function(subscribed, channel, message) {
     message = JSON.parse(message);
     io.emit(channel + ':' + message.event, message.data);
 });
+
+
+/*
+var app = require('express')();
+var server = require('https').createServer(app);
+var io = require('socket.io')(server);
+io.on('connection', function(){});
+server.listen(8080);
+
+var Redis = require('ioredis');
+var redis = new Redis();
+
+redis.psubscribe('*', function(err, count) {});
+
+redis.on('pmessage', function(subscribed, channel, message) {
+    message = JSON.parse(message);
+    io.emit(channel + ':' + message.event, message.data);
+});
+*/
