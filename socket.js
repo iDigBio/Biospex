33679cba47cc903abdb56239f33715439c9830aa
Robert <bruhnrp@gmail.com>
2016-06-23 13:02:08 -0400

    Update NodeJs to use SSL

--- socket.js
+++ socket.js
@@ -1,26 +1,33 @@
 var https = require('https');
-var app = require('express')();
 var fs = require('fs');
-var options = {
-    key  : fs.readFileSync('/etc/letsencrypt/live/biospex.org/privkey.pem'),
-    cert : fs.readFileSync('/etc/letsencrypt/live/biospex.org/cert.pem')
+var socketio = require('socket.io');
+var Redis = require('ioredis');
+
+var svrPort = 443; // This is the port of service
+var svrOptions = {
+    key: fs.readFileSync('/etc/letsencrypt/live/biospex.org/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/biospex.org/cert.pem'),
+    ca: fs.readFileSync( '/etc/letsencrypt/live/biospex.org/fullchain.pem')
 };
 
-var server = https.createServer(options, app);
-var io = require('socket.io')(server);
-io.on('connection', function(){});
-server.listen(8080);
+var servidor = https.createServer( svrOptions , function( req , res ){
+    res.writeHead(200);
+    res.end('OK');
+});
 
-var Redis = require('ioredis');
-var redis = new Redis();
+io = socketio.listen( servidor );
 
-redis.psubscribe('*', function(err, count) {});
+// Now listen in the specified Port
+servidor.listen( svrPort );
 
-redis.on('pmessage', function(subscribed, channel, message) {
+var redisService = new Redis();
+
+redisService.psubscribe('*', function(err, count) {});
+
+redisService.on('pmessage', function(subscribed, channel, message) {
     message = JSON.parse(message);
     io.emit(channel + ':' + message.event, message.data);
 });
-
 
 /*
 var app = require('express')();
