bf9ad65f060745ab8e8af632287674cd52865397
Robert <bruhnrp@gmail.com>
2016-06-30 15:01:12 -0400

    Remove queries for updating

--- app/Console/Commands/UpdateQueries.php
+++ app/Console/Commands/UpdateQueries.php
@@ -2,9 +2,6 @@
 
 namespace App\Console\Commands;
 
-use App\Repositories\Contracts\FaqCategory;
-use App\Repositories\Contracts\Group;
-use App\Repositories\Contracts\TeamCategory;
 use Illuminate\Console\Command;
 
 
@@ -20,29 +17,13 @@ class UpdateQueries extends Command
      * The console command description.
      */
     protected $description = 'Used for custom queries when updating database';
-    /**
-     * @var FaqCategory
-     */
-    private $faqCategory;
-    /**
-     * @var TeamCategory
-     */
-    private $teamCategory;
-    /**
-     * @var Group
-     */
-    private $group;
-
 
     /**
      * UpdateQueries constructor.
      */
-    public function __construct(FaqCategory $faqCategory, TeamCategory $teamCategory, Group $group)
+    public function __construct()
     {
         parent::__construct();
-        $this->faqCategory = $faqCategory;
-        $this->teamCategory = $teamCategory;
-        $this->group = $group;
     }
 
     /**
@@ -50,28 +31,6 @@ class UpdateQueries extends Command
      */
     public function handle()
     {
-        $groups = $this->group->skipCache()->all();
-        foreach ($groups as $group)
-        {
-            $group->name = $group->name === 'admins' ? 'Admin' : ucwords(str_replace('-', ' ', $group->name));
-            echo $group->name . PHP_EOL;
-            $this->group->update(['name' => $group->name], $group->id);
-        }
-        
-        $faqs = $this->faqCategory->skipCache()->all();
-        foreach ($faqs as $faq)
-        {
-            $faq->name = ucwords(str_replace('-', ' ', $faq->name));
-            echo $faq->name . PHP_EOL;
-            $this->faqCategory->update(['name' => $faq->name], $faq->id);
-        }
 
-        $teams = $this->teamCategory->skipCache()->all();
-        foreach ($teams as $team)
-        {
-            $team->name = ucwords(str_replace('-', ' ', $team->name));
-            echo $team->name . PHP_EOL;
-            $this->teamCategory->update(['name' => $team->name], $team->id);
-        }
     }
 }
