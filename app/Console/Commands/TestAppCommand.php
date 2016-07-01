08336ad0c43ba13ba3feaf9593c96eaba6794dbe
Robert <bruhnrp@gmail.com>
2016-07-01 09:04:06 -0400

    NfN APi testing

--- app/Console/Commands/TestAppCommand.php
+++ app/Console/Commands/TestAppCommand.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Client;
 
 class TestAppCommand extends Command
 {
@@ -27,6 +29,16 @@ class TestAppCommand extends Command
 
     public function fire()
     {
+        $api = 'https://panoptes-staging.zooniverse.org/oauth/authorize?response_type=token&client_id=24ad5676d5d25c6aa850dc5d5f63ec8c03dbc7ae113b6442b8571fce6c5b974c&redirect_uri=https:biospex.org';
+
+        $client = new Client();
+        $res = $client->request('get', $api);
+        echo $res->getStatusCode() . PHP_EOL;
+        // "200"
+        //echo $res->getHeader('content-type') . PHP_EOL;
+        // 'application/json; charset=utf8'
+        echo $res->getBody() . PHP_EOL;
+        // {"type":"User"...'
     }
 
 }
