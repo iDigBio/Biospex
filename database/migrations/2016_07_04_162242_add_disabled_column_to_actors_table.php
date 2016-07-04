113685f62f96037381900ebed35407de5e50112d
Robert <bruhnrp@gmail.com>
2016-07-04 14:50:40 -0400

    Add disabled column to actors table

---
+++ database/migrations/2016_07_04_162242_add_disabled_column_to_actors_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDisabledColumnToActorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('actors', function (Blueprint $table) {
+            $table->tinyInteger('disabled')->default(0)->after('private');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('actors', function (Blueprint $table) {
+            $table->dropColumn('disabled');
+        });
+    }
+}
