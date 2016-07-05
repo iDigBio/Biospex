54d91703edbeba69221799ce465d8328dbe72e2f
Robert <bruhnrp@gmail.com>
2016-07-05 08:28:05 -0400

    Change disabled to enabled.

--- database/migrations/2016_07_04_162242_add_enabled_column_to_actors_table.php
+++ database/migrations/2016_07_04_162242_add_enabled_column_to_actors_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddDisabledColumnToActorsTable extends Migration
+class AddEnabledColumnToActorsTable extends Migration
 {
     /**
      * Run the migrations.
