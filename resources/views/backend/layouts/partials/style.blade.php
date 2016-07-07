a3d82f6ace67ae5b3f06f38afa99b5ff15cfd3dd
Robert <bruhnrp@gmail.com>
2016-07-07 11:44:28 -0400

    Added icheck

--- resources/views/backend/layouts/partials/style.blade.php
+++ resources/views/backend/layouts/partials/style.blade.php
@@ -8,14 +8,17 @@
 <link href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet" type="text/css" />
 <!-- jQuery UI Theme -->
 <link rel="stylesheet" href="https://code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
+<!-- iCheck style -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/skins/minimal/blue.css">
+
+@yield('styles')
+
+<!-- Theme style -->
+<link href="{{ asset('/adminlte/css/main.css') }}" rel="stylesheet" type="text/css" />
+
 <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 <!--[if lt IE 9]>
 <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 <![endif]-->
-
-@yield('styles')
-
-<!-- Theme style -->
-<link href="{{ asset('/adminlte/css/main.css') }}" rel="stylesheet" type="text/css" />
