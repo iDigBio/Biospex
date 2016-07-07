83c6d5a2837f87804f7d812bc427840198715bbe
Robert <bruhnrp@gmail.com>
2016-07-07 11:48:35 -0400

    Split templates into sections.

--- resources/views/backend/workflows/partials/workflows.blade.php
+++ resources/views/backend/workflows/partials/workflows.blade.php
@@ -1,25 +1,24 @@
-@foreach($workflows as $workflow)
-    <tr>
-        <td>{{ $workflow->title }}</td>
-        <td>
-            @if($workflow->enabled)
-                <button class="label label-success" onClick="location.href='{{ route('admin.workflows.disable', [$workflow->id]) }}'">Enabled</button>
-            @else
-                <button class="label label-danger" onClick="location.href='{{ route('admin.workflows.enable', [$workflow->id]) }}'">Disabled</button>
-            @endif
-        </td>
-        <td class="button-fix">
-            <div class="btn-toolbar">
-                <button title="@lang('buttons.editTitle')" class="btn btn-warning btn-xs" type="button"
-                        onClick="location.href='{{ route('admin.workflows.edit', [$workflow->id]) }}'">
-                    <span class="fa fa-wrench fa-sm"></span> <!-- @lang('buttons.edit') --></button>
-
-                <button title="@lang('buttons.deleteTitle')" class="btn btn-danger btn-xs" type="button"
-                        data-toggle="confirmation" data-placement="left"
-                        data-href="{{ route('admin.workflows.delete', [$workflow->id]) }}"
-                        data-method="delete">
-                    <span class="fa fa-remove fa-sm"></span> <!-- @lang('buttons.delete') --></button>
+<div class="row">
+    <div class="col-xs-12">
+        <div class="box box-info">
+            <h3 class="box-title">Current Workflows</h3>
+            <div class="box-body table-responsive no-padding">
+                <table class="table table-hover">
+                    <colgroup>
+                        <col class="col-md-10">
+                        <col class="col-md-1">
+                        <col class="col-md-1">
+                    </colgroup>
+                    <tr>
+                        <th>Workflow</th>
+                        <th>Status</th>
+                        <th></th>
+                    </tr>
+                    @include('backend.workflows.partials.workflows-loop')
+                </table>
             </div>
-        </td>
-    </tr>
-@endforeach
+            <!-- /.box-body -->
+        </div>
+        <!-- /.box -->
+    </div>
+</div>
