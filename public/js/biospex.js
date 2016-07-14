59c5219fa553c23a416b212a62b4622a9ecce4bd
Robert <bruhnrp@gmail.com>
2016-07-14 12:20:37 -0400

    Set max records for paging grid to 500

--- public/js/biospex.js
+++ public/js/biospex.js
@@ -4782,7 +4782,7 @@ function jqBuildGrid() {
             colModel: cm,
             rowNum: 10,
             gridview: true,
-            rowList: [10, 20, 50, 100, 500, 1000],
+            rowList: [10, 20, 50, 100, 500],
             multiSort: true,
             sortable: true,
             sortorder: 'asc',
