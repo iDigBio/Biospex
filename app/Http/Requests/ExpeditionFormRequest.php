967fb9b4bb215d8de6ba4201568d0b69911be8ea
Robert <bruhnrp@gmail.com>
2016-07-19 11:04:01 -0400

    Biospex-136 Soft deleted titles. Fixes #136

--- app/Http/Requests/ExpeditionFormRequest.php
+++ app/Http/Requests/ExpeditionFormRequest.php
@@ -12,6 +12,11 @@ class ExpeditionFormRequest extends Request
         return Auth::check();
     }
 
+    /**
+     * Validation rules.
+     *
+     * @return array
+     */
     public function rules()
     {
         $rules = [
@@ -22,5 +27,19 @@ class ExpeditionFormRequest extends Request
         ];
 
         return $rules;
+    }
+
+    /**
+     * Alter group form input before validation.
+     *
+     * @return array
+     */
+    public function alterInput()
+    {
+        $input = $this->all();
+        $input['title'] = trim($input['title']);
+        $this->replace($input);
+
+        return $this->all();
     }
 }
