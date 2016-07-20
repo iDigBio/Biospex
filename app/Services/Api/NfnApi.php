01c840a3c7cb02423e2d45a7ace9481f12329769
Robert <bruhnrp@gmail.com>
2016-07-20 10:37:52 -0400

    Add functions for calling api

--- app/Services/Api/NfnApi.php
+++ app/Services/Api/NfnApi.php
@@ -28,10 +28,11 @@ class NfnApi
         $this->client = new Client(['cookies' => true, 'allow_redirects' => true]);
     }
 
-    public function auth()
+    private function auth()
     {
         $response = $this->client->request('POST', $this->config->get('config.nfnApi.auth'), [
-            'Content-Type' => 'application/x-www-form-urlencoded',
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/vnd.api+json; version=1',
             'form_params' => [
                 'grant_type'    => 'client_credentials',
                 'client_id' => $this->config->get('config.nfnApi.clientId'),
@@ -41,7 +42,29 @@ class NfnApi
 
         $json = json_decode($response->getBody());
 
-        $this->access_token = isset($json->access_toke) ? $json->access_token : null;
+        $this->access_token = isset($json->access_token) ? $json->access_token : null;
+
+        return $this->access_token;
                 
+    }
+
+    public function getClassification($id)
+    {
+        $uri = 'https://panoptes-staging.zooniverse.org/api/projects/1613';
+        $response = $this->client->request('GET', $uri, [
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/vnd.api+json; version=1',
+            'x-csrf-token' => $this->access_token,
+            'data' => $this->access_token
+        ]);
+
+        $json = json_decode($response->getBody());
+
+        return $json;
+    }
+
+    public function token()
+    {
+        return null === $this->access_token ? $this->auth() : $this->access_token;
     }
 }
