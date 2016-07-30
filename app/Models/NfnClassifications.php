440cdf552fc64d1d34cc311fdc169b23b47cf1c7
Robert <bruhnrp@gmail.com>
2016-07-30 15:56:45 -0400

    Add NfnClassification model

---
+++ app/Models/NfnClassifications.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class NfnClassification extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'nfn_classifications';
+
+    protected $fillable = [
+        'classification_id',
+        'project_id',
+        'expedition_id',
+        'finished_at'
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at', 'finished_at'];
+
+
+    /**
+     * Project relationship.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    /**
+     * Expedition relationship.
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function expedition()
+    {
+        return $this->belongsTo(Expedition::class);
+    }
+
+}
