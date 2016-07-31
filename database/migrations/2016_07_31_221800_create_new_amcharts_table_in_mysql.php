6771def4459e65f67b2c24793861b82ba605daf8
Robert <bruhnrp@gmail.com>
2016-07-31 18:20:49 -0400

    Create new table in mysql for amcharts and drop mongodb collection

---
+++ database/migrations/2016_07_31_221800_create_new_amcharts_table_in_mysql.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNewAmchartsTableInMysql extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('mongodb')->drop('amcharts');
+
+        Schema::create('amcharts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('project_id');
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+            $table->binary('data');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->engine = 'InnoDB';
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('amcharts');
+    }
+}
