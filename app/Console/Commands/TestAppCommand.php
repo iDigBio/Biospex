36ed069d7009bcdb5f2498a1228d5c92236dd33e
Robert <bruhnrp@gmail.com>
2016-08-01 15:51:20 -0400

    Set Test console back to default.

--- app/Console/Commands/TestAppCommand.php
+++ app/Console/Commands/TestAppCommand.php
@@ -2,8 +2,6 @@
 
 namespace App\Console\Commands;
 
-use App\Models\Expedition;
-use App\Repositories\Contracts\ExpeditionStat;
 use Illuminate\Console\Command;
 
 class TestAppCommand extends Command
@@ -31,16 +29,7 @@ class TestAppCommand extends Command
 
     public function fire()
     {
-        $repo = app(ExpeditionStat::class);
 
-        $stats = $repo->skipCache()->with(['expedition.project'])->get();
-        foreach ($stats as $stat)
-        {
-            echo '---' . PHP_EOL;
-            echo $stat->id . PHP_EOL;
-            echo $stat->expedition->id . PHP_EOL;
-            echo $stat->expedition->project->id . PHP_EOL;
-        }
     }
 
 }
