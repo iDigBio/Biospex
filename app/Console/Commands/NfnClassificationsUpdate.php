09781ba440ef6f88c41faaed8eb5926e7e74ef3e
Robert <bruhnrp@gmail.com>
2016-08-03 15:58:02 -0400

    Change code so command starts NfnClassificationsJob

--- app/Console/Commands/NfnClassificationsUpdate.php
+++ app/Console/Commands/NfnClassificationsUpdate.php
@@ -2,14 +2,10 @@
 
 namespace App\Console\Commands;
 
-use App\Jobs\AmChartJob;
-use App\Jobs\ExpeditionStatJob;
+use App\Jobs\NfnClassificationsJob;
 use App\Repositories\Contracts\Expedition;
-use App\Repositories\Contracts\NfnClassification;
-use App\Services\Api\NfnApi;
 use Illuminate\Console\Command;
 use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Config;
 
 class NfnClassificationsUpdate extends Command
@@ -21,41 +17,34 @@ class NfnClassificationsUpdate extends Command
      *
      * @var string
      */
-    protected $signature = 'classifications:update {id?}';
+    protected $signature = 'classifications:update {ids?}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Update NfN Classifications for Expeditions.';
+    protected $description = 'Update NfN Classifications for Expeditions. Arguement can be comma separated ids or empty.';
+
     /**
      * @var Expedition
      */
     private $expedition;
 
     /**
-     * @var NfnApi
+     * @var
      */
-    private $api;
-    /**
-     * @var NfnClassification
-     */
-    private $classification;
+    private $expeditionIds;
 
     /**
      * Create a new command instance.
      *
      * @param Expedition $expedition
-     * @param NfnClassification $classification
-     * @param NfnApi $api
      */
-    public function __construct(Expedition $expedition, NfnClassification $classification, NfnApi $api)
+    public function __construct(Expedition $expedition)
     {
         parent::__construct();
         $this->expedition = $expedition;
-        $this->api = $api;
-        $this->classification = $classification;
     }
 
     /**
@@ -65,18 +54,22 @@ class NfnClassificationsUpdate extends Command
      */
     public function handle()
     {
-        $expeditions = $this->getExpeditions();
+        $this->setIds();
 
-        $this->api->setProvider();
+        $expeditions = $this->getExpeditions();
 
         foreach ($expeditions as $expedition)
         {
-            $this->retrieveClassifications($expedition);
-
-            $this->dispatch((new ExpeditionStatJob($expedition->project->id, $expedition->id))->onQueue(Config::get('config.beanstalkd.job')));
+            $this->dispatch((new NfnClassificationsJob($expedition->id))->onQueue(Config::get('config.beanstalkd.job')));
         }
+    }
 
-        Artisan::call('amchart:update');
+    /**
+     * Set expedition ids if passed via argument.
+     */
+    private function setIds()
+    {
+        $this->expeditionIds = null ===  $this->argument('ids') ? null : explode(',', $this->argument('ids'));
     }
 
     /**
@@ -86,43 +79,8 @@ class NfnClassificationsUpdate extends Command
      */
     private function getExpeditions()
     {
-        $id = $this->argument('id');
-
-        return null === $id ?
+        return null === $this->expeditionIds ?
             $expeditions = $this->expedition->skipCache()->with(['project'])->whereNotNull('nfn_workflow_id')->get() :
-            $expeditions = $this->expedition->skipCache()->with(['project'])->where(['id' => $id])->whereNotNull('nfn_workflow_id')->get();
-    }
-
-    /**
-     * Retrieve classifications from api.
-     *
-     * @param $expedition
-     */
-    private function retrieveClassifications($expedition)
-    {
-        $pageSize = Config::get('config.expedition_size') * 3;
-        $result = json_decode($this->api->getClassifications($expedition->nfn_workflow_id, $pageSize), true);
-
-        foreach ($result['classifications'] as $classification)
-        {
-            $this->saveClassification($expedition, $classification);
-        }
-    }
-
-    /**
-     * @param $expedition
-     * @param $result
-     */
-    private function saveClassification($expedition, $result)
-    {
-        $data = [
-            'classification_id' => $result['id'],
-            'project_id'        => $expedition->project->id,
-            'expedition_id'     => $expedition->id,
-            'started_at'        => $result['metadata']['started_at'],
-            'finished_at'       => $result['metadata']['finished_at']
-        ];
-
-        $this->classification->updateOrCreate(['classification_id' => $result['id']], $data);
+            $expeditions = $this->expedition->skipCache()->with(['project'])->whereIn('id', [$this->expeditionIds])->whereNotNull('nfn_workflow_id')->get();
     }
 }
