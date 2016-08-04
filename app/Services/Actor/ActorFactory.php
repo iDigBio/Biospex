35f4b86847506e305a2ecd5bccb39baebc33c79c
Robert <bruhnrp@gmail.com>
2016-08-04 13:27:08 -0400

    Update actor factory

--- app/Services/Actor/ActorFactory.php
+++ app/Services/Actor/ActorFactory.php
@@ -6,13 +6,9 @@ use Illuminate\Support\Facades\App;
 
 class ActorFactory
 {
-    public function factory($actor)
+    public static function create($actor)
     {
         $classPath = __NAMESPACE__ . '\\' . $actor->class . '\\' . $actor->class;
-        if ( ! class_exists($classPath)) {
-            throw new \Exception(trans('emails.actor_factory_create_error', ['class' => $classPath]));
-        }
-
         $class = App::make($classPath);
         $class->process($actor);
     }
