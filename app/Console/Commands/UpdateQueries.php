49984e4e321cb2c55bebb2e622f562e2d50c5726
Robert <bruhnrp@gmail.com>
2016-08-27 14:42:18 -0400

    Update to default.

--- app/Console/Commands/UpdateQueries.php
+++ app/Console/Commands/UpdateQueries.php
@@ -2,16 +2,8 @@
 
 namespace App\Console\Commands;
 
-use App\Jobs\NfnClassificationsJob;
-use App\Repositories\Contracts\Actor;
-use App\Repositories\Contracts\Expedition;
-use App\Repositories\Contracts\NfnClassification;
-use App\Repositories\Contracts\NfnWorkflow;
-use App\Services\Api\NfnApi;
 use Illuminate\Console\Command;
 use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\DB;
 
 
 class UpdateQueries extends Command
@@ -40,52 +32,8 @@ class UpdateQueries extends Command
     /**
      * handle
      */
-    public function handle(
-        NfnApi $nfnApi,
-        Expedition $expeditionRepo,
-        NfnWorkflow $nfnWorkflowRepo
-    )
+    public function handle()
     {
 
-        $expeditions = $expeditionRepo->skipCache()->whereHas('nfnWorkflow')->get();
-
-        $nfnApi->setProvider();
-        foreach ($expeditions as $expedition)
-        {
-            $this->dispatch((new NfnClassificationsJob($expedition->id, true))->onQueue(Config::get('config.beanstalkd.job')));
-        }
-
-        $workflows = $nfnWorkflowRepo->skipCache()->with(['expedition.stat', 'expedition.actors'])->get();
-        foreach ($workflows as $workflow)
-        {
-            if ((int) $workflow->expedition->stat->transcriptions_completed === 100)
-            {
-                foreach ($workflow->expedition->actors as $actor)
-                {
-                    if ($actor->id === 2)
-                    {
-                        $actor->pivot->queued = 0;
-                        ++$actor->pivot->state;
-                        $actor->completed = 1;
-                        $actor->pivot->save();
-                    }
-                }
-            }
-            else
-            {
-                foreach ($workflow->expedition->actors as $actor)
-                {
-                    if ($actor->id === 2)
-                    {
-                        $actor->pivot->queued = 0;
-                        $actor->pivot->state = 1;
-                        $actor->completed = 0;
-                        $actor->error = 0;
-                        $actor->pivot->save();
-                    }
-                }
-            }
-
-        }
     }
 }
