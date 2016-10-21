09f34627f42f25f51eb97958f11e01e6a22901e3
Robert <bruhnrp@gmail.com>
2016-10-20 22:57:05 -0400

    Try fix for export

--- config/queue.php
+++ config/queue.php
@@ -46,7 +46,7 @@ return [
             'driver' => 'beanstalkd',
             'host'   => 'localhost',
             'queue'  => 'default',
-            'ttr'    => 60,
+            'ttr'    => env('QUEUE_TTR', 3600),
         ],
 
         'sqs' => [
