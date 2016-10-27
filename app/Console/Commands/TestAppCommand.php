8c5774a7999e535dbb4e32de189a61184ced7aa5
Robert <bruhnrp@gmail.com>
2016-10-27 18:37:26 -0400

    Temp fix

--- app/Console/Commands/TestAppCommand.php
+++ app/Console/Commands/TestAppCommand.php
@@ -2,6 +2,8 @@
 
 namespace App\Console\Commands;
 
+use App\Repositories\Contracts\Group;
+use App\Repositories\Contracts\User;
 use Illuminate\Console\Command;
 
 class TestAppCommand extends Command
@@ -16,17 +18,46 @@ class TestAppCommand extends Command
      * The console command description.
      */
     protected $description = 'Used to test code';
+    /**
+     * @var Group
+     */
+    private $group;
+    /**
+     * @var User
+     */
+    private $user;
 
     /**
      * TestAppCommand constructor.
+     * @param Group $group
+     * @param User $user
      */
-    public function __construct()
+    public function __construct(Group $group, User $user)
     {
         parent::__construct();
+        $this->group = $group;
+        $this->user = $user;
     }
 
     public function fire()
     {
+        $user = $this->user->find(1);
+
+        $groups = $this->group->all();
+
+        foreach ($groups as $group)
+        {
+            if ($user->hasGroup($group))
+            {
+                echo 'Has group . ' . $group->name . PHP_EOL;
+            }
+            else
+            {
+                echo 'Assigning group . ' . $group->name . PHP_EOL;
+                $user->assignGroup($group);
+            }
+        }
+
 
     }
 }
