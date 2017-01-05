38eb61cc3addaacf921ac355a89225b71451385e
Robert <bruhnrp@gmail.com>
2017-01-05 13:07:15 -0500

    Fix no project page

--- resources/views/frontend/projects/index.blade.php
+++ resources/views/frontend/projects/index.blade.php
@@ -10,22 +10,22 @@
 @section('content')
     @if ($groups->isEmpty())
         <div class="col-md-10 col-md-offset-1">
-            {{ trans('html.welcome') }}
+            {!!  trans('html.welcome') !!}
         </div>
         <div class="col-md-10 col-md-offset-1">
-            {{ trans('html.new-user-intro') }}
-            <button class="btn btn-success" title="@lang('buttons.createTitleG')"
-                    onClick="location.href='{{ route('web.groups.create') }}'"><span
-                        class="glyphicon glyphicon-plus"></span> @lang('buttons.create')</button>
-        </div>
-    @else
-        <h3>{{ trans('projects.projects') }}
-            <button title="@lang('buttons.createTitleP')" class="btn btn-success"
-                    onClick="location.href='{{ route('web.projects.create') }}'"><span
-                        class="fa fa-plus fa-lg"></span> @lang('buttons.create')</button>
-        </h3>
-        @include('frontend.projects.partials.project-table')
-        <h3>{{ trans('pages.trash') }}</h3>
-        @include('frontend.projects.partials.project-trashed-table')
-    @endif
+        {!! trans('html.new-user-intro') !!}
+        <button class="btn btn-success" title="@lang('buttons.createTitleG')"
+                onClick="location.href='{{ route('web.groups.create') }}'"><span
+                    class="glyphicon glyphicon-plus"></span> @lang('buttons.create')</button>
+    </div>
+@else
+    <h3>{!! trans('projects.projects') !!}
+        <button title="@lang('buttons.createTitleP')" class="btn btn-success"
+                onClick="location.href='{{ route('web.projects.create') }}'"><span
+                    class="fa fa-plus fa-lg"></span> @lang('buttons.create')</button>
+    </h3>
+    @include('frontend.projects.partials.project-table')
+    <h3>{!! trans('pages.trash') !!}</h3>
+    @include('frontend.projects.partials.project-trashed-table')
+@endif
 @stop
