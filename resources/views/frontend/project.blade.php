285e2acf794a5c58e2c2192a4da856b2945d3a24
Robert <bruhnrp@gmail.com>
2017-01-12 12:46:07 -0500

    Fix error in expeditions being null

--- resources/views/frontend/project.blade.php
+++ resources/views/frontend/project.blade.php
@@ -44,26 +44,28 @@
                     </tr>
                     </thead>
                     <tbody>
-                    @foreach ($expeditions as $expedition)
-                        <tr>
-                            <td>{{ $expedition->title }}</td>
-                            @if( ! $expedition->actors->isEmpty())
-                                <td class="nowrap">
+                    @if (null !== $expeditions)
+                        @foreach ($expeditions as $expedition)
+                            <tr>
+                                <td>{{ $expedition->title }}</td>
+                                @if( ! $expedition->actors->isEmpty())
+                                    <td class="nowrap">
                                         <span class="complete">
                                         <span class="complete{{ round_up_to_any_five($expedition->stat->percent_completed) }}">&nbsp;</span>
                                         </span> {{ $expedition->stat->percent_completed }}%
+                                    </td>
+                                @else
+                                    <td class="nowrap"
+                                        colspan="3">{{ trans('expeditions.processing_not_started') }}</td>
+                                @endif
+                                <td>
+                                    @foreach($expedition->actors as $actor)
+                                        <a href="{{ $actor->url }}">{{ $actor->title }}</a>&nbsp;&nbsp;
+                                    @endforeach
                                 </td>
-                            @else
-                                <td class="nowrap"
-                                    colspan="3">{{ trans('expeditions.processing_not_started') }}</td>
-                            @endif
-                            <td>
-                                @foreach($expedition->actors as $actor)
-                                    <a href="{{ $actor->url }}">{{ $actor->title }}</a>&nbsp;&nbsp;
-                                @endforeach
-                            </td>
-                        </tr>
-                    @endforeach
+                            </tr>
+                        @endforeach
+                    @endif
                     </tbody>
                 </table>
             </div>
