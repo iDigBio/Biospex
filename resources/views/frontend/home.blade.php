11739bfa2b8564434360a846797e17dce5043f91
Robert <bruhnrp@gmail.com>
2017-01-13 09:46:04 -0500

    Adding Carousel

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -11,6 +11,46 @@
         <div id="splash">
             <img src="/img/logo.png" alt="biospex"/>
             {!! trans('html.homepage-header') !!}
+            <div class="pull-right">
+                <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                    <!-- Indicators -->
+                    <ol class="carousel-indicators">
+                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#myCarousel" data-slide-to="1"></li>
+                        <li data-target="#myCarousel" data-slide-to="2"></li>
+                        <li data-target="#myCarousel" data-slide-to="3"></li>
+                    </ol>
+
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner" role="listbox">
+                        <div class="item active">
+                            <img src="img_chania.jpg" alt="Chania">
+                        </div>
+
+                        <div class="item">
+                            <img src="img_chania2.jpg" alt="Chania">
+                        </div>
+
+                        <div class="item">
+                            <img src="img_flower.jpg" alt="Flower">
+                        </div>
+
+                        <div class="item">
+                            <img src="img_flower2.jpg" alt="Flower">
+                        </div>
+                    </div>
+
+                    <!-- Left and right controls -->
+                    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </div>
         </div>
         <div class="container-fluid" id="home-content">
             <div class="col-md-11 col-lg-offset-1">
