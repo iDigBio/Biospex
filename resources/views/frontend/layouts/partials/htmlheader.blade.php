b70bc6b12c67dbd3a213303276d9ea04cc9e5064
Robert <bruhnrp@gmail.com>
2017-02-01 11:53:06 -0500

    Added html for google verfication

--- resources/views/frontend/layouts/partials/htmlheader.blade.php
+++ resources/views/frontend/layouts/partials/htmlheader.blade.php
@@ -3,6 +3,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="csrf-param" content="_token">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="google-site-verification" content="DRVQlYZQo5OkUlUhNG8Re-CgYEB7ELA0I_3qJJlzb0U" />
     <title>
         @section('title')
         @show
