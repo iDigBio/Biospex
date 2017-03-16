7b08ee7b9edc2da055d4b13a0f65f32077d344a4
Robert <bruhnrp@gmail.com>
2017-03-15 22:10:53 -0400

    Test fix

---
+++ resources/views/frontend/layouts/test.blade.php
@@ -0,0 +1,12 @@
+@extends('frontend.layouts.default')
+
+{{-- Web site Title --}}
+@section('title')
+    @parent
+    @lang('projects.projects')
+@stop
+
+{{-- Content --}}
+@section('content')
+    Hello
+@stop
