606eec1b5d18ee59c9178d1940cb4f1a7623db72
Robert <bruhnrp@gmail.com>
2017-03-27 20:56:21 -0400

    Update for creating and managing Fusion tables.

--- app/Console/Commands/TestAppCommand.php
+++ app/Console/Commands/TestAppCommand.php
@@ -61,7 +61,7 @@ class TestAppCommand extends Command
 
     public function handle()
     {
-        $service = Google::make('fusiontables_column');
+        $service = Google::make('fusiontables');
     }
 
 }
