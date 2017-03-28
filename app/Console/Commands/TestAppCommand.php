44e50bade07b9250be6ea2e57ab4d59e6ce24360
Robert <bruhnrp@gmail.com>
2017-03-27 20:44:54 -0400

    Update for creating and managing Fusion tables.

--- app/Console/Commands/TestAppCommand.php
+++ app/Console/Commands/TestAppCommand.php
@@ -11,6 +11,7 @@ use App\Services\Google\Drive;
 use Illuminate\Console\Command;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use App\Services\Google\Table;
+use PulkitJalan\Google\Facades\Google;
 
 class TestAppCommand extends Command
 {
@@ -53,35 +54,14 @@ class TestAppCommand extends Command
     /**
      * TestAppCommand constructor.
      */
-    public function __construct(
-        ProjectContract $projectContract,
-        TranscriptionLocationContract $locationContract,
-        Table $table,
-        Bucket $bucket,
-        Drive $drive
-    )
+    public function __construct()
     {
         parent::__construct();
-
-        $this->projectContract = $projectContract;
-        $this->locationContract = $locationContract;
-        $this->table = $table;
-        $this->bucket = $bucket;
-        $this->drive = $drive;
     }
 
     public function handle()
     {
-        $ids = [13,15,16,17,18,26,31,33];
-
-        $this->job = new NfnClassificationsFusionTableJob($ids);
-        $this->job->handle(
-            $this->projectContract,
-            $this->locationContract,
-            $this->table,
-            $this->bucket,
-            $this->drive
-        );
+        $service = Google::make('fusiontables_column');
     }
 
 }
