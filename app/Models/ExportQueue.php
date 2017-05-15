b84e0d8337d589473a33a97119474924e0c08520
Robert <bruhnrp@gmail.com>
2017-05-15 13:20:09 -0400

    Add export queue model

--- app/Models/ExportQueue.php
+++ app/Models/ExportQueue.php
@@ -2,11 +2,11 @@
 
 namespace App\Models;
 
+
 class ExportQueue extends BaseEloquentModel
 {
-
     /**
-     * @inheritDoc
+     * @ineritDoc
      */
     protected $table = 'export_queues';
 
@@ -15,38 +15,10 @@ class ExportQueue extends BaseEloquentModel
      */
     protected $fillable = [
         'expedition_id',
-        'state',
+        'actor_id',
+        'stage',
         'queued',
         'missing'
     ];
 
-    /**
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
-    public function expedition()
-    {
-        return $this->belongsTo(Expedition::class);
-    }
-
-    /**
-     * Set queued attribute.
-     *
-     * @param $value
-     * @return int
-     */
-    public function getQueuedAttribute($value)
-    {
-        return $value === null ? 0 : $value;
-    }
-
-    /**
-     * Set state attribute.
-     *
-     * @param $value
-     * @return int
-     */
-    public function getStateAttribute($value)
-    {
-        return $value === null ? 0 : $value;
-    }
 }
