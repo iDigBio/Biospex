c7c431a36b71fe04e243d58cc55d55dfe6256f6e
Robert <bruhnrp@gmail.com>
2017-05-15 08:43:45 -0400

    Add checkForQueuedJob method

--- app/Repositories/Contracts/ExportJobQueueContract.php
+++ app/Repositories/Contracts/ExportJobQueueContract.php
@@ -5,4 +5,10 @@ namespace App\Repositories\Contracts;
 interface ExportJobQueueContract extends RepositoryContract, CacheableContract
 {
 
+    /**
+     * Check for queued job.
+     *
+     * @return int
+     */
+    public function checkForQueuedJob();
 }
