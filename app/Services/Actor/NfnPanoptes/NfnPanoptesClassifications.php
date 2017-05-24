462fa42f446585a7a9538b4ed61c3e94c78f77b0
Robert <bruhnrp@gmail.com>
2017-05-24 17:52:26 -0400

    Update all files

--- app/Services/Actor/NfnPanoptes/NfnPanoptesClassifications.php
+++ app/Services/Actor/NfnPanoptes/NfnPanoptesClassifications.php
@@ -9,6 +9,7 @@ use App\Jobs\NfnClassificationsUpdateJob;
 use App\Services\Actor\ActorInterface;
 use App\Services\Actor\ActorService;
 use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Contracts\Events\Dispatcher as Event;
 
 class NfnPanoptesClassifications implements ActorInterface
 {
@@ -24,17 +25,22 @@ class NfnPanoptesClassifications implements ActorInterface
      * @var \App\Services\Actor\ActorRepositoryService
      */
     private $actorRepoService;
+    /**
+     * @var Event
+     */
+    private $dispatcher;
 
     /**
      * NfnPanoptesClassifications constructor.
      *
      * @param ActorService $service
+     * @param Event $dispatcher
      */
-    public function __construct(ActorService $service
-    )
+    public function __construct(ActorService $service, Event $dispatcher)
     {
         $this->service = $service;
         $this->actorRepoService = $service->actorRepoService;
+        $this->dispatcher = $dispatcher;
     }
 
     /**
@@ -80,6 +86,7 @@ class NfnPanoptesClassifications implements ActorInterface
             $actor->pivot->queued = 0;
             $actor->completed = 1;
             $actor->pivot->save();
+            $this->dispatcher->fire('actor.pivot.updated');
 
             $this->sendReport($record);
         }
