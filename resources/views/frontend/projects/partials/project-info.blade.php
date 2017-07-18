b3bd4f4267c16bd4fc6bab5db6b5b7122b3029d2
Robert <bruhnrp@gmail.com>
2017-07-18 10:36:08 -0400

    Fix transcript button permission

--- resources/views/frontend/projects/partials/project-info.blade.php
+++ resources/views/frontend/projects/partials/project-info.blade.php
@@ -43,13 +43,13 @@
                 <button title="@lang('buttons.advertiseTitle')" class="btn btn-success btn-sm" type="button"
                         onClick="location.href='{{ route('web.advertises.index', [$project->id]) }}'"><span
                             class="fa fa-globe fa-lrg"></span> @lang('buttons.advertise')</button>
-                @if(request()->user()->id === $project->group->user_id)
+                @can('isOwner', $project->group)
                     <button title="@lang('buttons.projectStatsTitle')" class="btn btn-success btn-sm"
                             type="button"
                             onClick="location.href='{{ route('web.projects.stats', [$project->id]) }}'">
                         <span class="fa fa-bar-chart fa-lrg"></span> @lang('buttons.projectStats')
                     </button>
-                @endif
+                @endcan
             </div>
         </div>
     </div>
