cea3d6434029f2dc20f6d2cb84ca1bd163edb62b
Robert <bruhnrp@gmail.com>
2017-08-01 13:04:41 -0400

    Testing error in fusion tables

--- app/Console/Commands/TestAppCommand.php
+++ app/Console/Commands/TestAppCommand.php
@@ -3,6 +3,9 @@
 namespace App\Console\Commands;
 
 use App\Jobs\NfnClassificationsFusionTableJob;
+use App\Repositories\Contracts\ProjectContract;
+use App\Repositories\Contracts\TranscriptionLocationContract;
+use App\Services\Google\FusionTableService;
 use Illuminate\Console\Command;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 
@@ -20,13 +23,28 @@ class TestAppCommand extends Command
      * The console command description.
      */
     protected $description = 'Used to test code';
+    /**
+     * @var ProjectContract
+     */
+    private $projectContract;
+    /**
+     * @var TranscriptionLocationContract
+     */
+    private $transcriptionLocationContract;
+    /**
+     * @var FusionTableService
+     */
+    private $fusionTableService;
 
     /**
      * TestAppCommand constructor.
      */
-    public function __construct()
+    public function __construct(ProjectContract $projectContract, TranscriptionLocationContract $transcriptionLocationContract, FusionTableService $fusionTableService)
     {
         parent::__construct();
+        $this->projectContract = $projectContract;
+        $this->transcriptionLocationContract = $transcriptionLocationContract;
+        $this->fusionTableService = $fusionTableService;
     }
 
     /**
@@ -36,7 +54,8 @@ class TestAppCommand extends Command
     {
         echo 'Sending to job' . PHP_EOL;
 
-        new NfnClassificationsFusionTableJob();
+        $job = new NfnClassificationsFusionTableJob();
+        $job->handle($this->projectContract, $this->transcriptionLocationContract, $this->fusionTableService);
 
         echo 'Finished' . PHP_EOL;
     }
