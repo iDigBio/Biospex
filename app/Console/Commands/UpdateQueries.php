53d76e72af65e49fb5566a04610c9be56c9643eb
Robert <bruhnrp@gmail.com>
2017-08-02 15:34:03 -0400

    Add update queries for downloads

--- app/Console/Commands/UpdateQueries.php
+++ app/Console/Commands/UpdateQueries.php
@@ -2,6 +2,8 @@
 
 namespace App\Console\Commands;
 
+use App\Services\Model\DownloadService;
+use File;
 use Illuminate\Console\Command;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 
@@ -20,13 +22,19 @@ class UpdateQueries extends Command
      * The console command description.
      */
     protected $description = 'Used for custom queries when updating database';
+    /**
+     * @var DownloadService
+     */
+    private $downloadService;
 
     /**
      * UpdateQueries constructor.
+     * @param DownloadService $downloadService
      */
-    public function __construct()
+    public function __construct(DownloadService $downloadService)
     {
         parent::__construct();
+        $this->downloadService = $downloadService;
     }
 
     /**
@@ -34,6 +42,54 @@ class UpdateQueries extends Command
      */
     public function fire()
     {
+        $files = File::allFiles(config('config.classifications_download'));
+        foreach ($files as $file)
+        {
+            $id = basename($file, '.csv');
 
+            $csvPath = config('config.classifications_download') . '/' . $id . '.csv';
+            $recPath = config('config.classifications_reconcile') . '/' . $id . '.csv';
+            $tranPath = config('config.classifications_transcript') . '/' . $id . '.csv';
+            $sumPath = config('config.classifications_summary') . '/' . $id . '.html';
+
+            if (File::exists($csvPath))
+            {
+                $this->updateOrCreateDownloads($id, 'classifications');
+            }
+
+            if (File::exists($tranPath))
+            {
+                $this->updateOrCreateDownloads($id, 'transcriptions');
+            }
+
+            if (File::exists($recPath))
+            {
+                $this->updateOrCreateDownloads($id, 'reconciled');
+            }
+
+            if (File::exists($sumPath))
+            {
+                $this->updateOrCreateDownloads($id, 'summary');
+            }
+
+        }
+    }
+
+    public function updateOrCreateDownloads($expeditionId, $type)
+    {
+        $values = [
+            'expedition_id' => $expeditionId,
+            'actor_id' => 2,
+            'file' => $expeditionId . '.csv',
+            'type' => $type
+        ];
+        $attributes = [
+            'expedition_id' => $expeditionId,
+            'actor_id' => 2,
+            'file' => $expeditionId . '.csv',
+            'type' => $type
+        ];
+
+        $this->downloadService->updateOrCreate($attributes, $values);
     }
 }
