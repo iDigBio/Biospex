abcb0f888d88b220d9a993a6864593acf772f76a
Robert <bruhnrp@gmail.com>
2017-08-16 08:59:09 -0400

    Update breadcrumbs and add table for transcribers

--- resources/views/frontend/statistics/index.blade.php
+++ resources/views/frontend/statistics/index.blade.php
@@ -2,28 +2,34 @@
 {{-- Web site Title --}}
 @section('title')
     @parent
-    {{ $project->title }}
+    {{ $project->title }} @lang('pages.project_stats')
 @stop
 
 {{-- Content --}}
 @section('content')
-    {!! Breadcrumbs::render('web.projects.show', $project) !!}
+    {!! Breadcrumbs::render('web.projects.show.title', $project, trans('pages.project_stats')) !!}
     @include('frontend.statistics.partials.project-info')
     <div class="row">
-        <div class="col-md-3">
+        <div class="col-md-6">
             <div class="panel panel-info">
                 <div class="panel-heading panel-title">
                     {{ trans('pages.transcriber_summary') }}
                     <i class="fa fa-expand pull-right"></i>
                 </div>
                 <div id="transcribers" class="panel-body">
-                    <table class="transcribers">
-                    @each('frontend.statistics.partials.transcriber', $transcribers, 'transcriber')
+                    <table class="table-responsive">
+                        <tr>
+                            <th>User</th>
+                            <th>Expeditions</th>
+                            <th>Transcriptions</th>
+                            <th>Last Date</th>
+                        </tr>
+                        @each('frontend.statistics.partials.transcriber', $transcribers, 'transcriber')
                     </table>
                 </div>
             </div>
         </div>
-        <div class="col-md-9">
+        <div class="col-md-6">
             <div id="chartTranscriptionsDiv"></div>
         </div>
     </div>
