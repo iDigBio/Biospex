d3b7db13f7e55e8501ea91d192d67b5e995d0c6c
Robert <bruhnrp@gmail.com>
2017-08-16 08:48:00 -0400

    Altered to retrieve transcription information

--- app/Repositories/Eloquent/PanoptesTranscriptionRepository.php
+++ app/Repositories/Eloquent/PanoptesTranscriptionRepository.php
@@ -9,6 +9,7 @@ use Illuminate\Contracts\Container\Container;
 
 class PanoptesTranscriptionRepository extends EloquentRepository implements PanoptesTranscriptionContract
 {
+
     /**
      * PanoptesTranscriptionRepository constructor.
      * @param Container $container
@@ -89,8 +90,7 @@ class PanoptesTranscriptionRepository extends EloquentRepository implements Pano
      */
     public function getTranscriptionCountPerDate($workflowId)
     {
-        return $this->raw(function ($collection) use ($workflowId)
-        {
+        return $this->raw(function ($collection) use ($workflowId) {
             return $collection->aggregate(
                 [
                     ['$match' => ['workflow_id' => $workflowId]],
@@ -121,11 +121,44 @@ class PanoptesTranscriptionRepository extends EloquentRepository implements Pano
      */
     public function getUserTranscriptionCount($projectId)
     {
-        $result = $this->raw(function($collection) use ($projectId) {
+        $result = $this->raw(function ($collection) use ($projectId) {
             return $collection->aggregate(
                 [
-                    ['$match' => ['subject_projectId' => (int) $projectId]],
-                    ['$group' => ['_id' => '$user_name', 'count' => ['$sum' => 1]]]
+                    [
+                        '$match' => [
+                            'subject_projectId' => (int) $projectId
+                        ]
+                    ],
+                    [
+                        '$sort' => [
+                            'classification_finished_at' => -1
+                        ]
+                    ],
+                    [
+                        '$group' => [
+                            '_id'                => '$user_name',
+                            'transcriptionCount' => [
+                                '$sum' => 1
+                            ],
+                            'expedition'         => [
+                                '$addToSet' => '$subject_expeditionId'
+                            ],
+                            'last_date'          => [
+                                '$first' => '$classification_finished_at'
+                            ]
+                        ]
+                    ],
+                    [
+                        '$project' => [
+                            '_id'                => 0,
+                            'user_name'          => '$_id',
+                            'transcriptionCount' => 1,
+                            'expeditionCount'    => [
+                                '$size' => '$expedition'
+                            ],
+                            'last_date'          => 1
+                        ]
+                    ]
                 ]);
         });
 
