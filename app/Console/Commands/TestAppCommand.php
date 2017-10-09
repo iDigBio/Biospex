fb1193c3bdb203abe1b92f4471e739ef337f9f08
Robert <bruhnrp@gmail.com>
2017-10-09 14:24:05 -0400

    Reset to normal

--- app/Console/Commands/TestAppCommand.php
+++ app/Console/Commands/TestAppCommand.php
@@ -2,7 +2,6 @@
 
 namespace App\Console\Commands;
 
-use App\Repositories\Contracts\SubjectContract;
 use Illuminate\Console\Command;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 
@@ -10,7 +9,6 @@ class TestAppCommand extends Command
 {
 
     use DispatchesJobs;
-    public $subjectContract;
 
     /**
      * The console command name.
@@ -25,10 +23,9 @@ class TestAppCommand extends Command
     /**
      * TestAppCommand constructor.
      */
-    public function __construct(SubjectContract $subjectContract)
+    public function __construct()
     {
         parent::__construct();
-        $this->subjectContract = $subjectContract;
     }
 
     /**
@@ -36,10 +33,5 @@ class TestAppCommand extends Command
      */
     public function handle()
     {
-        $subjects = $this->subjectContract->findWhere(['project_id', '=', 55]);
-        $subjects->each(function($subject){
-            $array[] = 91;
-            $this->subjectContract->update($subject->_id, ['expedition_ids' => $array]);
-        });
     }
 }
