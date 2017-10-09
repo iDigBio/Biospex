65b3d59c59e6c9980b8b888e3fa1c9c9a663d471
Robert <bruhnrp@gmail.com>
2017-10-09 14:42:40 -0400

    Reset to normal

--- app/Console/Commands/TestAppCommand.php
+++ app/Console/Commands/TestAppCommand.php
@@ -2,13 +2,21 @@
 
 namespace App\Console\Commands;
 
+use App\Repositories\Contracts\ExpeditionContract;
+use App\Repositories\Contracts\ExportQueueContract;
 use Illuminate\Console\Command;
+use Illuminate\Contracts\Bus\Dispatcher;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 
 class TestAppCommand extends Command
 {
 
     use DispatchesJobs;
+    public $nfnActors;
+    public $expeditionContract;
+    public $dispatcher;
+    public $exportQueueContract;
+
 
     /**
      * The console command name.
@@ -23,9 +31,17 @@ class TestAppCommand extends Command
     /**
      * TestAppCommand constructor.
      */
-    public function __construct()
+    public function __construct(
+        ExpeditionContract $expeditionContract,
+        Dispatcher $dispatcher,
+        ExportQueueContract $exportQueueContract
+    )
     {
         parent::__construct();
+        $this->nfnActors = explode(',', config('config.nfnActors'));
+        $this->expeditionContract = $expeditionContract;
+        $this->dispatcher = $dispatcher;
+        $this->exportQueueContract = $exportQueueContract;
     }
 
     /**
@@ -33,5 +49,51 @@ class TestAppCommand extends Command
      */
     public function handle()
     {
+        $records = $this->exportQueueContract->setCacheLifetime(0)
+            ->orderBy('id', 'asc')
+            ->findAll();
+
+        if ($records->isEmpty())
+        {
+            $data = trans('pages.processing_empty');
+            $this->dispatcher->fire(new PollExportEvent($data));
+
+            return;
+        }
+
+        $count = 0;
+        $data = $records->map(function($record) use ($count) {
+
+            $queue = $this->exportQueueContract->setCacheLifetime(0)
+                ->findQueueProcessData($record->id, $record->expedition_id, $record->actor_id);
+
+            $actorClass = ActorFactory::create($queue->expedition->actor->class, $queue->expedition->actor->class . 'Export');
+            $stage = $actorClass->stage[$queue->stage];
+            $processed = $queue->expedition->actor->pivot->processed === 0 ? 1 : $queue->expedition->actor->pivot->processed;
+            $total = $queue->expedition->actor->pivot->total;
+            $count++;
+
+            $message = $record->queued ?
+                trans('pages.export_processing', [
+                    'stage' => camelCaseToWords($stage),
+                    'title' => $queue->expedition->title,
+                    'processedRecords' => trans_choice('pages.processed_records', $processed, [
+                        'processed' => $processed,
+                        'total' => $total
+                    ]),
+                ]) :
+                trans_choice('pages.export_queued', $count, [
+                    'title' => $queue->expedition->title,
+                    'count' => $count
+                ]);
+
+            return [
+                'groupUuid'       => $queue->expedition->project->group->uuid,
+                'message'         => $message
+            ];
+        });
+
+        dd($data);
+
     }
 }
