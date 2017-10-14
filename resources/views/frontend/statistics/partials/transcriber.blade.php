b3306780b6b83cb68b0529d2354cbb87c2a4beba
Robert <bruhnrp@gmail.com>
2017-10-14 09:21:50 -0400

    Replaces date with helper function

--- resources/views/frontend/statistics/partials/transcriber.blade.php
+++ resources/views/frontend/statistics/partials/transcriber.blade.php
@@ -2,5 +2,5 @@
     <td>{{ $transcriber['user_name'] }}</td>
     <td>{{ $transcriber['expeditionCount'] }}</td>
     <td>{{ $transcriber['transcriptionCount'] }}</td>
-    <td>{{ date('Y-m-d', $transcriber['last_date']->sec) }}</td>
+    <td>{{ mongodb_date_format($transcriber['last_date']) }}</td>
 </tr>
