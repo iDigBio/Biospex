a516ee2a538da0f69411f54d4e188dec523fa1e2
Robert <bruhnrp@gmail.com>
2017-10-17 16:00:57 -0400

    Add local socket.io.js

--- socket.js
+++ socket.js
@@ -1,16 +1,16 @@
-
+/*
 var app = require('express')();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 io.on('connection', function(){});
 server.listen(8080);
+*/
 
-/*
 var express = require('express');
 var app = express();
 var server = app.listen(8080);
 var io = require('socket.io').listen(server);
-*/
+
 var Redis = require('ioredis');
 var redis = new Redis();
 
