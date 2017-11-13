829576ab23707e89009b6eb9c23e774aea7149e5
Robert <bruhnrp@gmail.com>
2017-11-13 11:01:53 -0500

    Add migrations

---
+++ database/migrations/2017_11_13_145222_remove_unused_tables.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RemoveUnusedTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('api_subscribers');
+        Schema::dropIfExists('user_grid_field');
+        Schema::dropIfExists('user_grid_fields');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+}
