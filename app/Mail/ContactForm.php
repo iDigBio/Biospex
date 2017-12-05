6b913fbdfc81822fa4c0d8cac5a9d750cda0ea35
Robert <bruhnrp@gmail.com>
2017-12-05 16:18:19 -0500

    Add contact form mailable

---
+++ app/Mail/ContactForm.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class ContactForm extends Mailable implements ShouldQueue
+{
+    use Queueable, SerializesModels;
+
+    private $contact;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param $contact
+     */
+    public function __construct($contact)
+    {
+        $this->contact = $contact;
+
+        $this->onQueue(config('beanstalkd.default'));
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('mail.contact')->with('contact', $this->contact);
+    }
+}
