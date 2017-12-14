c55d9c05b1b543ae9593b576210954299878d1dc
Robert <bruhnrp@gmail.com>
2017-12-14 12:36:17 -0500

    Removed code for ajax query to expeditions. Folders were removed.

--- resources/assets/js/biospex.js
+++ resources/assets/js/biospex.js
@@ -1,72 +1,58 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    $('.toggle').on('click', function () {
-        if ($(this).hasClass('fa-folder')) {
-            $(this).removeClass("fa-folder").addClass("fa-folder-open");
-            var curRow = $(this).closest('tr');
-            var newRow = '<tr class="ajax-rows"><td></td><td colspan="4"><span id="row'+this.id+'"></span></td></tr>';
-            curRow.after(newRow);
-            $("#row"+this.id).load("/projects/"+this.id+"/expeditions");
-        } else {
-            $(this).removeClass("fa-folder-open").addClass("fa-folder");
-            $(this).closest('tr').next('tr').remove();
-        }
+    // If process modal, prevent click and return without active class
+    // get current URL path and assign 'active' class
+    let href = window.location.href;
+    $('.nav > li > a[href="'+href+'"]').parent().addClass('active');
+
+    $(".preventDefault").click(function (e) {
+        e.preventDefault();
     });
 
-    $(".table-sort").tablesorter({
+    let tableSort = $(".table-sort");
+    tableSort.tablesorter({
         // this will apply the bootstrap theme if "uitheme" widget is included
         // the widgetOptions.uitheme is no longer required to be set
-        theme : "bootstrap",
-
-        widthFixed: false,
-
-        headerTemplate : '{content} {icon}', // new in v2.7. Needed to add the bootstrap icon!
-
-        // widget code contained in the jquery.tablesorter.widgets.js file
-        // use the zebra stripe widget if you plan on hiding any rows (filter widget)
-        widgets : [ "uitheme", "zebra" ],
-
-        widgetOptions : {
-            // using the default zebra striping class name, so it actually isn't included in the theme variable above
-            // this is ONLY needed for bootstrap theming if you are using the filter widget, because rows are hidden
-            zebra : ["even", "odd"],
-            // reset filters button
-            filter_reset : ".reset",
-            // extra css class name (string or array) added to the filter element (input or select)
-            filter_cssFilter: "form-control",
-        }
+        theme: "bootstrap",
+        widgets : [ 'uitheme', 'zebra' ],
+        headerTemplate : '{content}{icon}'
     });
 
-    $(".table-sort").bind("sortStart",function() {
+    tableSort.bind("sortStart", function () {
         $('.ajax-rows').remove();
         $('.toggle').removeClass("fa-folder-open").addClass("fa-folder");
     });
 
-    $('#add_target').on('click', function() {
-        if ($('div.target:first').is(":hidden") ) {
-            $('div.target:first').show();
+    $('#add_target').on('click', function () {
+        let first = $('div.target:first');
+        let last = $('div.target:last');
+
+
+        if (first.is(":hidden")) {
+            first.show();
         } else {
-            $('div.target:last').after($('div.target:last').clone()
+            last.after(last.clone()
                 .find(':input')
-                .each(function(){
-                    this.name = this.name.replace(/\[(\d+)\]/, function(str,p1){
-                        return '[' + (parseInt(p1,10)+1) + ']';
+                .each(function () {
+                    this.name = this.name.replace(/\[(\d+)\]/, function (str, p1) {
+                        return '[' + (parseInt(p1, 10) + 1) + ']';
                     });
                 })
                 .end());
         }
         $('#targetCount').val($("div.target:visible").length);
     });
-    $('#remove_target').click(function() {
-        if ($('div.target').length == 1) {
-            $('div.target').hide();
+    $('#remove_target').click(function () {
+        let target = $('div.target');
+        if (target.length === 1) {
+            target.hide();
         } else {
             $('div.target:last').remove();
         }
         $('#targetCount').val($("div.target:visible").length);
     });
 
-    $( "#form-data" ).validate({
+    $("#form-data").validate({
         rules: {
             dwc: {
                 required: true,
@@ -74,21 +60,21 @@ $(document).ready(function() {
             }
         }
     });
-    $( "#form-recordset" ).validate({
+    $("#form-recordset").validate({
         rules: {
             recordset: {
                 required: true
             }
         }
     });
-    $( "#form-data-url" ).validate({
+    $("#form-data-url").validate({
         rules: {
             "data-url": {
                 required: true
             }
         }
     });
-    $( "#form-trans" ).validate({
+    $("#form-trans").validate({
         rules: {
             transcription: {
                 required: true,
@@ -97,52 +83,56 @@ $(document).ready(function() {
         }
     });
 
-
-    $("#userGroup").change(function(){
-        this.value == 'new' ? $("#groupInput").show() : $("#groupInput").hide();
+    let userGroup = $("#userGroup");
+    let groupInput = $("#groupInput");
+    userGroup.change(function () {
+        this.value === 'new' ? groupInput.show() : groupInput.hide();
     });
-    if ($("#userGroup").length > 0){
-        $("#userGroup").val() == 'new' ? $("#groupInput").show() : $("#groupInput").hide();
+    if (userGroup.length) {
+        userGroup.val() === 'new' ? groupInput.show() : groupInput.hide();
     }
 
-    $('#selectall').click(function(event) {  //on click
-        if(this.checked) { // check select status
-            $('.checkbox-all').each(function() { //loop through each checkbox
+    $('#selectall').click(function () {  //on click
+        let checkboxAll = $('.checkbox-all');
+        if (this.checked) { // check select status
+            checkboxAll.each(function () { //loop through each checkbox
                 this.checked = true;  //select all checkboxes with class "checkbox1"
             });
-        }else{
-            $('.checkbox-all').each(function() { //loop through each checkbox
+        } else {
+            checkboxAll.each(function () { //loop through each checkbox
                 this.checked = false; //deselect all checkboxes with class "checkbox1"
             });
         }
     });
-    
-    $('#processes').click(function() {
-        $('#processModal')
-            .prop('class', 'modal fade') // revert to default
-            .addClass('left');
-        $('#processModal').modal('show');
-    });
 
-    $('a.noClick').click(function(event){
-        $.get( "/poll");
+    let homeProjectList = $('a.home-project-list');
+    homeProjectList.click(function (event) {
+        let count = $(this).data("count");
+        $.get($(this).attr("href") + '/' + count, function (data) {
+            $(".recent-projects-pane").html(data);
+            homeProjectList.data("count", count + 5);
+        });
         event.preventDefault();
     });
 
-    $('a.home-project-list').click(function(event){
-        var count = $(this).data("count");
-        $.get( $(this).attr("href") + '/' + count, function( data ) {
-                $( ".recent-projects-pane" ).html( data );
-                $('a.home-project-list').data("count", count+5);
-            });
-        event.preventDefault();
-    });
+    let textarea = $('.textarea');
+    if (textarea.length) {
+        textarea.summernote({
+            height: 200,
+            toolbar: [
+                ['style', ['bold', 'italic', 'underline', 'clear']],
+                ['font', ['strikethrough', 'superscript', 'subscript']],
+                ['fontsize', ['fontsize']],
+                ['color', ['color']],
+                ['para', ['ul', 'ol', 'paragraph']],
+                ['height', ['height']]
+            ]
+        });
+    }
 
-    $('.textarea').wysihtml5();
-
-    $('div.panel-heading i').click(function(){
-        var div = '#transcribers';
-        if ( $(div).height() !== 370) {
+    $('div.panel-heading i').click(function () {
+        let div = '#transcribers';
+        if ($(div).height() !== 370) {
             $(div).css('height', '370px');
             $(div).css('overflow', 'scroll');
         } else {
@@ -151,15 +141,71 @@ $(document).ready(function() {
         }
     });
 
+    if (Laravel.flashMessage.length) {
+        $.notify({
+            icon: 'glyphicon glyphicon-' + Laravel.flashIcon,
+            message: Laravel.flashMessage
+        }, {
+            type: Laravel.flashType,
+            placement: {
+                from: "top",
+                align: "center"
+            },
+            offset: 50,
+            spacing: 10,
+            animate: {
+                enter: 'animated fadeInDown',
+                exit: 'animated fadeOutUp'
+            }
+        });
+    }
+
+    if ($('#processModal').length) {
+        Echo.channel(Laravel.ocrChannel)
+            .listen('PollOcrEvent', (e) => {
+                let uuids = Laravel.groupUuids;
+                let ocrHtml = '';
+
+                if ($.isArray(e.data)) {
+                    $.each(e.data, function (index) {
+                        if ($.inArray(e.data[index].groupUuid, uuids) === -1) {
+                            return true;
+                        }
+                        ocrHtml += e.data[index].message;
+                    });
+                } else {
+                    ocrHtml = e.data;
+                }
+
+                $('#ocrHtml').html(ocrHtml);
+            });
+
+        Echo.channel(Laravel.exportChannel)
+            .listen('PollExportEvent', (e) => {
+                let uuids = Laravel.groupUuids;
+                let exportHtml = '';
+
+                if ($.isArray(e.data)) {
+                    $.each(e.data, function (index) {
+                        if ($.inArray(e.data[index].groupUuid, uuids) === -1) {
+                            return true;
+                        }
+                        exportHtml += e.data[index].message;
+                    });
+                } else {
+                    exportHtml = e.data;
+                }
+
+                $('#exportHtml').html(exportHtml);
+            });
+    }
 });
 
-$(function()
-{
-    $(document).on('click', '.btn-add', function(e)
-    {
+$(function () {
+    $(document).on('click', '.btn-add', function (e) {
         e.preventDefault();
 
-        var controls = $('.controls'),
+        let controls = $('.controls'),
             currentEntry = $(this).parents('.entry:first'),
             newEntry = $(currentEntry.clone()).appendTo(controls);
 
@@ -168,11 +214,11 @@ $(function()
             .removeClass('btn-add').addClass('btn-remove')
             .removeClass('btn-success').addClass('btn-danger')
             .html('<span class="fa fa-minus fa-lrg"></span>');
-    }).on('click', '.btn-remove', function(e)
-    {
+    }).on('click', '.btn-remove', function (e) {
         $(this).parents('.entry:first').remove();
 
         e.preventDefault();
         return false;
     });
 });
+
