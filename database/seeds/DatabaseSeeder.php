47fe8eebee7eef765a442236dc249e7e1c4270f2
Robert <bruhnrp@gmail.com>
2017-12-31 10:28:31 -0500

    Commit completed upgrade

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,23 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        if (env('APP_ENV') === 'local')
-            echo env('APP_ENV') . PHP_EOL;
-
-        return;
-
-
-        $this->call('TruncateTables');
-        $this->call('PermissionTableSeeder');
-        $this->call('ApiSubscribersTableSeeder');
-        $this->call('GroupTableSeeder');
-        $this->call('UserGroupTableSeeder');
-        $this->call('ActorsTableSeeder');
-        $this->call('ProjectsTableSeeder');
-        $this->call('ExpeditionsTableSeeder');
-        $this->call('SubjectsTableSeeder');
-        $this->call('ExpeditionSubjectTableSeeder');
-
-        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        // $this->call(UsersTableSeeder::class);
     }
 }
