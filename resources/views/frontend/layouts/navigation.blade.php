2e587eba6ca4ae3e76dd78973c1e61b51357213f
Robert <bruhnrp@gmail.com>
2018-01-17 10:24:47 -0500

    Test Google translate

--- resources/views/frontend/layouts/navigation.blade.php
+++ resources/views/frontend/layouts/navigation.blade.php
@@ -22,13 +22,6 @@
                 @include('frontend.layouts.partials.authmenu')
             @endif
             @include('frontend.layouts.partials.commonmenu')
-            <li>
-                <div id="google_translate_element"></div><script type="text/javascript">
-                    function googleTranslateElementInit() {
-                        new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
-                    }
-                </script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-            </li>
         </ul>
         <ul class="nav navbar-nav  pull-right">
             @if (Auth::check())
