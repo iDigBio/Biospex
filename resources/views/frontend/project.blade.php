a19e1d2b77bb6dc575540452889ec6275ce0837e
Robert <bruhnrp@gmail.com>
2018-01-17 12:54:43 -0500

    Biospex-181 Add code to leave out fields not populated

--- resources/views/frontend/project.blade.php
+++ resources/views/frontend/project.blade.php
@@ -40,40 +40,70 @@
             </div>
             <div class="col-md-5">
                 <dl>
-                    <dt class="firstdl">{{ trans('forms.organization') }}</dt>
-                    <dd class="firstdl">{{ $project->organization }}&nbsp;</dd>
-                    <dt>{{ trans('forms.contact') }}</dt>
-                    <dd><a href="mailto:{{ $project->contact_email }}">{{ $project->contact }}</a>&nbsp;</dd>
-                    <dt>{{ trans('forms.contact_title') }}</dt>
-                    <dd>{{ $project->contact_title }}&nbsp;</dd>
-                    <dt>{{ trans('forms.organization_website') }}</dt>
-                    <dd><a href="{{ $project->organization_website }}">{{ $project->organization_website }}</a>&nbsp;
-                    </dd>
-                    <dt>{{ trans('forms.project_partners') }}</dt>
-                    <dd>{{ $project->project_partners }}&nbsp;</dd>
-                    <dt>{{ trans('forms.funding_source') }}</dt>
-                    <dd>{{ $project->funding_source }}&nbsp;</dd>
-                    <dt>{{ trans('forms.incentives') }}</dt>
-                    <dd>{{ $project->incentives }}&nbsp;</dd>
-                    <dt>{{ trans('forms.geographic_scope') }}</dt>
-                    <dd>{{ $project->geographic_scope }}&nbsp;</dd>
-                    <dt>{{ trans('forms.taxonomic_scope') }}</dt>
-                    <dd>{{ $project->taxonomic_scope }}&nbsp;</dd>
-                    <dt>{{ trans('forms.temporal_scope') }}</dt>
-                    <dd>{{ $project->temporal_scope }}&nbsp;</dd>
-                    <dt>{{ trans('forms.language_skills') }}</dt>
-                    <dd>{{ $project->language_skills }}&nbsp;</dd>
-                    <dt>{{ trans('forms.activities') }}</dt>
-                    <dd>{{ $project->activities }}&nbsp;</dd>
-                    <dt>{{ trans('forms.keywords') }}</dt>
-                    <dd>{{ $project->keywords }}&nbsp;</dd>
-                    <dt>{{ trans('forms.blog_url') }}</dt>
-                    <dd><a href="{{ $project->blog_url }}">{{ $project->blog_url }}</a>&nbsp;</dd>
-                    <dt>{{ trans('forms.facebook') }}</dt>
-                    <dd><a href="{{ $project->facebook }}">{{ $project->facebook }}</a>&nbsp;</dd>
-                    <dt>{{ trans('forms.twitter') }}</dt>
-                    <dd><a href="{{ $project->twitter }}" target="_blank">{{ $project->twitter }}</a>&nbsp;
-                    </dd>
+                    @isset($project->organization)
+                        <dt class="firstdl">{{ trans('forms.organization') }}</dt>
+                        <dd class="firstdl">{{ $project->organization }}&nbsp;</dd>
+                    @endisset
+                    @isset($project->contact_email)
+                        <dt>{{ trans('forms.contact') }}</dt>
+                        <dd><a href="mailto:{{ $project->contact_email }}">{{ $project->contact }}</a>&nbsp;</dd>
+                    @endisset
+                    @isset($project->contact_title)
+                        <dt>{{ trans('forms.contact_title') }}</dt>
+                        <dd>{{ $project->contact_title }}&nbsp;</dd>
+                    @endisset
+                    @isset($project->organization_website)
+                        <dt>{{ trans('forms.organization_website') }}</dt>
+                        <dd><a href="{{ $project->organization_website }}">{{ $project->organization_website }}</a></dd>
+                    @endisset
+                    @isset($project->project_partners)
+                        <dt>{{ trans('forms.project_partners') }}</dt>
+                        <dd>{{ $project->project_partners }}&nbsp;</dd>
+                    @endisset
+                    @isset($project->funding_source)
+                        <dt>{{ trans('forms.funding_source') }}</dt>
+                        <dd>{{ $project->funding_source }}&nbsp;</dd>
+                    @endisset
+                    @isset($project->incentives)
+                        <dt>{{ trans('forms.incentives') }}</dt>
+                        <dd>{{ $project->incentives }}&nbsp;</dd>
+                    @endisset
+                    @isset($project->geographic_scope)
+                        <dt>{{ trans('forms.geographic_scope') }}</dt>
+                        <dd>{{ $project->geographic_scope }}&nbsp;</dd>
+                    @endisset
+                    @isset($project->taxonomic_scope)
+                        <dt>{{ trans('forms.taxonomic_scope') }}</dt>
+                        <dd>{{ $project->taxonomic_scope }}&nbsp;</dd>
+                    @endisset
+                    @isset($project->temporal_scope)
+                        <dt>{{ trans('forms.temporal_scope') }}</dt>
+                        <dd>{{ $project->temporal_scope }}&nbsp;</dd>
+                    @endisset
+                    @isset($project->language_skills)
+                        <dt>{{ trans('forms.language_skills') }}</dt>
+                        <dd>{{ $project->language_skills }}&nbsp;</dd>
+                    @endisset
+                    @isset($project->activities)
+                        <dt>{{ trans('forms.activities') }}</dt>
+                        <dd>{{ $project->activities }}&nbsp;</dd>
+                    @endisset
+                    @isset($project->keywords)
+                        <dt>{{ trans('forms.keywords') }}</dt>
+                        <dd>{{ $project->keywords }}&nbsp;</dd>
+                    @endisset
+                    @isset($project->blog_url)
+                        <dt>{{ trans('forms.blog_url') }}</dt>
+                        <dd><a href="{{ $project->blog_url }}">{{ $project->blog_url }}</a></dd>
+                    @endisset
+                    @isset($project->facebook)
+                        <dt>{{ trans('forms.facebook') }}</dt>
+                        <dd><a href="{{ $project->facebook }}">{{ $project->facebook }}</a></dd>
+                    @endisset
+                    @isset($project->twitter)
+                        <dt>{{ trans('forms.twitter') }}</dt>
+                        <dd><a href="{{ $project->twitter }}" target="_blank">{{ $project->twitter }}</a></dd>
+                    @endisset
                 </dl>
             </div>
         </div>
@@ -84,16 +114,16 @@
             </div>
         @endif
         <div class="row">
-            @if ($project->fusion_table_id !== null)
-                @include('frontend.layouts.partials.projectmap')
-            @endif
-            <!---
+        @if ($project->fusion_table_id !== null)
+            @include('frontend.layouts.partials.projectmap')
+        @endif
+        <!---
             <div class="col-md-4 organizers">
                 <h2 class="project-page-header">{{ trans('pages.project_page_organizers') }}</h2>
                 <dl>
                     @foreach($project->group->users as $user)
-                        <dt>
-                            <img src="{{ $user->profile->avatar->url('small') }}"/>
+            <dt>
+                <img src="{{ $user->profile->avatar->url('small') }}"/>
                         </dt>
                         <dd>{!! HTML::mailto($user->email, $user->profile->full_name) !!}</dd>
                     @endforeach
