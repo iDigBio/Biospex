98cede8947c24b5d36bb5ebc46087319f642df55
Robert <bruhnrp@gmail.com>
2018-01-17 13:20:54 -0500

    Add project resources table

---
+++ database/migrations/2018_01_17_175807_create_project_resources_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectResourcesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_resources', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('project_id');
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+            $table->enum('type', ['file', 'website', 'video'])->default('file')
+            $table->string('name');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_resources');
+    }
+}
