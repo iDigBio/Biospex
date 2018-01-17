8043a74249eecbf8dc1c2795336b1874f7ed7967
Robert <bruhnrp@gmail.com>
2018-01-17 11:14:05 -0500

    Test Google translate

--- resources/views/frontend/layouts/partials/commonmenu.blade.php
+++ resources/views/frontend/layouts/partials/commonmenu.blade.php
@@ -7,9 +7,3 @@
 @can('admin', Auth::getUser())
     <li><a href="{{ route('admin.dashboard.index') }}">{{ trans('pages.admin') }}</a></li>
 @endcan
-<li class="translate"><div id="google_translate_element"></div><script type="text/javascript">
-        function googleTranslateElementInit() {
-            new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
-        }
-    </script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-</li>
