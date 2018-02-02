4e4d23295ef097591637935fb0949382e5894857
Robert <bruhnrp@gmail.com>
2018-02-02 11:14:13 -0500

    Commit changes to switch branches

--- app/Repositories/Eloquent/EventRepository.php
+++ app/Repositories/Eloquent/EventRepository.php
@@ -17,4 +17,32 @@ class EventRepository extends EloquentRepository implements Event
     {
         return Model::class;
     }
+
+    /**
+     * @inheritdoc
+     */
+    public function create(array $attributes)
+    {
+        $this->create($attributes);
+
+        $groups = collect($attributes['groups'])->reject(function ($resource) {
+            return $this->filterOrDeleteResources($resource);
+        })->map(function ($resource) {
+            return new ProjectResourceModel($resource);
+        });
+
+        $project->resources()->saveMany($resources->all());
+
+        if ($project) {
+            $this->notifyActorContacts($project->id);
+
+            Flash::success(trans('projects.project_created'));
+
+            return $project;
+        }
+
+        Flash::error(trans('projects.project_save_error'));
+
+        return false;
+    }
 }
--- app/Repositories/Interfaces/Event.php
+++ app/Repositories/Interfaces/Event.php
@@ -6,5 +6,9 @@ use App\Repositories\RepositoryInterface;
 
 interface Event extends RepositoryInterface
 {
-
+    /**
+     * @param array $attributes
+     * @return mixed
+     */
+    public function create(array $attributes);
 }
