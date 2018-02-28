dbacb44eab13b9b55400537c9e29d50b8d63e7c8
Robert <bruhnrp@gmail.com>
2018-02-28 10:59:05 -0500

    Change transcription_id to classification_id

--- database/migrations/2018_02_14_144436_create_event_transcriptions_table.php
+++ database/migrations/2018_02_14_144436_create_event_transcriptions_table.php
@@ -15,8 +15,8 @@ class CreateEventTranscriptionsTable extends Migration
     {
         Schema::create('event_transcriptions', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('transcription_id');
-            $table->unique('transcription_id');
+            $table->integer('classification_id');
+            $table->unique('classification_id');
             $table->unsignedInteger('event_id');
             $table->foreign('event_id')->references('id')->on('events')->onDelete('cascade');
             $table->unsignedInteger('group_id');
