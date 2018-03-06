bab1a82da5673d09d4389b33b54e6c42ed507e27
Robert <bruhnrp@gmail.com>
2018-03-06 09:48:57 -0500

    Add getGroupByUuid method

--- app/Repositories/Eloquent/EventGroupRepository.php
+++ app/Repositories/Eloquent/EventGroupRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repositories\Eloquent;
 
+use App\Facades\GeneralHelper;
 use App\Models\EventGroup as Model;
 use App\Repositories\Interfaces\EventGroup;
 
@@ -16,5 +17,19 @@ class EventGroupRepository extends EloquentRepository implements EventGroup
     public function model()
     {
         return Model::class;
+    }
+
+    /**
+     * @param $uuid
+     * @return \Illuminate\Database\Eloquent\Model|null|object|static
+     * @throws \Exception
+     */
+    public function getGroupByUuid($uuid)
+    {
+        $results = $this->model->with(['event'])->where('uuid', GeneralHelper::uuidToBin($uuid))->first();
+
+        $this->resetModel();
+
+        return $results;
     }
 }
