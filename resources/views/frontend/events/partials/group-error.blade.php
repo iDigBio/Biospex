5020a3c653addb6837002e32d508187f9d3279ce
Robert <bruhnrp@gmail.com>
2018-03-06 10:18:42 -0500

    Fix issue with error reporting

--- resources/views/frontend/events/partials/group-error.blade.php
+++ resources/views/frontend/events/partials/group-error.blade.php
@@ -1,16 +1,16 @@
 <div class="entry col-md-12">
-    <div class="col-sm-6 top10 {!! $errors->has('groups.'.  $key  . '.title') ? 'has-error' : '' !!}">
-        {!! $errors->has('groups.'.  $key  . '.title') ? '&nbsp;' . $errors->first('groups.'.  $key  . '.title') : '' !!}
+    <div class="col-sm-6 top10 {!! $errors->has('groups.'.  $i  . '.title') ? 'has-error' : '' !!}">
+        {!! $errors->has('groups.'.  $i  . '.title') ? '&nbsp;' . $errors->first('groups.'.  $i  . '.title') : '' !!}
         <div class="input-group">
         <span class="input-group-btn">
             {!! Form::button('<i class="fa fa-plus fa-lrg"></i> ', ['type' => 'button', 'class' => 'btn btn-success btn-add']) !!}
         </span>
-            {!! Form::text('groups[' . $key . '][title]', old('groups.[' . $key . '].title'), [
+            {!! Form::text('groups[' . $i . '][title]', old('groups.[' . $i . '].title'), [
                 'class' => 'form-control',
                 'placeholder' =>
                 trans('pages.event_groups_title')
                 ]) !!}
         </div>
-        {!! Form::hidden('groups['. $key .'][id]', old('groups.[' . $key . '].id')) !!}
+        {!! Form::hidden('groups['. $i .'][id]', old('groups.[' . $i . '].id')) !!}
     </div>
 </div>
