7efa5e699f997f6f48191c6fc9e17c98c4fbffe4
Robert <bruhnrp@gmail.com>
2018-03-08 10:27:38 -0500

    Add default schema for index length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,6 +18,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        \Schema::defaultStringLength(191);
+
         $this->setupBlade();
 
         if ($this->app->environment() === 'local' && env('DB_LOG'))
