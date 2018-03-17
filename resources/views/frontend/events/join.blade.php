bbc58424baf75f47e8274d1126bf8abb755d7d98
Robert <bruhnrp@gmail.com>
2018-03-17 10:41:27 -0400

    Add event and group title to the join form

--- resources/views/frontend/events/join.blade.php
+++ resources/views/frontend/events/join.blade.php
@@ -24,26 +24,36 @@
                     ]) !!}
                     {!! csrf_field() !!}
                     <div class="row">
-                    <div class="col-md-6 col-md-offset-3">
-                        <div class="form-group">
-                            <div class="input-group {{ ($errors->has('nfn_user')) ? 'has-error' : '' }}">
-                                <span class="input-group-addon"><i class="fa fa-user fa-lg"></i></span>
-                                {!! Form::text('nfn_user', '', ['id' => 'nfn_user', 'class' => 'form-control', 'placeholder' => trans('pages.nfn_user'),]) !!}
-                            </div>
-                            {{ $errors->first('nfn_user') }}
+                        <div class="col-md-10 col-md-offset-2">
+                            <h4>@lang('pages.event'): {{ $group->event->title }}</h4>
                         </div>
-                    </div>
                     </div>
                     <div class="row">
-                    <div class="col-md-4 col-md-offset-4">
-                        <div class="form-group">
-                            {!! Honeypot::generate('registeruser', 'registertime') !!}
-                            {!! Form::submit(trans('pages.register'), array('class' => 'btn btn-primary btn-block')) !!}
-                            {!! Form::hidden('group_id', $group->id) !!}
-                            {!! Form::hidden('uuid', $group->uuid) !!}
-                            {!! Form::close() !!}
+                        <div class="col-md-10 col-md-offset-2">
+                            <h4>@lang('pages.group'): {{ $group->title }}</h4>
                         </div>
                     </div>
+                    <div class="row top10">
+                        <div class="col-md-6 col-md-offset-3">
+                            <div class="form-group">
+                                <div class="input-group {{ ($errors->has('nfn_user')) ? 'has-error' : '' }}">
+                                    <span class="input-group-addon"><i class="fa fa-user fa-lg"></i></span>
+                                    {!! Form::text('nfn_user', '', ['id' => 'nfn_user', 'class' => 'form-control', 'placeholder' => trans('pages.nfn_user'),]) !!}
+                                </div>
+                                {{ $errors->first('nfn_user') }}
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4 col-md-offset-4">
+                            <div class="form-group">
+                                {!! Honeypot::generate('registeruser', 'registertime') !!}
+                                {!! Form::submit(trans('pages.register'), array('class' => 'btn btn-primary btn-block')) !!}
+                                {!! Form::hidden('group_id', $group->id) !!}
+                                {!! Form::hidden('uuid', $group->uuid) !!}
+                                {!! Form::close() !!}
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
