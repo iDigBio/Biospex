6287dbf2c701c57671305e23b8e1e80b90e0bf24
Robert <bruhnrp@gmail.com>
2018-03-21 13:00:29 -0400

    Fix event scoreboard text.

--- resources/views/frontend/project.blade.php
+++ resources/views/frontend/project.blade.php
@@ -131,10 +131,10 @@
                                 <div class="row">
                                     <div class="col-md-4"><b>@lang('pages.groups')</b></div>
                                     <div class="col-md-6">
-                                        <b>% of Total Transcriptions</b>
+                                        <b>@lang('pages.event_board_percent')</b>
                                     </div>
                                     <div class="col-md-2">
-                                        <b>Count</b>
+                                        <b>@lang('pages.event_board_count')</b>
                                     </div>
                                 </div>
                                 @foreach($event->groups as $group)
@@ -152,7 +152,7 @@
                                 @endforeach
                                 <div class="row top5">
                                     <div class="col-md-5">
-                                        Total Transcriptions
+                                        @lang('pages.event_board_total')
                                     </div>
                                     <div class="col-md-5"></div>
                                     <div class="col-md-2">{{ $event->transcriptionCount }}</div>
