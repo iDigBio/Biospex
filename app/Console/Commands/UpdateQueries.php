3e8601941b8658b1bfc54e14b1cc81d69967dea9
Robert <bruhnrp@gmail.com>
2018-04-07 13:28:21 -0400

    Run update queries to fix subject counts

--- app/Console/Commands/UpdateQueries.php
+++ app/Console/Commands/UpdateQueries.php
@@ -2,20 +2,9 @@
 
 namespace App\Console\Commands;
 
-use App\Repositories\Interfaces\Download;
-use App\Repositories\Interfaces\Expedition;
-use App\Repositories\Interfaces\ExpeditionStat;
-use App\Repositories\Interfaces\Group;
-use App\Repositories\Interfaces\OcrQueue;
-use App\Repositories\Interfaces\Project;
-use App\Repositories\Interfaces\User;
-use App\Services\MongoDbService;
+use App\Models\Expedition;
 use Illuminate\Console\Command;
-use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Support\Facades\DB;
-use File;
-use Ramsey\Uuid\Uuid;
 
 class UpdateQueries extends Command
 {
@@ -33,19 +22,11 @@ class UpdateQueries extends Command
     protected $description = 'Used for custom queries when updating database';
 
     /**
-     * @var \App\Repositories\Interfaces\ExpeditionStat
-     */
-    private $expeditionStat;
-
-    /**
      * UpdateQueries constructor.
      */
-    public function __construct(
-        ExpeditionStat $expeditionStat
-    )
+    public function __construct()
     {
         parent::__construct();
-        $this->expeditionStat = $expeditionStat;
     }
 
     /**
@@ -53,10 +34,12 @@ class UpdateQueries extends Command
      */
     public function handle()
     {
-        $stats = $this->expeditionStat->all();
-        $stats->each(function($stat) {
-            $stat->local_subject_count = $stat->subject_count;
-            $stat->save();
+        $expeditions = Expedition::with('stat')->get();
+        $expeditions->each(function($expedition){
+            if ($expedition->stat != null) {
+                $expedition->stat->local_subject_count = $expedition->subjectCount;
+                $expedition->save();
+            }
         });
     }
 
