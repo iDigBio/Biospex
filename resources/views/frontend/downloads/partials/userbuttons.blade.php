dd34b725b187435c95de4490cab721edd4ae54d4
Robert <bruhnrp@gmail.com>
2018-04-09 13:46:09 -0400

    Replace notice composer

--- resources/views/frontend/downloads/partials/userbuttons.blade.php
+++ resources/views/frontend/downloads/partials/userbuttons.blade.php
@@ -1,30 +1,30 @@
 @if ($download->type != 'export')
     @can('isOwner', $expedition->project->group)
-        <button title="@lang('buttons.downloadTitle')"
+        <button title="@lang('pages.downloadTitle')"
                 class="btn btn-success btn-xs"
                 type="button"
                 onClick="location.href='{{ route('projects.expeditions.downloads.get.show', [$expedition->project->id, $expedition->id, $download->id]) }}'">
-            <span class="fa fa-download"></span> @lang('buttons.download')
+            <span class="fa fa-download"></span> @lang('pages.download')
         </button>
         @if ($download->type === 'summary')
-            <button title="@lang('buttons.summaryTitle')" class="btn btn-primary btn-xs" type="button"
-                    onClick="window.open('{{ route('web.downloads.summary', [$expedition->project->id, $expedition->id]) }}', '_blank')">
-                <span class="fa fa-eye fa-lrg"></span> @lang('buttons.summary')
+            <button title="@lang('pages.summaryTitle')" class="btn btn-primary btn-xs" type="button"
+                    onClick="window.open('{{ route('webauth.downloads.summary', [$expedition->project->id, $expedition->id]) }}', '_blank')">
+                <span class="fa fa-eye fa-lrg"></span> @lang('pages.summary')
             </button>
         @endif
     @endcan
 @else
-    <button title="@lang('buttons.downloadTitle')"
+    <button title="@lang('pages.downloadTitle')"
             class="btn btn-success btn-xs"
             type="button"
             onClick="location.href='{{ route('projects.expeditions.downloads.get.show', [$expedition->project->id, $expedition->id, $download->id]) }}'">
-        <span class="fa fa-download"></span> @lang('buttons.download')
+        <span class="fa fa-download"></span> @lang('pages.download')
     </button>
     @if ($download->type === 'export')
-        <button title="@lang('buttons.regenerateDownload')"
+        <button title="@lang('pages.regenerateDownload')"
                 class="btn btn-success btn-xs" type="button"
-                onClick="location.href='{{ route('web.downloads.regenerate', [$expedition->project->id, $expedition->id]) }}'">
-            <span class="fa fa-refresh"></span> @lang('buttons.regenerateDownload')
+                onClick="location.href='{{ route('webauth.downloads.regenerate', [$expedition->project->id, $expedition->id]) }}'">
+            <span class="fa fa-refresh"></span> @lang('pages.regenerateDownload')
         </button>
     @endif
 @endif
