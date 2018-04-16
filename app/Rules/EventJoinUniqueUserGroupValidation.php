b41f01de18e3f6107c7e9b1a06f4f6d0c2e8accb
Robert <bruhnrp@gmail.com>
2018-04-16 11:29:32 -0400

    Custom validation rule for event user join.

---
+++ app/Rules/EventJoinUniqueUserGroupValidation.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Rules;
+
+use App\Models\Event;
+use Illuminate\Contracts\Validation\Rule;
+
+class EventJoinUniqueUserGroupValidation implements Rule
+{
+    /**
+     * Create a new rule instance.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string $attribute
+     * @param  mixed $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $groupId = request()->get('group_id');
+        $nfnUser = request()->get('nfn_user');
+
+        $result = Event::whereHas('groups', function($query) use ($groupId){
+            $query->where('id', $groupId);
+        })->whereHas('groups.users', function($query) use($nfnUser){
+            $query->where('nfn_user', $nfnUser);
+        })->count();
+
+        return ! $result;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return trans('messages.event_join_user_error');
+    }
+}
