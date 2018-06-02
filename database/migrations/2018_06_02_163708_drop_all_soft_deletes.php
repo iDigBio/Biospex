66a116f3dcfa3a5993d4d215205d741fe5572a42
Robert <bruhnrp@gmail.com>
2018-06-02 12:56:53 -0400

    Migration to delete all softDeletes

---
+++ database/migrations/2018_06_02_163708_drop_all_soft_deletes.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DropAllSoftDeletes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $softDeletes = [
+            'actors',
+            'amcharts',
+            'expeditions',
+            'expedition_stats',
+            'groups',
+            'headers',
+            'metas',
+            'nfn_workflows',
+            'notices',
+            'projects',
+            'resources',
+            'transcription_locations',
+            'users',
+            'workflows',
+            'workflow_managers'
+        ];
+
+        foreach ($softDeletes as $softDelete)
+        {
+            Schema::table($softDelete, function ($table) {
+                $table->dropSoftDeletes();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
