a25441ef2470e4e04ca7e4a9601d46a8b1be1665
Robert <bruhnrp@gmail.com>
2018-10-06 09:40:00 -0400

    Clean up command

--- app/Console/Commands/AppCommand.php
+++ app/Console/Commands/AppCommand.php
@@ -2,10 +2,6 @@
 
 namespace App\Console\Commands;
 
-use App\Events\ScoreboardEvent;
-use App\Models\EventTranscription;
-use App\Repositories\Interfaces\Event;
-use App\Repositories\Interfaces\EventUser;
 use Illuminate\Console\Command;
 
 class AppCommand extends Command
@@ -21,24 +17,12 @@ class AppCommand extends Command
     protected $description = 'Used to test code';
 
     /**
-     * @var \App\Repositories\Interfaces\EventUser
-     */
-    private $eventUser;
-
-    /**
-     * @var \App\Repositories\Interfaces\Event
-     */
-    private $eventContract;
-
-    /**
      * Create a new job instance.
-     *
      */
-    public function __construct(EventUser $eventUser, Event $eventContract)
+    public function __construct(
+    )
     {
         parent::__construct();
-        $this->eventUser = $eventUser;
-        $this->eventContract = $eventContract;
     }
 
     /**
@@ -47,30 +31,6 @@ class AppCommand extends Command
     public function handle()
     {
 
-        $user = $this->eventUser->getEventUserByName('zorrobis');
-        dd($user);
-        /*
-        EventTranscription::create($this->create());
-        EventTranscription::create($this->create());
-
-        $projectId = 13;
-
-        $events = $this->eventContract->getEventsByProjectId($projectId);
-        $data = $events->mapWithKeys(function($event) {
-            $event->teams->sortBy('transcription_count');
-            return [$event->id => view('frontend.events.scoreboard-content', ['event' => $event])->render()];
-        });
-
-        ScoreboardEvent::dispatch($projectId, $data->toArray());
-        */
     }
 
-    public function create() {
-        return [
-                'classification_id' => mt_rand(10000000, 99999999),
-                'event_id' => 2,
-                'team_id' => 4,
-                'user_id' => 16
-        ];
-    }
 }
