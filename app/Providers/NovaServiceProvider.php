11f07af21fe3d2d53e67e7ff68b21ec5dbe3fa55
Robert <bruhnrp@gmail.com>
2018-10-07 10:41:41 -0400

    Fix issue with Nova login and layout

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -42,7 +42,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewNova', function ($user) {
-            return $user->idAdmin();
+            return $user->isAdmin();
         });
     }
 
