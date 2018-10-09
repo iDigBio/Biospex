08a886a20c62ab3e2823f3533f7e78327aaf44ef
Robert <bruhnrp@gmail.com>
2018-10-09 15:41:46 -0400

    Fix team tiles

--- resources/views/front/team/partials/categories.blade.php
+++ resources/views/front/team/partials/categories.blade.php
@@ -1,6 +1,6 @@
-<h3 class="ml-3 pt-4">{{ $category->name }}</h3>
+<h3 class="ml-3 pt-4 text-center">{{ $category->name }}</h3>
 <div class="card-deck">
-@foreach($category->teams as $team)
-    @include('front.team.partials.teams')
-@endforeach
+    @foreach($category->teams as $team)
+        @include('front.team.partials.teams')
+    @endforeach
 </div>
--- resources/views/front/team/partials/teams.blade.php
+++ resources/views/front/team/partials/teams.blade.php
@@ -1,18 +1,22 @@
-    <div class="col-md-4 text-center">
-        <div class="card-project mb-4 px-4 box-shadow" style="max-width: 25rem;">
-            <h2 class="text-center pt-4"><a href="mailto:ed.edu">{{ $team->present()->full_name }}</a></h2>
-            <hr>
-            <p class="text-center"><strong>Professor</strong><br>
-                {{ $team->institution }}<br>
-            <h3 class="pb-3"><a href="//fsu.edu">Florida State University</a></h3>
+<div class="col-md-4 text-center">
+    <div class="card-project mb-4 px-4 box-shadow h-100" style="max-width: 25rem;">
+        <h2 class="text-center pt-4"><a href="mailto:{{ $team->email }}">{{ $team->present()->full_name }}</a></h2>
+        <hr>
+        <p class="text-center"><strong>{{ $team->title }}</strong><br>
+            {{ $team->department }}<br>
+        <h3 class="pb-3"><a href="#">{{ $team->institution }}</a></h3>
 
-            <div class="card-footer">
-                <div class="d-flex align-items-start justify-content-between mt-4 mb-3">
-                    <a href="#"><i class="fab fa-linkedin"></i> <span class="d-none text d-sm-inline"></span></a>
-                    <a href="#"><i class="fab fa-twitter"></i> <span class="d-none text d-sm-inline"></span></a>
-                    <a href="mailto:{!! $team->email !!}"><i class="far fa-envelope"></i> <span class="d-none text d-sm-inline"></span></a>
-                    <a href="tel:+18506451500"><i class="fas fa-phone-square"></i> <span class="d-none text d-sm-inline"></span></a>
-                </div>
+        <div class="card-footer">
+            <div class="d-flex align-items-start justify-content-between mt-4 mb-3">
+                <a href="mailto:{!! $team->email !!}"><i class="far fa-envelope"></i> <span
+                            class="d-none text d-sm-inline"></span></a>
+                <!--
+                <a href="#"><i class="fab fa-linkedin"></i> <span class="d-none text d-sm-inline"></span></a>
+                <a href="#"><i class="fab fa-twitter"></i> <span class="d-none text d-sm-inline"></span></a>
+                <a href="tel:+18506451500"><i class="fas fa-phone-square"></i> <span
+                            class="d-none text d-sm-inline"></span></a>
+                -->
             </div>
         </div>
     </div>
+</div>
