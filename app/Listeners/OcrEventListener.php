ab951121161d5e3e899d5598e3cb23db5111a0a3
Robert <bruhnrp@gmail.com>
2018-10-09 11:13:11 -0400

    Commit new changes

---
+++ app/Listeners/OcrEventListener.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Repositories\Interfaces\OcrQueue;
+use Artisan;
+
+class OcrEventListener
+{
+
+    /**
+     * @var \App\Repositories\Interfaces\OcrQueue
+     */
+    private $ocrQueue;
+
+    /**
+     * Create the event listener.
+     *
+     * @param \App\Repositories\Interfaces\OcrQueue $ocrQueue
+     */
+    public function __construct(OcrQueue $ocrQueue)
+    {
+        $this->ocrQueue = $ocrQueue;
+    }
+
+    /**
+     * Register the listeners for the subscriber.
+     *
+     * @param $events
+     */
+    public function subscribe($events)
+    {
+        $events->listen(
+            'ocr.poll',
+            'App\Listeners\OcrEventListener@poll'
+        );
+
+        $events->listen(
+            'ocr.queued',
+            'App\Listeners\OcrEventListener@queued'
+        );
+
+        $events->listen(
+            'ocr.error',
+            'App\Listeners\OcrEventListener@error'
+        );
+    }
+
+    /**
+     * Record created.
+     */
+    public function poll()
+    {
+        Artisan::call('ocr:poll');
+    }
+
+    /**
+     * Record queued.
+     *
+     * @param \App\Models\OcrQueue $record
+     */
+    public function queued(\App\Models\OcrQueue $record)
+    {
+        $record->processed = 0;
+        $record->queued = 1;
+        $record->save();
+    }
+
+    /**
+     * Record error.
+     *
+     * @param \App\Models\OcrQueue $record
+     */
+    public function error(\App\Models\OcrQueue $record)
+    {
+        $record->queued = 0;
+        $record->error = 1;
+        $record->save();
+    }
+}
