40fcf7f3aad09417dbcde8e3b94c63b9588476ab
Robert <bruhnrp@gmail.com>
2018-10-09 15:33:44 -0400

    Add team and category to nova

---
+++ app/Nova/Team.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Nova;
+
+use Laravel\Nova\Fields\ID;
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\BelongsTo;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class Team extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = 'App\Models\Team';
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'id';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id',
+        'first_name',
+        'last_name',
+        'title',
+        'department',
+        'institution'
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            ID::make()->sortable(),
+            Text::make('First Name')->onlyOnForms(),
+            Text::make('Last Name')->onlyOnForms(),
+            Text::make('Name', function () {
+                return $this->first_name.' '.$this->last_name;
+            })->hideWhenCreating()->hideWhenUpdating(),
+            Text::make('Email')->hideFromIndex(),
+            Text::make('Title')->hideFromIndex(),
+            Text::make('Department')->hideFromIndex(),
+            Text::make('Institution')->hideFromIndex(),
+            BelongsTo::make('TeamCategory'),
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
+}
