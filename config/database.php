edbab2ba13e0a5a507965115af172cc54b71a5c8
Robert <bruhnrp@gmail.com>
2019-03-10 15:54:41 -0400

    Add path to handling missing csv file

--- config/database.php
+++ config/database.php
@@ -125,6 +125,20 @@ return [
             'read_write_timeout' => 0
         ],
 
+        'cache' => [
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_CACHE_DB', 1),
+        ],
+
+        'session' => [
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => 1,
+        ],
+
     ],
 
 ];
