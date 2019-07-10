6fd084f41df56ab0be935a040e150ddce0037964
Robert <bruhnrp@gmail.com>
2019-07-10 15:05:00 -0400

    Fix sort results and add project id for project page requests

--- resources/views/common/event-sort.blade.php
+++ resources/views/common/event-sort.blade.php
@@ -1,5 +1,6 @@
 <div class="d-flex justify-content-center col-sm-12 mb-4">
     <span data-type="{{ $type }}" data-sort="title" data-order="asc"
+          data-id="{{ isset($id) ? $id : null }}"
           data-url="{{ $route }}"
           data-target="{{ $type }}-events" class="sort-page mr-2 text-uppercase"
           style="color: #e83f29; cursor: pointer; display: inline-block">
@@ -12,6 +13,7 @@
         <i class="fas fa-sort"></i> {{ __('pages.project') }}</span>
     @endif
     <span data-type="{{ $type }}" data-sort="date" data-order="asc"
+          data-id="{{ isset($id) ? $id : null }}"
           data-url="{{ $route }}"
           data-target="{{ $type }}-events" class="sort-page ml-2 text-uppercase"
           style="color: #e83f29; cursor: pointer;display: inline-block">
