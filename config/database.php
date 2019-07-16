bbcbc012d043b6fdeff89e4ff18fea505c74aeab
Robert <bruhnrp@gmail.com>
2019-07-16 18:59:29 -0400

    Update packages and fix issues with pusher.

--- config/database.php
+++ config/database.php
@@ -161,19 +161,5 @@ return [
             'database' => 1,
         ],
 
-        'cache' => [
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
-            'database' => env('REDIS_CACHE_DB', 1),
-        ],
-
-        'session' => [
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
-            'database' => 1,
-        ],
-
     ],
 ];
