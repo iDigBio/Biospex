bd5937fac9369a93d2cba64e1b1e8c0c9248ccd1
Robert <bruhnrp@gmail.com>
2019-10-17 19:45:18 -0400

    Fix to match current version

--- app/Http/Controllers/Api/V0/PanoptesPusherController.php
+++ app/Http/Controllers/Api/V0/PanoptesPusherController.php
@@ -47,10 +47,8 @@ class PanoptesPusherController extends ApiController
             return;
         }
 
-        $data->title = $result->title;
-
         PusherEventTranscriptionJob::dispatch($data);
-        PusherWeDigBioDashboardJob::dispatch($data);
+        PusherWeDigBioDashboardJob::dispatch($data, $result);
 
         return;
     }
