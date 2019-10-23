5e3da72fe8c92ec6f32352dfca6d23a522b9f2ec
Robert <bruhnrp@gmail.com>
2019-10-23 10:27:48 -0400

    Reset to blank

--- app/Console/Commands/UpdateQueries.php
+++ app/Console/Commands/UpdateQueries.php
@@ -2,8 +2,6 @@
 
 namespace App\Console\Commands;
 
-use App\Repositories\Interfaces\Group;
-use App\Repositories\Interfaces\User;
 use Illuminate\Console\Command;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 
@@ -22,23 +20,11 @@ class UpdateQueries extends Command
     protected $description = 'Used for custom queries when updating database';
 
     /**
-     * @var \App\Repositories\Interfaces\Group
-     */
-    private $groupContract;
-
-    /**
-     * @var \App\Repositories\Interfaces\User
-     */
-    private $userContract;
-
-    /**
      * UpdateQueries constructor.
      */
-    public function __construct(Group $groupContract, User $userContract)
+    public function __construct()
     {
         parent::__construct();
-        $this->groupContract = $groupContract;
-        $this->userContract = $userContract;
     }
 
     /**
@@ -46,16 +32,7 @@ class UpdateQueries extends Command
      */
     public function handle()
     {
-        $user = $this->userContract->find(1);
-        $groups = $this->groupContract->all();
 
-        $groups->reject(function ($group) use ($user) {
-            echo 'Rejecting ' . $group->id . PHP_EOL;
-            return $user->hasGroup($group);
-        })->each(function($group) use ($user) {
-            echo 'Adding to ' . $group->id . PHP_EOL;
-            //$user->assignGroup($group);
-        });
     }
 
 }
