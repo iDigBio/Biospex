88620c857624b08c1b1db98f20d67a890c4b0e72
Robert <hb7dXEXggo6)Qq%EGs>
2019-11-16 11:59:32 -0500

    Add queries to update amchart table

--- app/Console/Commands/UpdateQueries.php
+++ app/Console/Commands/UpdateQueries.php
@@ -2,6 +2,9 @@
 
 namespace App\Console\Commands;
 
+use App\Repositories\Interfaces\AmChart;
+use App\Repositories\Interfaces\Project;
+use App\Services\Process\TranscriptionChartService;
 use Illuminate\Console\Command;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 
@@ -20,11 +23,33 @@ class UpdateQueries extends Command
     protected $description = 'Used for custom queries when updating database';
 
     /**
-     * UpdateQueries constructor.
+     * @var \App\Repositories\Interfaces\Project
      */
-    public function __construct()
+    private $projectContract;
+
+    /**
+     * @var \App\Services\Process\TranscriptionChartService
+     */
+    private $service;
+
+    /**
+     * @var \App\Repositories\Interfaces\AmChart
+     */
+    private $amChartContract;
+
+    /**
+     * UpdateQueries constructor.
+     *
+     * @param \App\Repositories\Interfaces\AmChart $amChartContract
+     * @param \App\Repositories\Interfaces\Project $projectContract
+     * @param \App\Services\Process\TranscriptionChartService $service
+     */
+    public function __construct(AmChart $amChartContract, Project $projectContract, TranscriptionChartService $service)
     {
         parent::__construct();
+        $this->projectContract = $projectContract;
+        $this->service = $service;
+        $this->amChartContract = $amChartContract;
     }
 
     /**
@@ -32,7 +57,15 @@ class UpdateQueries extends Command
      */
     public function handle()
     {
-
+        $projectIds = $this->amChartContract->all(['project_id'])->pluck('project_id');
+        $projectIds->each(function($id) {
+            $project = $this->projectContract->getProjectForAmChartJob($id);
+            $project->amChart->series = [];
+            $project->amChart->data = [];
+            $project->amChart->save();
+            $this->service->process($project);
+            echo 'processed project ' . $id . PHP_EOL;
+        });
     }
 
 }
