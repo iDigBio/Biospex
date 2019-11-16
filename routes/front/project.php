d7d3ba06229fa0f893be5cfbf33329d3f5754b1e
Robert <hb7dXEXggo6)Qq%EGs>
2019-11-16 09:00:37 -0500

    Add transcription chart routes, remove chart image route

--- routes/front/project.php
+++ routes/front/project.php
@@ -7,9 +7,8 @@ $router->get('project/{slug}', function($slug) {
     return redirect("/projects/$slug", 301);
 });
 $router->get('projects/{slug}')->uses('ProjectsController@project')->name('front.projects.slug');
-$router->get('projects/{project}/chart-image')->uses('ProjectsController@chartImage')->name('front.projects.image');
 
-$router->get('projects/{project}/test')->uses('ProjectsController@test')->name('front.projects.test');
+// Project Transcriptions
 $router->get('projects/{project}/transcriptions/{year}')->uses('TranscriptionsController@index')->name('front.projects.transcriptions');
 
 // Project Map
