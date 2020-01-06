793b33d514264b13197e77613543a6c79506d82a
Robert <hb7dXEXggo6)Qq%EGs>
2020-01-06 10:53:12 -0500

    Add method for event step chart

--- app/Http/Controllers/Front/AjaxController.php
+++ app/Http/Controllers/Front/AjaxController.php
@@ -1,36 +1,24 @@
-<?php 
+<?php
 
 namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
 use App\Repositories\Interfaces\AmChart;
 use App\Repositories\Interfaces\Event;
+use App\Repositories\Interfaces\EventTranscription;
 use App\Repositories\Interfaces\Expedition;
 use App\Repositories\Interfaces\Project;
+use App\Services\Model\AjaxService;
+use App\Services\Model\EventStepChartService;
 use Artisan;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Collection;
+use Illuminate\View\View;
+use phpDocumentor\Reflection\DocBlock\Description;
 
 class AjaxController extends Controller
 {
-    /**
-     * Load AmChart for project home page.
-     *
-     * @param AmChart $amChartContract
-     * @param $projectId
-     * @return mixed
-     */
-    public function loadAmChart(AmChart $amChartContract, $projectId)
-    {
-        /*
-        if (! request()->ajax() || $projectId === null) {
-            return response()->json(['html' => 'hitting null']);
-        }
-        */
-
-        $record = $amChartContract->findBy('project_id', $projectId);
-
-        return json_decode($record->data);
-    }
-
     /**
      * Call polling command when process modal opened. Trigger inside biospex.js
      */
@@ -43,12 +31,29 @@ class AjaxController extends Controller
     }
 
     /**
-     * @param $eventId
-     * @param \App\Repositories\Interfaces\Event $eventContract
-     * @return \Illuminate\Http\JsonResponse
-     * @throws \Throwable
+     * Load AmChart for project home page.
+     *
+     * @param AmChart $amChartContract
+     * @param $projectId
+     * @return mixed
      */
-    public function scoreboard(Event $eventContract, $eventId)
+    public function loadAmChart(AmChart $amChartContract, $projectId)
+    {
+        if (! request()->ajax() || $projectId === null) {
+            return response()->json(['html' => 'hitting null']);
+        }
+
+        $record = $amChartContract->findBy('project_id', $projectId);
+
+        return json_decode($record->data);
+    }
+
+    /**
+     * @param \App\Repositories\Interfaces\Event $eventContract
+     * @param string $eventId
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Http\JsonResponse|\Illuminate\View\View
+     */
+    public function scoreboard(Event $eventContract, string $eventId)
     {
         $event = $eventContract->getEventScoreboard($eventId, ['id']);
 
@@ -58,4 +63,20 @@ class AjaxController extends Controller
 
         return view('common.scoreboard-content', ['event' => $event]);
     }
+
+    /**
+     * Display for event step charts.
+     *
+     * @param \App\Services\Model\EventStepChartService $service
+     * @param string $eventId
+     * @param string|null $timestamp
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function eventStepChart(EventStepChartService $service, string $eventId, string $timestamp = null): JsonResponse
+    {
+        $result = $service->eventStepChart($eventId, $timestamp);
+
+        return response()->json($result);
+    }
+
 }
