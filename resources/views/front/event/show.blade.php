d9b9fa51a90d24b2a1abc279303bbc912dac88ff
Robert <hb7dXEXggo6)Qq%EGs>
2020-01-06 10:54:29 -0500

    Add event step chart

--- resources/views/front/event/show.blade.php
+++ resources/views/front/event/show.blade.php
@@ -30,6 +30,28 @@
         <p class="text-justify col-6 mx-auto mt-4">{!! __('html.event_join_show') !!}</p>
     </div>
     @endif
+
+    @if($chart)
+    <div class="row my-5">
+        <div class="col-sm-10 mx-auto mt-8">
+            <h1 class="text-center content-header text-uppercase"
+                id="expeditions">{{ __('Some Title Here') }}</h1>
+            <div class="jumbotron box-shadow pt-2 pb-5">
+                <div id="chartdiv" class="d-flex" style="width:100%; height: 500px"></div>
+                <div class="hide" id="eventUrl" data-href="{{ route('ajax.get.step', [$event->id]) }}"></div>
+            </div>
+        </div>
+    </div>
+    @endif
     @include('common.scoreboard')
+@endsection
+
+@section('custom-script')
+    @if($chart)
+        <script src="//www.amcharts.com/lib/4/core.js"></script>
+        <script src="//www.amcharts.com/lib/4/charts.js"></script>
+        <script src="//www.amcharts.com/lib/4/themes/animated.js"></script>
+        <script src="{{ asset('js/amChartEventStep.min.js')}}"></script>
+    @endif
 @endsection
 
