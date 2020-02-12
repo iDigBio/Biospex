10c0bfc24f9c624e4a0b784039dce65fd7fc68c6
Robert <hb7dXEXggo6)Qq%EGs>
2020-02-12 13:08:28 -0500

    Add delete export file functions

--- app/Repositories/Interfaces/Download.php
+++ app/Repositories/Interfaces/Download.php
@@ -3,12 +3,22 @@
 namespace App\Repositories\Interfaces;
 
 use App\Repositories\RepositoryInterface;
+use Illuminate\Support\Collection;
 
 interface Download extends RepositoryInterface
 {
+    /**
+     * Get downloads for over night cleaning.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public function getDownloadsForCleaning(): Collection;
 
     /**
-     * @return mixed
+     * Get export files for expedition.
+     *
+     * @param string $expeditionId
+     * @return \Illuminate\Support\Collection
      */
-    public function getDownloadsForCleaning();
+    public function getExportFiles(string $expeditionId): Collection;
 }
