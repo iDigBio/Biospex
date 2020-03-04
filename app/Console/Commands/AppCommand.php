1fad4e6878ca1e86bab8220e5dc64cad67957fad
Robert <hb7dXEXggo6)Qq%EGs>
2020-03-04 10:00:43 -0500

    Store commits

--- app/Console/Commands/AppCommand.php
+++ app/Console/Commands/AppCommand.php
@@ -2,6 +2,8 @@
 
 namespace App\Console\Commands;
 
+use App\Repositories\Interfaces\ExportQueue;
+use App\Repositories\Interfaces\Group;
 use Illuminate\Console\Command;
 
 class AppCommand extends Command
@@ -17,10 +19,22 @@ class AppCommand extends Command
     protected $description = 'Used to test code';
 
     /**
+     * @var \App\Repositories\Interfaces\Group
+     */
+    private $group;
+
+    /**
+     * @var \App\Repositories\Interfaces\ExportQueue
+     */
+    private $queue;
+
+    /**
      * AppCommand constructor.
      */
-    public function __construct() {
+    public function __construct(Group $group, ExportQueue $queue) {
         parent::__construct();
+        $this->group = $group;
+        $this->queue = $queue;
     }
 
     /**
@@ -28,5 +42,13 @@ class AppCommand extends Command
      */
     public function handle()
     {
+        $result = $this->queue->findByIdExpeditionActor(1,251,2);
+        $users = $result->expedition->project->group->users->push($result->expedition->project->group->owner);
+        dd($users);
+
+        $group = \App\Models\Group::with('owner')->find(22);
+        $result = $this->group->getUserNotifications(22);
+        $result->push($group->owner);
+        dd($result);
     }
 }
