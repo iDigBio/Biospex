8bc17b4c480af52c1216d6f2d4ca401e7f3cf8ee
Robert <hb7dXEXggo6)Qq%EGs>
2020-09-24 16:51:48 -0400

    Add view option to data url

--- routes/front/home.php
+++ routes/front/home.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/')->uses('IndexController@index')->name('home');
 Route::get('/record/{id}')->uses('IndexController@show')->name('front.show.get');
-Route::get('/record/{id}/data')->uses('IndexController@data')->name('front.data.get');
+Route::get('/record/{id}/data/{view?}')->uses('IndexController@data')->name('front.data.get');
 
