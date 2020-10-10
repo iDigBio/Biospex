804e00ed62c380a83b6ed2418c37bbee1d01eb55
Robert <hb7dXEXggo6)Qq%EGs>
2020-10-10 10:37:40 -0400

    Fix error in text string to close it.

--- resources/views/mail/nfnexportcomplete.blade.php
+++ resources/views/mail/nfnexportcomplete.blade.php
@@ -2,7 +2,7 @@
 # {{ t('Zooniverse Export Completed') }}
 
 {{ t('The export process for "%s" has been completed successfully.', $title) }}
-{{ t('If a download file was created during this process, you may access the link on the Expedition view page.) }}
+{{ t('If a download file was created during this process, you may access the link on the Expedition view page.') }}
 
 @isset($url)
 {{ t('Errors detected during export can be downloaded by clicking the button below.') }}
