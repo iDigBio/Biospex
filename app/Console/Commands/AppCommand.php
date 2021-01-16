d6528817fa07664158c0e2579d1f2854996d1418
Robert <hb7dXEXggo6)Qq%EGs>
2021-01-16 15:16:01 -0500

    Fix issues with CSV export for grid

--- app/Console/Commands/AppCommand.php
+++ app/Console/Commands/AppCommand.php
@@ -19,9 +19,6 @@
 
 namespace App\Console\Commands;
 
-use App\Models\User;
-use App\Services\Model\ExpeditionStatService;
-use App\Services\Model\PanoptesProjectService;
 use Illuminate\Console\Command;
 
 /**
@@ -42,26 +39,10 @@ class AppCommand extends Command
     protected $description = 'Used to test code';
 
     /**
-     * @var \App\Services\Model\ExpeditionStatService
-     */
-    private $expeditionStatService;
-
-    /**
-     * @var \App\Services\Model\PanoptesProjectService
-     */
-    private $panoptesProjectService;
-
-    /**
      * AppCommand constructor.
      */
-    public function __construct(
-        ExpeditionStatService $expeditionStatService,
-        PanoptesProjectService $panoptesProjectService
-    ) {
+    public function __construct() {
         parent::__construct();
-
-        $this->expeditionStatService = $expeditionStatService;
-        $this->panoptesProjectService = $panoptesProjectService;
     }
 
     /**
@@ -69,15 +50,6 @@ class AppCommand extends Command
      */
     public function handle()
     {
-        $stats = $this->expeditionStatService->getBy('percent_completed', '100.00', '!=');
 
-        echo 'project_id, expedition_id, panoptes_project_id, panoptes_workflow_id' . PHP_EOL;
-        $stats->filter(function ($stat) {
-            return $panoptesProject = $this->panoptesProjectService->count(['expedition_id' => $stat->expedition_id]);
-        })->each(function($stat){
-            $project = $this->panoptesProjectService->findBy('expedition_id', $stat->expedition_id);
-
-            echo $project->project_id . ', ' . $project->expedition_id . ', ' . $project->panoptes_project_id . ', ' . $project->panoptes_workflow_id . PHP_EOL;
-        });
     }
 }
