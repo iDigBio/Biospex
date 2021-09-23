2ef4d06ed45f92c2c36b7cf26d08129fd00c8e6c
Robert <hb7dXEXggo6)Qq%EGs>
2021-09-23 10:02:46 -0400

    Stash changes for updates to Ubuntu 20

--- resources/js/front-app.js
+++ resources/js/front-app.js
@@ -53,38 +53,3 @@ if (token) {
 } else {
     console.error("CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token");
 }
-
-window.Vue = require("vue");
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component(
-    "passport-clients",
-    require("./components/passport/Clients.vue")
-);
-
-Vue.component(
-    "passport-authorized-clients",
-    require("./components/passport/AuthorizedClients.vue")
-);
-
-Vue.component(
-    "passport-personal-access-tokens",
-    require("./components/passport/PersonalAccessTokens.vue")
-);
-
-if ($("#app").length) {
-    const app = new Vue({
-        el: "#app"
-    });
-}
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,52 @@
 let mix = require('laravel-mix');
+let path = require("path");
 
-if (process.env.section) {
-    require(`${__dirname}/webpack.mix.${process.env.section}.js`);
-}
+mix.js("resources/js/front-app.js", "public/js/front.js")
+    .sass("resources/sass/front.scss", "public/css")
+    .extract([
+        "jquery",
+        "bootstrap",
+        "bootstrap-notify/bootstrap-notify",
+        "socket.io-client/dist/socket.io",
+        "confetti-js/dist/index.min"
+    ])
+    .copy('resources/js/amChartMap.js', 'public/js/amChartMap.js')
+    .minify('public/js/amChartMap.js')
+    .copy('resources/js/amChartTranscript.js', 'public/js/amChartTranscript.js')
+    .minify('public/js/amChartTranscript.js')
+    .copy('resources/js/amChartStat.js', 'public/js/amChartStat.js')
+    .minify('public/js/amChartStat.js')
+    .copy('resources/js/amChartEventRate.js', 'public/js/amChartEventRate.js')
+    .minify('public/js/amChartEventRate.js')
+    .copy('resources/js/amChartBingo.js', 'public/js/amChartBingo.js')
+    .minify('public/js/amChartBingo.js')
+    .copy('resources/images', 'public/images').version().disableNotifications();
+
+mix.webpackConfig({
+    resolve: {
+        alias: {
+            'jquery': path.join(__dirname, 'node_modules/jquery/src/jquery'),
+            "jquery-ui/sortable": "jquery-ui/ui/widgets/sortable"
+        }
+    }
+}).js("resources/js/admin-app.js", "public/backend/js/admin.js")
+    .sass("resources/sass/admin.scss", "public/backend/css")
+    .extract([
+        "jquery",
+        "bootstrap",
+        "bootstrap-notify/bootstrap-notify",
+        "jquery-ui/ui/widgets/dialog",
+        "jquery-ui/ui/widgets/draggable",
+        "jquery-ui/ui/widgets/droppable",
+        "jquery-ui/ui/widgets/resizable",
+        "jquery-datetimepicker/build/jquery.datetimepicker.full",
+        "free-jqgrid/dist/plugins/ui.multiselect",
+        "free-jqgrid/js/jquery.jqgrid.min",
+
+        "codemirror/lib/codemirror",
+        "summernote/dist/summernote",
+        "socket.io-client/dist/socket.io",
+    ])
+    .copy('resources/js/jquery.form.min.js', 'public/backend/js/jquery.form.min.js')
+    .copy('resources/js/expertReview.js', 'public/backend/js/expertReview.js')
+    .minify('public/backend/js/expertReview.js').version().disableNotifications();
