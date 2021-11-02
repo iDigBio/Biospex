d5418571cbc151d5873b188b6df9dd84f4905c8e
Robert <hb7dXEXggo6)Qq%EGs>
2021-11-02 12:27:23 -0400

    change cookie setting in sessions to avoid error

--- config/session.php
+++ config/session.php
@@ -166,7 +166,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE', null),
+    'secure' => env('SESSION_SECURE_COOKIE'),
 
     /*
     |--------------------------------------------------------------------------
@@ -194,6 +194,6 @@ return [
     |
     */
 
-    'same_site' => null,
+    'same_site' => 'lax',
 
 ];
