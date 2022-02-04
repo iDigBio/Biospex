ffbff14a5662a80b3e1e199f2c39443b61f9c809
Robert <hb7dXEXggo6)Qq%EGs>
2022-02-04 13:18:26 -0500

    Change env queue driver to avoid conflict with soketi

--- config/queue.php
+++ config/queue.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('QUEUE_DRIVER', 'sync'),
+    'default' => env('LARAVEL_QUEUE_DRIVER', 'sync'),
 
     /*
     |--------------------------------------------------------------------------
