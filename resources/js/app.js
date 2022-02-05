69a9f1795ae08972444fbe0d79824f41f6d7b008
Robert <hb7dXEXggo6)Qq%EGs>
2022-02-05 14:24:00 -0500

    Change out laravel-echo-server for soketi

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,10 +5,18 @@
  */
 import Echo from "laravel-echo"
 
-window.io = require("socket.io-client");
+window.Pusher = require('pusher-js');
+
 window.Echo = new Echo({
-    broadcaster: "socket.io",
-    host: window.location.hostname + ':6001'
+    broadcaster: 'pusher',
+    key: process.env.MIX_PUSHER_APP_KEY,
+    forceTLS: false,
+    encrypted: true,
+    wsHost: window.location.hostname,
+    wsPort: 80,
+    wssPort: 443,
+    disableStats: true,
+    enabledTransports: ["ws", "wss"],
 });
 
 /**
