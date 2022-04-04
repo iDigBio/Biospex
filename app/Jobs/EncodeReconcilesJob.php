0e733c872e12222b36988418861e5b9403464e4d
Robert <hb7dXEXggo6)Qq%EGs>
2022-04-03 21:51:17 -0400

    Set back to regular job of copying reconciles.

--- app/Jobs/EncodeReconcilesJob.php
+++ app/Jobs/EncodeReconcilesJob.php
@@ -45,13 +45,10 @@ class EncodeReconcilesJob implements ShouldQueue
         $reserved = config('config.reserved_encoded');
         $user = User::find(1);
         try {
-            /*
             $timestamp = Carbon::now()->subDays(2);
             $cursor = Reconcile::where('created_at', '>=', $timestamp)
                 ->orderBy('created_at', 'DESC')
                 ->cursor();
-            */
-            $cursor = Reconcile::orderBy('created_at', 'DESC')->cursor();
 
             $i=0;
             foreach ($cursor as $record) {
