0c8b0ca458801f7c1f3f0d4ec56b5e84a610c309
Robert <hb7dXEXggo6)Qq%EGs>
2022-04-17 12:09:20 -0400

    Commit changes

--- resources/lang/en/auth.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return array (
-  'failed' => 'These credentials do not match our records or account may need to be activated.',
-  'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
-);
