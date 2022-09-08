ef863d269211b5e397cbad59bea1c730ddf9223f
Robert <hb7dXEXggo6)Qq%EGs>
2022-09-08 11:25:08 -0400

    Add spacing for email

--- config/filesystems.php
+++ config/filesystems.php
@@ -53,13 +53,13 @@ return [
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-            'throw' => false,
+            'throw' => true,
             'visibility' => 'private'
         ],
         'efs' => [
             'driver' => 'local',
             'root' => '/efs',
-            'throw' => false,
+            'throw' => true,
         ]
     ],
 
