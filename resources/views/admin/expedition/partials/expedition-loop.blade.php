1c1624a5f135e4c477b5198840d99800647563fa
Robert <hb7dXEXggo6)Qq%EGs>
2023-07-14 09:16:36 -0400

    fix error in blade template

--- resources/views/admin/expedition/partials/expedition-loop.blade.php
+++ resources/views/admin/expedition/partials/expedition-loop.blade.php
@@ -20,7 +20,7 @@
                 {!! $expedition->present()->expedition_show_icon !!}
                 {!! $expedition->present()->expedition_edit_icon !!}
                 {!! $expedition->present()->expedition_clone_icon !!}
-                @can('isOwner', $group)
+                @can('isOwner', $expedition->project->group)
                     {!! $expedition->present()->expedition_delete_icon !!}
                 @endcan
             </div>
