553e4476de0158cfb51a24b92f40b03862239e06
rbruhn <bruhnrp@yahoo.com>
2023-08-19 17:32:51 -0400

    Change button label.

--- resources/views/admin/expedition/partials/geolocate-btns.blade.php
+++ resources/views/admin/expedition/partials/geolocate-btns.blade.php
@@ -1,3 +1,3 @@
 <hr class="header mx-auto" style="width:300px;">
 <h4>{{ $actor->title }}</h4>
-<a href="{{ route('admin.geolocate.index', [$expedition->project_id, $expedition->id]) }}" class="btn btn-primary rounded-0 mb-1">{{ t('GeoLocate Index') }}</a>
+<a href="{{ route('admin.geolocate.index', [$expedition->project_id, $expedition->id]) }}" class="btn btn-primary rounded-0 mb-1">{{ t('GeoLocate') }}</a>
