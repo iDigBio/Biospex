f990d30f9467842ee10ac3a95184463520cf328c
rbruhn <bruhnrp@gmail.com>
2023-12-04 12:28:17 -0500

    Fix issue with Handler.php throwing errors

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -61,14 +61,12 @@ class Handler extends ExceptionHandler
     }
 
     /**
-     * Render an exception into an HTTP response.
-     *
-     * @param \Illuminate\Http\Request $request
+     * @param $request
      * @param \Throwable $e
-     * @return \Illuminate\Http\Response
+     * @return \Symfony\Component\HttpFoundation\Response
      * @throws \Throwable
      */
-    public function render($request, Throwable $e): \Illuminate\Http\Response
+    public function render($request, Throwable $e): \Symfony\Component\HttpFoundation\Response
     {
         return parent::render($request, $e);
     }
