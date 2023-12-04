c2a3c8b6d3222ca04d313cc026002ee4316c1471
rbruhn <bruhnrp@yahoo.com>
2023-12-04 11:59:28 -0500

    Change contributor logos

--- resources/views/front/layout/contributors.blade.php
+++ resources/views/front/layout/contributors.blade.php
@@ -5,9 +5,10 @@
             <!-- Grid column -->
             <div class="col-md-12 text-center d-inline d-sm-flex align-items-start justify-content-between">
                 <a class="figure-img" href="https://www.fsu.edu" target="_blank"><img src="/images/contributors/fsu.png" alt="FSU"></a>
-                <a class="figure-img" href="https://www.floridamuseum.ufl.edu/" target="_blank"><img src="/images/contributors/flmnh.png" alt="Florida Museum"></a>
-                <a class="figure-img" href="https://www.idigbio.org/" target="_blank"><img src="/images/contributors/idigbio.png" alt="IdigBio" style="max-width:200px; margin-top:-10px;"></a>
-                <a class="figure-img" href="http://www.ufl.edu/" target="_blank"><img src="/images/contributors/uf.png" alt="University of Florida"></a>
+                <!-- <a class="figure-img" href="https://www.floridamuseum.ufl.edu/" target="_blank"><img src="/images/contributors/flmnh.png" alt="Florida Museum"></a> -->
+                <a class="figure-img" href="https://idiginfo.org" target="_blank"><img src="/images/contributors/idiginfo.png" alt="iDigInfo" style="max-width:200px; margin-top:20px;"></a>
+                <a class="figure-img" href="https://www.idigbio.org/" target="_blank"><img src="/images/contributors/idigbio.png" alt="iDigBio" style="max-width:200px; margin-top:-10px;"></a>
+                <!-- <a class="figure-img" href="http://www.ufl.edu/" target="_blank"><img src="/images/contributors/uf.png" alt="University of Florida"></a> -->
                 <a class="figure-img" href="https://www.nsf.gov/" target="_blank"><img src="/images/contributors/nsf.png" alt="National Science Foundation"></a>
             </div>
             <!-- Grid column -->
