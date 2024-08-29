abf2b4bba8f0027a27d87f7545d176cdab6acde5
rbruhn <bruhnrp@gmail.com>
2024-08-29 14:45:16 -0400

    Add comment

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,15 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-
+/*
+ * index
+ * create
+ * store
+ * show
+ * edit
+ * update
+ * destroy
+ */
 abstract class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
