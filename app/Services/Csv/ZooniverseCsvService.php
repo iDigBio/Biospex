87b0d4bb7b0d02be979cd9a967943d35df31649e
rbruhn <bruhnrp@gmail.com>
2024-09-08 13:13:46 -0400

    Update Zooniverse files to Master

--- app/Services/Csv/ZooniverseCsvService.php
+++ app/Services/Csv/ZooniverseCsvService.php
@@ -122,23 +122,15 @@ class ZooniverseCsvService
     }
 
     /**
-     * Check for errors field on zooniverse classification.
-     */
-    public function checkErrors(array $result): bool
-    {
-        return isset($result['errors']);
-    }
-
-    /**
-     * Calculate time difference.
-     * If errors, csv doesn't exist yet.
+     * Check if enough time has passed to create or update CSV files.
      * Hours must be greater than 24 hours for Zooniverse to create CSV.
+     * Job is set to run every 48 hours.
      */
     public function checkDateTime(array $result): bool
     {
-        return empty($result['media'][0]['updated_at']) ?
-            $this->parseTime($result['media'][0]['created_at']) > 32 :
-            $this->parseTime($result['media'][0]['updated_at']) > 32;
+        return empty($result['media'][0]['created_at']) ||
+            empty($result['media'][0]['updated_at']) ||
+            $this->parseTime($result['media'][0]['updated_at']) > 47;
     }
 
     /**
