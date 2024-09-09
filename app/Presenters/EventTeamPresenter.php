406074f97cd905cae509a891896963a9f3c5c23d
rbruhn <bruhnrp@gmail.com>
2024-09-09 14:50:33 -0400

    Commit changes to updating events.

--- app/Presenters/EventTeamPresenter.php
+++ app/Presenters/EventTeamPresenter.php
@@ -31,10 +31,11 @@ class EventTeamPresenter extends Presenter
      */
     public function teamJoinUrlIcon()
     {
-        return '<a href="#" class="btn btn-primary p-2 m-1 prevent-default"
-                title="'.t('Copy To Clipboard').'" 
-                data-hover="tooltip"
-                data-name="js-copy"
+        return '<a href="'.route('front.events_team_user.create', [$this->model]).'" 
+            class="btn btn-primary p-2 m-1 prevent-default"
+            title="'.t('Copy To Clipboard').'" 
+            data-hover="tooltip"
+            data-name="js-copy"
             data-value="'.route('front.events_team_user.create', [$this->model]).'">
             <i class="fas fa-clipboard align-middle" aria-hidden="true"></i>
             <span class="align-middle">'.$this->model->title.'</span></a>';
