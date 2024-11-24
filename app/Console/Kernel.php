6ea6eddc4d293a5b35e2408c27fede4614b59e33
rbruhn <bruhnrp@gmail.com>
2024-11-24 15:07:09 -0500

    Fixes #290 Over night script to check lambda-reconciliation folder

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -1,4 +1,21 @@
 <?php
+/*
+ * Copyright (C) 2015  Biospex
+ * biospex@gmail.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 namespace App\Console;
 
@@ -38,19 +55,15 @@ class Kernel extends ConsoleKernel
         // Clean bingo maps
         $schedule->command('bingo:clean')->dailyAt('10:05');
 
+        // Run lambda-reconciliation check every morning at 6:00
+        $schedule->command('app:check-lambda-reconcile')->dailyAt('6:00');
+
         if ($this->app->environment('production')) {
             // Trigger workflow manager to handle csv creation and updating expedition/project
             $schedule->command('workflow:manage')->daily()->before(function () {
                 Cache::flush();
                 Artisan::call('lada-cache:flush');
             });
-
-            /* May need to be re-enabled
-            $schedule->command('workflow:manage')->days([0, 2, 4])->before(function () {
-                Cache::flush();
-                Artisan::call('lada-cache:flush');
-            });
-            */
 
             // WeDigBio classification cron. Pulls pusher records from MySql table and enters into pusher_transcriptions
             $schedule->command('dashboard:records')->everyFiveMinutes();
