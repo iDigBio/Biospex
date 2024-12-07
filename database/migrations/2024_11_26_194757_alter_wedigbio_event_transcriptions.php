adc5817e93864c561a86b9077f07866009cc64cd
rbruhn <bruhnrp@gmail.com>
2024-12-07 18:14:17 -0500

    Fix migration

--- database/migrations/2024_11_26_194757_alter_wedigbio_event_transcriptions.php
+++ database/migrations/2024_11_26_194757_alter_wedigbio_event_transcriptions.php
@@ -1,4 +1,21 @@
 <?php
+/*
+ * Copyright (C) 2015  Biospex
+ * biospex@gmail.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 use Illuminate\Database\Migrations\Migration;
 
@@ -11,7 +28,7 @@ return new class extends Migration
     {
         Schema::table('wedigbio_event_transcriptions', function (\Illuminate\Database\Schema\Blueprint $table) {
             DB::statement('ALTER TABLE `wedigbio_event_transcriptions` CHANGE `date_id` `event_id` BIGINT UNSIGNED NOT NULL;');
-            DB::statement('ALTER TABLE `biospex`.`wedigbio_event_transcriptions` DROP INDEX `wedigbio_event_transcriptions_date_id_foreign`, ADD INDEX `wedigbio_event_transcriptions_event_id_foreign` (`event_id`) USING BTREE;');
+            DB::statement('ALTER TABLE `wedigbio_event_transcriptions` DROP INDEX `wedigbio_event_transcriptions_date_id_foreign`, ADD INDEX `wedigbio_event_transcriptions_event_id_foreign` (`event_id`) USING BTREE;');
         });
     }
 
