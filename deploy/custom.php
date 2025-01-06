cbbbcc474841435994a1cb94e71761dac7fec84f
rbruhn <bruhnrp@gmail.com>
2025-01-06 15:38:57 -0500

    Add custom deployment tasks in deploy/custom.php
    Introduced deployment tasks for running update queries, managing file deployments, setting permissions, and handling dependencies. Also added environment file upload logic based on the deployment host. These changes enhance deployment flexibility and automate key processes.

---
+++ deploy/custom.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * Copyright (c) 2022. Digitization Academy
+ * idigacademy@gmail.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace Deployer;
+
+desc('Running update queries...');
+task('artisan:app:update-queries', function () {
+    cd('{{release_or_current_path}}');
+    run('php artisan app:update-queries');
+});
+
+desc('Deploying files...');
+task('artisan:app:deploy-files', function () {
+    cd('{{release_or_current_path}}');
+    run('php artisan app:deploy-files');
+});
+
+desc('Setting permissions...');
+task('set:permissions', function () {
+    run('sudo chown -R ubuntu.www-data {{deploy_path}}');
+    run('sudo truncate -s 0 {{release_or_current_path}}/storage/logs/laravel.log');
+});
+
+desc('Install project dependencies');
+task('yarn:run-install', function () {
+    cd('{{release_path}}');
+    run('yarn install --ignore-engines');
+});
+
+desc('Build project');
+task('npm:run-build', function () {
+    cd('{{release_path}}');
+    run('npm run build');
+});
+
+desc('Upload env file depending on the host');
+task('upload:env', function () {
+    $alias = currentHost()->get('alias');
+    $file = match ($alias) {
+        'production' => '.env.aws.prod',
+        'development' => '.env.aws.dev'
+    };
+    upload($file, '{{deploy_path}}/shared/.env');
+});
