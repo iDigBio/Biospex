87d11f982aee39f64b071f631fc1721e5db38795
rbruhn <bruhnrp@gmail.com>
2025-03-08 12:56:57 -0500

    Add AppSupervisorProcessCommand to manage supervisor processes
    This command provides an interactive console to view and restart specific supervisor processes. It executes system commands to fetch process statuses and allows users to choose processes for restarting directly via the CLI.

---
+++ app/Console/Commands/AppSupervisorProcessCommand.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+ * Copyright (C) 2015  Biospex
+ * biospex@gmail.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class AppSupervisorProcessCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:supervisor-process';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        exec('sudo supervisorctl status', $output, $return_var);
+
+        foreach ($output as &$line) {
+            $line = preg_replace('/\s.*/', '', $line);
+        }
+        unset($line);
+
+        $output[] = 'exit';
+        $choice = $this->choice('Please choose process to restart:', $output);
+
+        if ($choice === 'exit') {
+            return;
+        }
+
+        echo "Restarting $choice...".PHP_EOL;
+
+        exec("sudo supervisorctl restart $choice");
+    }
+}
